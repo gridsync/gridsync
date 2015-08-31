cc2fb1c6ce1c956cca8fc2081714cccfba326360
Chris Wood <crwood@gmail.com>
2015-08-31 17:28:00 -0400

    Add trivial tahoe tests

--- tests/test_tahoe.py
+++ tests/test_tahoe.py
@@ -1,10 +1,32 @@
-from gridsync.tahoe import *
+# -*- coding: utf-8 -*-
 
-def setup_module():
-    print 'setup'
+import os
+import tempfile
 
-def test_print():
-    assert 1 == 2
+from gridsync.tahoe import bin_tahoe, Tahoe
 
-def teardown_module():
-    print 'teardown'
+
+def test_bin_tahoe_exists():
+    assert bin_tahoe()
+
+def test_bin_tahoe_is_executable():
+    assert os.access(bin_tahoe(), os.X_OK)
+
+
+class TestTahoe():
+    def setup_class(self):
+        self.tmp_dir = tempfile.mkdtemp()
+        self.tahoe = Tahoe(self.tmp_dir)
+
+    def test_init(self):
+        assert self.tahoe
+
+    def test_name(self):
+        assert self.tahoe.name
+
+    def test_node_dir_exists(self):
+        assert os.path.isdir(self.tahoe.node_dir)
+
+    def test_tahoe_version_1_10_or_greater(self):
+        assert self.tahoe.version() > '1.10'
+
