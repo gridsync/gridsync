cc299b219507985b5e80f23a1191d695e0151e0e
Chris Wood <crwood@gmail.com>
2015-08-30 20:43:28 -0400

    Move watcher functionality to separate module

--- gridsync/sync.py
+++ gridsync/sync.py
@@ -8,16 +8,13 @@ import time
 
 from twisted.internet import reactor
 from twisted.internet.defer import gatherResults
-from twisted.internet.task import LoopingCall
 from twisted.internet.threads import deferToThread, blockingCallFromThread
-from watchdog.events import PatternMatchingEventHandler
-from watchdog.observers import Observer
+
+from gridsync.watcher import LocalWatcher, RemoteWatcher
 
 
-class SyncFolder(PatternMatchingEventHandler):
+class SyncFolder():
     def __init__(self, local_dir, remote_dircap, tahoe=None):
-        super(SyncFolder, self).__init__(
-                ignore_patterns=["*.gridsync-versions*"])
         if not tahoe:
             from gridsync.tahoe import Tahoe
             tahoe = Tahoe()
@@ -26,93 +23,17 @@ class SyncFolder(PatternMatchingEventHandler):
         self.remote_dircap = remote_dircap
         self.versions_dir = os.path.join(self.local_dir, '.gridsync-versions')
         self.local_snapshot = 0
-        self.filesystem_modified = False
         self.do_backup = False
         self.sync_state = 0
         self.sync_log = []
+        self.local_watcher = LocalWatcher(self, self.local_dir)
+        self.remote_watcher = RemoteWatcher(self, self.remote_dircap, self.tahoe)
         logging.debug("{} initialized; "
                 "{} <-> {}".format(self, self.local_dir, self.remote_dircap))
 
     def start(self):
-        self.local_checker = LoopingCall(self.check_for_backup)
-        self.local_checker.start(1)
-        self.remote_checker = LoopingCall(
-                reactor.callInThread, self.check_for_new_snapshot)
-        self.remote_checker.start(30)
-        self.start_observer()
-
-    def on_modified(self, event):
-        self.filesystem_modified = True
-
-    def check_for_backup(self):
-        if self.filesystem_modified:
-            self.filesystem_modified = False
-            if self.sync_state:
-                #self.do_backup = True
-                pass
-            else:
-                reactor.callInThread(self.wait_for_writes)
-
-    def wait_for_writes(self):
-        time.sleep(1)
-        if not self.filesystem_modified:
-            self.perform_backup()
-
-    def perform_backup(self):
-        remote_snapshot = self.get_latest_snapshot()
-        if self.local_snapshot == remote_snapshot:
-            self.sync(skip_comparison=True)
-        else:
-            self.sync(snapshot=remote_snapshot)
-
-    def check_for_new_snapshot(self, num_attempts=3):
-        logging.debug("Checking for new snapshot...")
-        try:
-            remote_snapshot = self.get_latest_snapshot()
-        except Exception, e:
-            logging.error(e)
-            if num_attempts:
-                logging.debug("get_latest_snapshot() failed; "
-                        "(maybe we're not connected yet?).")
-                logging.debug("Trying again after 1 sec. "
-                        "({} attempts remaining)...".format(num_attempts))
-                time.sleep(1)
-                self.check_for_new_snapshot(num_attempts - 1)
-            else:
-                logging.debug("Attempts exhausted; attempting repair...")
-                # XXX
-            return
-        if remote_snapshot != self.local_snapshot:
-            logging.debug("New snapshot available: {}".format(remote_snapshot))
-            if not self.sync_state:
-                #reactor.callInThread(self.sync, snapshot=remote_snapshot)
-                self.sync(snapshot=remote_snapshot)
-
-    def get_latest_snapshot(self):
-        # TODO: If /Archives doesn't exist, perform (first?) backup?
-        dircap = self.remote_dircap + "/Archives"
-        j = self.tahoe.ls_json(dircap)
-        snapshots = []
-        for snapshot in j[1]['children']:
-            snapshots.append(snapshot)
-        snapshots.sort()
-        return snapshots[-1:][0]
-
-    def get_local_metadata(self, basedir):
-        metadata = {}
-        for root, dirs, files in os.walk(basedir, followlinks=True):
-            for name in dirs:
-                path = os.path.join(root, name)
-                if not path.startswith(self.versions_dir):
-                    metadata[path] = {}
-            for name in files:
-                path = os.path.join(root, name)
-                if not path.startswith(self.versions_dir):
-                    metadata[path] = {
-                        'mtime': os.path.getmtime(path),
-                        'size': os.path.getsize(path)
-                    }
-        return metadata
+        self.local_watcher.start()
+        self.remote_watcher.start()
 
     def _create_conflicted_copy(self, filename, mtime):
         base, extension = os.path.splitext(filename)
@@ -133,37 +54,25 @@ class SyncFolder(PatternMatchingEventHandler):
         logging.info("Creating {}".format(versioned_filepath))
         shutil.copy2(local_filepath, versioned_filepath)
 
-    def get_remote_metadata(self, dircap, basedir=''):
-        metadata = {}
-        logging.debug("Getting remote metadata from {}...".format(dircap))
-        received_data = self.tahoe.ls_json(dircap)
-        jobs = []
-        for filename, data in received_data[1]['children'].iteritems():
-            path = '/'.join([basedir, filename]).strip('/')
-            metadata[path] = {
-                'uri': data[1]['ro_uri'],
-                'mtime': data[1]['metadata']['mtime'],
-            }
-            if data[0] == 'dirnode':
-                jobs.append(deferToThread(self.get_remote_metadata, 
-                    metadata[path]['uri'], path))
-        results = blockingCallFromThread(reactor, gatherResults, jobs)
-        for result in results:
-            metadata.update(result)
-        return metadata
-
     def sync(self, snapshot='Latest', skip_comparison=False):
         if snapshot != 'Latest':
             snapshot = 'Archives/' + snapshot
         self.sync_state += 1
+        
+        remote_snapshot = self.remote_watcher.get_latest_snapshot()
+        if self.local_snapshot == remote_snapshot:
+            skip_comparison=True
+        else:
+            snapshot = 'Archives/' + remote_snapshot
+        
         logging.info("Syncing {} with {}...".format(self.local_dir, snapshot))
         if skip_comparison:
             self.backup(self.local_dir, self.remote_dircap)
             self.sync_complete()
             return
         remote_path = '/'.join([self.remote_dircap, snapshot])
-        self.local_metadata = self.get_local_metadata(self.local_dir)
-        self.remote_metadata = self.get_remote_metadata(remote_path)
+        self.local_metadata = self.local_watcher.get_local_metadata(self.local_dir)
+        self.remote_metadata = self.remote_watcher.get_remote_metadata(remote_path)
         # TODO: If tahoe.get_metadata() fails or doesn't contain a
         # valid snapshot, jump to backup?
         jobs = []
@@ -214,7 +123,7 @@ class SyncFolder(PatternMatchingEventHandler):
         self.sync_complete()
 
     def sync_complete(self):
-        self.local_snapshot = self.get_latest_snapshot() # XXX Race
+        self.local_snapshot = self.remote_watcher.get_latest_snapshot() # XXX Race
         logging.info("Synchronized {} with {}".format(
                 self.local_dir, self.local_snapshot))
         self.sync_state -= 1
@@ -233,27 +142,7 @@ class SyncFolder(PatternMatchingEventHandler):
                 filename = line.split()[1][:-3][1:].lstrip(self.local_dir) # :/
                 self.sync_log.append("Uploaded {}".format(filename))
 
-    def start_observer(self):
-        logging.info("Starting Observer in {}...".format(self.local_dir))
-        try:
-            self.observer = Observer()
-            self.observer.schedule(self, self.local_dir, recursive=True)
-            self.observer.start()
-        except Exception, e:
-            logging.error(e)
-
-    def stop_observer(self):
-        logging.info("Stopping Observer in {}...".format(self.local_dir))
-        try:
-            self.observer.stop()
-            self.observer.join()
-        except Exception, e:
-            logging.error(e)
-
     def stop(self):
-        logging.info("Stopping SyncFolder ({} <-> {})"
-                .format(self.local_dir, self.remote_dircap))
-        self.local_checker.stop()
-        self.remote_checker.stop()
-        self.stop_observer()
+        self.local_watcher.stop()
+        self.remote_watcher.stop()
 
---
+++ gridsync/watcher.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+import time
+
+from twisted.internet import reactor
+from twisted.internet.defer import gatherResults
+from twisted.internet.task import LoopingCall
+from twisted.internet.threads import deferToThread, blockingCallFromThread
+from watchdog.events import PatternMatchingEventHandler
+from watchdog.observers import Observer
+
+
+class LocalWatcher(PatternMatchingEventHandler):
+    def __init__(self, parent, local_dir):
+        super(LocalWatcher, self).__init__(
+                ignore_patterns=["*.gridsync-versions*"])
+        self.parent = parent
+        self.local_dir = os.path.expanduser(local_dir)
+        self.versions_dir = os.path.join(self.local_dir, '.gridsync-versions')
+        self.filesystem_modified = False
+        logging.debug("LocalWatcher initialized for {} ({})".format(
+                self.local_dir, self))
+
+    def start(self):
+        self.local_checker = LoopingCall(self.check_for_changes)
+        self.local_checker.start(1)
+        logging.info("Starting Observer in {}...".format(self.local_dir))
+        try:
+            self.observer = Observer()
+            self.observer.schedule(self, self.local_dir, recursive=True)
+            self.observer.start()
+        except Exception, e:
+            logging.error(e)
+
+    def on_modified(self, event):
+        self.filesystem_modified = True
+
+    def check_for_changes(self):
+        if self.filesystem_modified:
+            self.filesystem_modified = False
+            if not self.parent.sync_state:
+                reactor.callInThread(self.wait_for_writes)
+            #else:
+            #    self.parent.do_backup = True
+
+    def wait_for_writes(self):
+        time.sleep(1)
+        if not self.filesystem_modified:
+            self.parent.sync()
+
+    def get_local_metadata(self, basedir=None):
+        metadata = {}
+        if not basedir:
+            basedir = self.local_dir
+        for root, dirs, files in os.walk(basedir, followlinks=True):
+            for name in dirs:
+                path = os.path.join(root, name)
+                if not path.startswith(self.versions_dir):
+                    metadata[path] = {}
+            for name in files:
+                path = os.path.join(root, name)
+                if not path.startswith(self.versions_dir):
+                    metadata[path] = {
+                        'mtime': os.path.getmtime(path),
+                        'size': os.path.getsize(path)
+                    }
+        return metadata
+
+    def stop(self):
+        self.local_checker.stop()
+        logging.info("Stopping Observer in {}...".format(self.local_dir))
+        try:
+            self.observer.stop()
+            self.observer.join()
+        except Exception, e:
+            logging.error(e)
+
+
+class RemoteWatcher():
+    def __init__(self, parent, remote_dircap, tahoe=None):
+        if not tahoe:
+            from gridsync.tahoe import Tahoe
+            tahoe = Tahoe()
+        self.parent = parent
+        self.tahoe = tahoe
+        self.remote_dircap = remote_dircap
+        logging.debug("RemoteWatcher initialized for {} ({})".format(
+                self.remote_dircap, self))
+
+    def start(self):
+        self.remote_checker = LoopingCall(reactor.callInThread, 
+                self.check_for_changes)
+        self.remote_checker.start(30)
+
+    def check_for_changes(self, num_attempts=3):
+        logging.debug("Checking for new snapshot...")
+        try:
+            remote_snapshot = self.get_latest_snapshot()
+        except Exception, e:
+            logging.error(e)
+            if num_attempts:
+                logging.debug("get_latest_snapshot() failed; "
+                        "(maybe we're not connected yet?).")
+                logging.debug("Trying again after 1 sec. "
+                        "({} attempts remaining)...".format(num_attempts))
+                time.sleep(1)
+                self.check_for_changes(num_attempts - 1)
+            else:
+                logging.debug("Attempts exhausted; attempting repair...")
+                # XXX
+            return
+        if remote_snapshot != self.parent.local_snapshot:
+            logging.debug("New snapshot available: {}".format(remote_snapshot))
+            if not self.parent.sync_state:
+                #reactor.callInThread(self.parent.sync, snapshot=remote_snapshot)
+                self.parent.sync(snapshot=remote_snapshot)
+
+    def get_latest_snapshot(self):
+        # TODO: If /Archives doesn't exist, perform (first?) backup?
+        dircap = self.remote_dircap + "/Archives"
+        j = self.tahoe.ls_json(dircap)
+        snapshots = []
+        for snapshot in j[1]['children']:
+            snapshots.append(snapshot)
+        snapshots.sort()
+        return snapshots[-1:][0]
+
+    def get_remote_metadata(self, dircap, basedir=''):
+        metadata = {}
+        received_data = self.tahoe.ls_json(dircap)
+        logging.debug("Getting remote metadata from {}...".format(dircap))
+        jobs = []
+        for filename, data in received_data[1]['children'].iteritems():
+            path = '/'.join([basedir, filename]).strip('/')
+            metadata[path] = {
+                'uri': data[1]['ro_uri'],
+                'mtime': data[1]['metadata']['mtime'],
+            }
+            if data[0] == 'dirnode':
+                jobs.append(deferToThread(self.get_remote_metadata,
+                    metadata[path]['uri'], path))
+        results = blockingCallFromThread(reactor, gatherResults, jobs)
+        for result in results:
+            metadata.update(result)
+        return metadata
+
+    def stop(self):
+        self.remote_checker.stop()
+
