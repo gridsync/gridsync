b0db48f72b563a3ae5837784c261bf1091888fc0
Chris Wood <crwood@gmail.com>
2015-11-03 12:48:39 -0500

    Use absolute import for _version in __init__

--- gridsync/__init__.py
+++ gridsync/__init__.py
@@ -2,7 +2,7 @@
 Synchronize local directories with Tahoe-LAFS storage grids.
 """
 
-from _version import __version__
+from gridsync._version import __version__
 
 __author__ = 'Christopher R. Wood'
 __url__ = 'https://github.com/gridsync/gridsync'
