3d7fa9dfddc804cc58e505912b2c460e95fea984
Chris Wood <crwood@gmail.com>
2015-11-26 20:03:22 -0500

    Fix PyTest TestCommand in setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,23 @@ import os
 import re
 import sys
 
-from setuptools import setup, find_packages, Command
+from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-class PyTest(Command):
+class PyTest(TestCommand):
     user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
 
     def initialize_options(self):
+        TestCommand.initialize_options(self)
         self.pytest_args = []
 
     def finalize_options(self):
-        pass
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
-    def run(self):
+    def run_tests(self):
         import pytest
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
@@ -43,6 +46,7 @@ class Tox(TestCommand):
             args = shlex.split(self.tox_args)
         errno = tox.cmdline(args=args)
         sys.exit(errno)
+
 
 requirements = ['watchdog', 'qt5reactor-fork', 'requests', 'twisted']
 if sys.version_info.major == 2:
