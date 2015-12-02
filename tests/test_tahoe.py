90e4f771274a2f5eeebb2b63f283d2edf13a1f49
Chris Wood <crwood@gmail.com>
2015-12-02 18:33:56 -0500

    Add tests for tahoe:decode_introducer_furl()

--- tests/test_tahoe.py
+++ tests/test_tahoe.py
@@ -5,7 +5,32 @@ import tempfile
 
 import pytest
 
-from gridsync.tahoe import DEFAULT_SETTINGS, bin_tahoe, Tahoe
+from gridsync.tahoe import Tahoe, decode_introducer_furl
+
+
+def test_decode_introducer_furl():
+    furl = 'pb://abc234@example.org:12345/introducer'
+    assert decode_introducer_furl(furl) == ('abc234', 'example.org:12345')
+
+def test_decode_introducer_furl_no_host_separator():
+    furl = 'pb://abc234example.org:12345/introducer'
+    with pytest.raises(AttributeError):
+        assert decode_introducer_furl(furl)
+
+def test_decode_introducer_furl_no_port_separator():
+    furl = 'pb://abc234@example.org12345/introducer'
+    with pytest.raises(AttributeError):
+        assert decode_introducer_furl(furl)
+
+def test_decode_introducer_furl_invalid_char_in_connection_hint():
+    furl = 'pb://abc234@exam/ple.org:12345/introducer'
+    with pytest.raises(AttributeError):
+        assert decode_introducer_furl(furl)
+
+def test_decode_introducer_furl_tub_id_not_base32():
+    furl = 'pb://abc123@example.org:12345/introducer'
+    with pytest.raises(AttributeError):
+        assert decode_introducer_furl(furl)
 
 
 class TestTahoe():
