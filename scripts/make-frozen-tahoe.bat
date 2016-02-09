a10840284ba9129012fccd931c4aa4d1d5329ca3
Chris Wood <crwood@gmail.com>
2016-02-08 22:21:28 -0500

    Update add workaround for nevow/twisted version-pinning on Windows

--- scripts/make-frozen-tahoe.bat
+++ scripts/make-frozen-tahoe.bat
@@ -15,6 +15,9 @@ call git clone https://github.com/tahoe-lafs/tahoe-lafs.git .\build\tahoe-lafs
 :: Needed for frozen builds...
 call git apply disable_setuptools.patch
 
+:: Workaround for Nevow-0.10.0 requiring twisted.python when Twisted 12.1 is pinned..
+call pip install --upgrade twisted
+
 call pip install --upgrade .\build\tahoe-lafs
 
 call pip install --upgrade pyinstaller
