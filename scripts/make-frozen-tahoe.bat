2733660256fd9b756ca3f013180dcedeb4586f9a
Chris Wood <crwood@gmail.com>
2016-02-09 19:50:44 -0500

    Remove twisted/nevow workarounds in frozen tahoe build script

--- scripts/make-frozen-tahoe.bat
+++ scripts/make-frozen-tahoe.bat
@@ -6,17 +6,12 @@
 :: installed; see/run 'make-tahoe-deps.bat'
 ::
 
-call pip install --upgrade virtualenv
-call virtualenv --clear .\build\venv
+call C:\Python27\python.exe -m pip install --upgrade virtualenv
+call C:\Python27\python.exe -m pip virtualenv --clear .\build\venv
 call .\build\venv\Scripts\activate
 
-call git clone https://github.com/tahoe-lafs/tahoe-lafs.git .\build\tahoe-lafs
-
-:: Needed for frozen builds...
-call git apply disable_setuptools.patch
-
-:: Workaround for Nevow-0.10.0 requiring twisted.python when Twisted 12.1 is pinned ("Collecting Nevow<=0.10,>=0.9.33 [...] ImportError: No module named twisted.python")
-call pip install --upgrade twisted
+:: This should pull in pywin32 (via pypiwin32)
+call pip install --upgrade pyinstaller
 
 :: Build and install pyOpenSSL
 call "C:\Users\%USERNAME%\AppData\Local\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat" x86_amd64
@@ -29,15 +24,13 @@ call python setup.py build
 call python setup.py install
 call popd
 
+call git clone https://github.com/tahoe-lafs/tahoe-lafs.git .\build\tahoe-lafs
+
+:: Needed for frozen builds...
+call git apply disable_setuptools.patch
+
 call pip install --upgrade .\build\tahoe-lafs
 
-:: Needed to pass autodeps/init sequence when running frozen ("PackagingError: We require Twisted >= 13.0.0, but could only find version 12.1.0.")
-call pip install --upgrade twisted
-
-:: Same as above ("PackagingError: We require Nevow >= 0.11.1, but could only find version 0.10.0.")
-call pip install --upgrade nevow
-
-call pip install --upgrade pyinstaller
 call set PYTHONHASHSEED=1
 call pyinstaller --noconfirm tahoe.spec
 call python -m zipfile -c dist\Tahoe-LAFS.zip dist\Tahoe-LAFS
