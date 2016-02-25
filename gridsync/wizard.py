fe802285cf2612c5761394808a8cb3a4e15c7e27
Chris Wood <crwood@gmail.com>
2016-02-25 01:49:50 -0500

    Update wizard; enable storage providers, validate pages

--- gridsync/wizard.py
+++ gridsync/wizard.py
@@ -1,154 +1,160 @@
 # -*- coding: utf-8 -*-
 
-import logging
 import os
 import sys
 
-from PyQt5.QtGui import QFont, QPixmap 
-from PyQt5.QtWidgets import (QFileDialog, QHBoxLayout, QLabel, QLineEdit,
-        QMessageBox, QPushButton, QVBoxLayout, QWidget, QWizard, QWizardPage)
+from PyQt5.QtWidgets import (
+    QApplication, QLabel, QMessageBox, QVBoxLayout, QWizard, QWizardPage)
 
-from gridsync.tahoe import DEFAULT_SETTINGS, decode_introducer_furl
+from gridsync.widgets import GridSelector, FolderSelector
+
+
+class WelcomePage(QWizardPage):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setTitle("Welcome to Gridsync!")
+
+        label = QLabel(
+            "Gridsync is an experimental desktop client for for Tahoe-LAFS, "
+            "the Least Authority File Store. Unlike most traditional 'cloud' "
+            "services, any data stored inside a Tahoe-LAFS storage grid is "
+            "safe and secure by default; nobody can read or alter the files "
+            "stored in a grid without your permission -- not even the owners "
+            "of the computers that host them.\n\n"
+            #If you already have an invite code "
+            #"to join a grid, you can enter it on the following page. If not, "
+            "This setup wizard will guide you through the process of "
+            "selecting a storage grid so that you can begin to securely "
+            "store and retrieve your files.")
+        label.setWordWrap(True)
+
+        #radio_1 = QRadioButton("I already have an invite code.")
+        #radio_2 = QRadioButton("I do not have an invite code.")
+
+        vbox = QVBoxLayout(self)
+        vbox.addWidget(label)
+
+
+class SelectGridPage(QWizardPage):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.introducer_furl = None
+
+        self.grid_selector = GridSelector()
+
+        help_text = QLabel(
+            "A grid is a collection of computers that provide file storage "
+            "services. In future versions, Gridsync will allow you to "
+            "set up your own storage grid and invite others to join.")
+        help_text.setWordWrap(True)
+
+        vbox = QVBoxLayout(self)
+        vbox.addWidget(self.grid_selector)
+        vbox.addWidget(help_text)
+
+    def validatePage(self):
+        if self.grid_selector.introducer_furl:
+            self.introducer_furl = self.grid_selector.introducer_furl
+            return True
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setWindowTitle("Gridsync")
+            msg.setText("Please select a storage grid.")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec_()
+            return False
+
+
+#class DriveSharePage(QWizardPage):
+#    def __init__(self):
+#	super(self.__class__, self).__init__()
+#       self.drive_share = False
+
+
+class SelectFolderPage(QWizardPage):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.folder = None
+        default_path = os.path.join(os.path.expanduser('~'), 'Gridsync')
+
+        self.folder_selector = FolderSelector()
+        self.folder_selector.line_edit.setText(default_path)
+        self.folder_selector.folder = default_path
+
+        help_text = QLabel(
+            "The folder you select will be securely backed up into the "
+            "storage grid chosen on the previous page. In future versions, "
+            "Gridsync will allow you to share folders with other users.")
+        help_text.setWordWrap(True)
+
+        vbox = QVBoxLayout(self)
+        vbox.addWidget(self.folder_selector)
+        vbox.addWidget(help_text)
+
+    def validatePage(self):
+        self.folder = self.folder_selector.folder
+        if self.folder:
+            return True
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setWindowTitle("Gridsync")
+            msg.setText("Please select a folder.")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec_()
+            return False
+
+
+class FinishedPage(QWizardPage):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setTitle("Setup complete!")
+
+        verb = ('Done' if sys.platform == 'darwin' else 'Finish')
+
+        text = QLabel(
+            "By clicking {} below, Gridsync will synchronize your selected "
+            "folder with the storage grid and will continue to keep your "
+            "files backed up so long as it is running.\n\nPlease note that, "
+            "depending on how many files you have, your initial sync may "
+            "take a while. Gridsync will notify you when this process "
+            "has completed.".format(verb))
+        text.setWordWrap(True)
+
+        vbox = QVBoxLayout(self)
+        vbox.addWidget(text)
 
 
 class Wizard(QWizard):
-    def __init__(self, parent):
-        super(Wizard, self).__init__()
-        self.parent = parent
-        self.create_welcome_page()
-        self.create_systray_page()
-        self.create_configuration1_page()
-        self.create_configuration2_page()
-        self.create_finish_page()
-        self.selected_folder = ''
+    def __init__(self):
+        super(self.__class__, self).__init__()
         self.setWindowTitle("Gridsync - Welcome")
+        self.resize(800, 500)
+
+        self.welcome_page = WelcomePage()
+        self.select_grid_page = SelectGridPage()
+        self.select_folder_page = SelectFolderPage()
+        self.finished_page = FinishedPage()
+
+        self.addPage(self.welcome_page)
+        self.addPage(self.select_grid_page)
+        self.addPage(self.select_folder_page)
+        self.addPage(self.finished_page)
+
         self.finished.connect(self.finish)
 
-    def finish(self):
-        introducer_furl = str(self.introducer_furl.text())
-        settings = { introducer_furl: DEFAULT_SETTINGS }
-        settings[introducer_furl]['sync'] = self.selected_folder
-        logging.info(settings)
-        self.parent.settings = settings
-
-    def create_welcome_page(self):
-        page = QWizardPage()
-        page.setTitle("Welcome to Gridsync!")
-
-        label = QLabel('Gridsync is an experimental desktop client for for Tahoe-LAFS, the Least Authority File Store. Tahoe-LAFS allows you to safely store all of your important files into a storage "grid" -- a distributed cluster of servers connected to the Internet.\n\nUnlike most other traditional "Cloud" services, any data stored by Tahoe-LAFS is safe and secure by default; nobody can read or alter the files stored in your grid without your permission -- not even the owners of the servers that store them.')
-        label.setWordWrap(True)
-
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-        page.setLayout(layout)
-        
-        self.addPage(page)
-
-
-    def create_systray_page(self):
-        page = QWizardPage()
-        img = QLabel()
-        pixmap = QPixmap(":osx-prefs.png")
-        img.setPixmap(pixmap)
-
-        label = QLabel('Like many other popular applications, Gridsync runs in the background of your computer and works while you do. If you ever need to change your settings in the future, you can do so by clicking the Gridsync icon in your system tray.')
-        label.setWordWrap(True)
-        layout = QVBoxLayout()
-        layout.addWidget(img)
-        layout.addWidget(label)
-        page.setLayout(layout)
-        self.addPage(page)
-
-
-    def get_folder(self):
-        self.folder = QFileDialog.getExistingDirectory(
-                self, "Select local folder to sync")
-        if self.folder:
-            self.folder_text.setText(self.folder)
-            self.selected_folder = str(self.folder)
-
-
-    def create_configuration1_page(self):
-        page = QWizardPage()
-        page.setTitle("Gridsync configuration... in two easy steps")
-
-        label = QLabel('<b>Step 1.</b> Paste a Tahoe-LAFS Introducer fURL below:\n\n ')
-        label.setWordWrap(True)
-
-        self.introducer_furl = QLineEdit("")
-        
-        info_label = QLabel('\n\n\n\nAn "Introducer fURL" (in the form "<i>pb://nodeid@example.org/introducer</i>") is a special address that points to a Tahoe-LAFS storage grid. If you do not have an Introducer fURL, you can get one from the operator of a storage grid -- or from one of your friends that uses Tahoe-LAFS.')
-        info_label.setWordWrap(True)
-
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-        layout.addWidget(self.introducer_furl)
-        layout.addWidget(info_label)
-        
-        page.setLayout(layout)
-        self.addPage(page)
-
-
-    def create_configuration2_page(self):
-        page = QWizardPage()
-        page.setTitle("Gridsync configuration... in two easy steps")
-
-        label = QLabel('<b>Step 2.</b> Select a local folder:\n\n ')
-        label.setWordWrap(True)
-
-        hbox = QWidget(self)
-        hlayout = QHBoxLayout(hbox)
-        self.folder_text = QLineEdit(hbox)
-        button = QPushButton('Select Folder', self)
-        button.clicked.connect(self.get_folder)
-        hlayout.addWidget(self.folder_text)
-        hlayout.addWidget(button)
-        #self.folder_label = QLabel()
-        
-        warning_label = QLabel('\n\n\n\nGridsync works by pairing directories on your computer with directories located on a storage grid. The folder you select here will be automatically synchronized with the address provided on the previous page.')
-        warning_label.setWordWrap(True)
-
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-        #layout.addWidget(button)
-        layout.addWidget(hbox)
-        layout.addWidget(warning_label)
-
-
-        page.setLayout(layout)
-        self.addPage(page)
-
-    def create_finish_page(self):
-        page = QWizardPage()
-        page.setTitle("That's it!")
-        
-        #self.settings = self.gridsync_link
-        text = 'By clicking <b>Done</b> below, Gridsync will begin synchronizing your selected folder with your storage grid and will continue to keep your files in sync so long as it is running.'
-        label = QLabel(text)
-        label.setWordWrap(True)
-        
-        img = QLabel()
-        pixmap = QPixmap(":sync-complete.png")
-        img.setPixmap(pixmap)
-
-        warning_label = QLabel('\n\n\n\nPlease note that, depending on how many files you have, your initial sync may take a while. When Gridsync is working, the system tray icon will animate and you will receive a desktop notification as soon as the process completes.')
-        warning_label.setWordWrap(True)
-
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-        layout.addWidget(img)
-        layout.addWidget(warning_label)
-        page.setLayout(layout)
-
-        self.addPage(page)
-
-
     def closeEvent(self, event):
-        reply = QMessageBox.question(self, 'Message',
-            "Gridsync has not yet been configured. Are you sure you wish to quit?", QMessageBox.Yes | 
-            QMessageBox.No, QMessageBox.No)
+        reply = QMessageBox.question(self, 'Exit Gridsync setup?',
+                "Gridsync has not yet been configured. "
+                "Are you sure you wish to quit?",
+                QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 
         if reply == QMessageBox.Yes:
             event.accept()
         else:
-            event.ignore() 
+            event.ignore()
+
+    def finish(self):
+        self.introducer_furl = self.select_grid_page.introducer_furl
+        self.folder = self.select_folder_page.folder
