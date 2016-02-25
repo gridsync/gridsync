8b18ac6d25a3a0ed08dfa35ede70b6cbbba343fb
Chris Wood <crwood@gmail.com>
2016-02-25 01:48:05 -0500

    Update widgets, cleanup

--- gridsync/widgets.py
+++ gridsync/widgets.py
@@ -1,166 +1,215 @@
 # -*- coding: utf-8 -*-
 
-import sys
-
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QComboBox, QFormLayout, QHBoxLayout, QLabel, QLineEdit, QPlainTextEdit, QPushButton, QSizePolicy, QSpacerItem, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import (
+        QComboBox, QFileDialog, QFormLayout, QGroupBox, QHBoxLayout,
+        QLabel, QLineEdit, QMessageBox, QPlainTextEdit, QPushButton,
+        QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
 
 from gridsync.providers import get_storage_providers, add_storage_provider
 from gridsync.tahoe import decode_introducer_furl
-from gridsync import resources
 
 
-class GridSelector(QWidget):
+class GridComboBox(QWidget):
     def __init__(self):
         super(self.__class__, self).__init__()
-        self.layout = QHBoxLayout(self)
 
-        left_spacer = QSpacerItem(
-                100, 0, QSizePolicy.Preferred, QSizePolicy.Minimum)
-        self.layout.addItem(left_spacer)
-
-        self.label = QLabel('Storage provider:')
-        self.label.setSizePolicy(
-                QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Preferred))
-        self.layout.addWidget(self.label)
+        #self.label = QLabel('Storage provider:')
+        #self.label.setSizePolicy(QSizePolicy(QSizePolicy.Maximum, 0))
 
         self.combo_box = QComboBox(self)
-        self.storage_providers = get_storage_providers()
-        self.combo_box.addItem("--")
-        for name in sorted(self.storage_providers.keys(), reverse=True):
-            self.combo_box.addItem(name)
-        self.combo_box.insertSeparator(self.combo_box.count())
-        self.combo_box.addItem("Add new...")
-        self.layout.addWidget(self.combo_box)
 
-        right_spacer = QSpacerItem(
-                100, 0, QSizePolicy.Preferred, QSizePolicy.Minimum)
-        self.layout.addItem(right_spacer)
+        hbox = QHBoxLayout(self)
+        hbox.addItem(QSpacerItem(100, 0, QSizePolicy.Preferred, 0))
+        #hbox.addWidget(self.label)
+        hbox.addWidget(self.combo_box)
+        hbox.addItem(QSpacerItem(100, 0, QSizePolicy.Preferred, 0))
 
 
 class GridDescription(QWidget):
     def __init__(self):
         super(self.__class__, self).__init__()
-        self.layout = QFormLayout(self)
 
-        self.image_box = QLabel()
-        self.layout.setWidget(0, QFormLayout.LabelRole, self.image_box)
+        self.image = QLabel()
 
-        self.label = QLabel('', self)
-        self.label.setWordWrap(True)
-        self.label.setText('')
-        self.label.setOpenExternalLinks(True)
-        self.layout.setWidget(0, QFormLayout.FieldRole, self.label)
+        self.description = QLabel('', self)
+        self.description.setWordWrap(True)
+        self.description.setOpenExternalLinks(True)
 
-        self.image_box.hide()
-        self.label.linkActivated.connect(self.clicked)
+        form = QFormLayout()
+        form.setWidget(0, QFormLayout.LabelRole, self.image)
+        form.setWidget(0, QFormLayout.FieldRole, self.description)
 
-    def clicked(self, link):
-        from PyQt5.QtGui import QDesktopServices
-        from PyQt5.QtCore import QUrl
-        QDesktopServices.openUrl(QUrl(link))
+        hbox = QHBoxLayout(self)
+        hbox.addItem(QSpacerItem(30, 0, QSizePolicy.Preferred, 0))
+        hbox.addLayout(form)
+        hbox.addItem(QSpacerItem(30, 0, QSizePolicy.Preferred, 0))
 
-class NewGridForm(QWidget):
+
+class GridForm(QWidget):
     def __init__(self):
         super(self.__class__, self).__init__()
-        self.layout = QFormLayout(self)
 
         self.name_label = QLabel("Name (optional):")
-        self.layout.setWidget(0, QFormLayout.LabelRole, self.name_label)
 
         self.name_line_edit = QLineEdit()
-        self.layout.setWidget(0, QFormLayout.FieldRole, self.name_line_edit)
 
         self.introducer_label = QLabel("Introducer fURL:")
-        self.layout.setWidget(
-                1, QFormLayout.LabelRole, self.introducer_label)
 
         self.introducer_text_edit = QPlainTextEdit()
-        self.layout.setWidget(
-                1, QFormLayout.FieldRole, self.introducer_text_edit)
         self.introducer_text_edit.setMaximumHeight(70)
         self.introducer_text_edit.setTabChangesFocus(True)
 
-        #self.description_label = QLabel("Description (optional):")
-        #self.layout.setWidget(
-        #        2, QFormLayout.LabelRole, self.description_label)
+        self.description_label = QLabel("Description (optional):")
 
-        #self.description_text_edit = QPlainTextEdit()
-        #self.layout.setWidget(
-        #        2, QFormLayout.FieldRole, self.description_text_edit)
-        #self.description_text_edit.setMaximumHeight(70)
-        #self.description_text_edit.setTabChangesFocus(True)
+        self.description_text_edit = QPlainTextEdit()
+        self.description_text_edit.setMaximumHeight(70)
+        self.description_text_edit.setTabChangesFocus(True)
 
         self.push_button = QPushButton("Save")
-        self.layout.setWidget(3, QFormLayout.FieldRole, self.push_button)
+
+        form = QFormLayout()
+        form.setWidget(0, QFormLayout.LabelRole, self.name_label)
+        form.setWidget(0, QFormLayout.FieldRole, self.name_line_edit)
+        form.setWidget(1, QFormLayout.LabelRole, self.introducer_label)
+        form.setWidget(1, QFormLayout.FieldRole, self.introducer_text_edit)
+        #form.setWidget(2, QFormLayout.LabelRole, self.description_label)
+        #form.setWidget(2, QFormLayout.FieldRole, self.description_text_edit)
+        form.setWidget(3, QFormLayout.FieldRole, self.push_button)
+
+        hbox = QHBoxLayout(self)
+        #hbox.addItem(QSpacerItem(100, 0, QSizePolicy.Preferred, 0))
+        hbox.addLayout(form)
+        #hbox.addItem(QSpacerItem(100, 0, QSizePolicy.Preferred, 0))
 
 
-class StorageProviderSelector(QWidget):
+class GridSelector(QWidget):
     def __init__(self):
         super(self.__class__, self).__init__()
-        self.layout = QVBoxLayout(self)
         self.resize(800, 500)
+        self.introducer_furl = None
 
-        self.grid_selector = GridSelector()
-        self.layout.addWidget(self.grid_selector)
+        self.grid_combo_box = GridComboBox()
+        self.grid_combo_box.combo_box.activated[str].connect(self.on_selected)
 
         self.grid_description = GridDescription()
-        self.layout.addWidget(self.grid_description)
+        self.grid_description.hide()
 
-        self.new_grid_form = NewGridForm()
-        self.layout.addWidget(self.new_grid_form)
+        self.grid_form = GridForm()
+        self.grid_form.push_button.clicked.connect(self.on_save)
+        self.grid_form.hide()
 
-        self.spacer = QSpacerItem(
-                1, 0, QSizePolicy.Preferred, QSizePolicy.Expanding)
-        self.layout.addItem(self.spacer)
+        gbox = QGroupBox(self)
+        gbox.setTitle("Select a storage grid:")
+        gbox_layout = QVBoxLayout(gbox)
+        gbox_layout.addWidget(self.grid_combo_box)
+        gbox_layout.addWidget(self.grid_description)
+        gbox_layout.addWidget(self.grid_form)
+        #gbox_layout.addItem(QSpacerItem(0, 0, 0, QSizePolicy.Expanding))
 
-        self.new_grid_form.hide()
-        self.grid_selector.combo_box.activated[str].connect(self.on_selected)
-        self.new_grid_form.push_button.clicked.connect(self.on_save)
+        vbox = QVBoxLayout(self)
+        #vbox.addItem(QSpacerItem(0, 60, 0, QSizePolicy.Minimum))
+        #vbox.addWidget(self.grid_selector)
+        #vbox.addWidget(self.grid_description)
+        #vbox.addWidget(self.new_grid_form)
+        #vbox.addItem(QSpacerItem(0, 0, 0, QSizePolicy.Expanding))
+        vbox.addWidget(gbox)
+
+        self.combo_box = self.grid_combo_box.combo_box
+        self.image = self.grid_description.image
+        self.description = self.grid_description.description
+
+        self.populate_combo_box()
+
+    def populate_combo_box(self):
+        self.storage_providers = get_storage_providers()
+        self.combo_box.clear()
+        self.combo_box.addItem(" ")
+        for name in sorted(self.storage_providers.keys(), reverse=True):
+            self.combo_box.addItem(name)
+        self.combo_box.insertSeparator(
+                self.combo_box.count())
+        self.combo_box.addItem("Add new...")
 
     def on_selected(self, name):
-        if name == "Add new...":
+        if self.combo_box.itemText(0) == " ":
+            self.combo_box.removeItem(0)
+        if name == " ":
+            return
+        elif name == "Add new...":
             self.grid_description.hide()
-            self.new_grid_form.show()
+            self.grid_form.show()
         else:
-            if self.grid_selector.combo_box.itemText(0) == "--":
-                self.grid_selector.combo_box.removeItem(0)
-            provider = self.grid_selector.storage_providers[name]
+            provider = self.storage_providers[name]
+            self.introducer_furl = provider['introducer.furl']
             description = provider['description']
             try:
                 description += '<p>Homepage: <a href="{}">{}</a>'.format(
                         provider['homepage'], provider['homepage'])
             except KeyError:
                 pass
-            self.grid_description.label.setText(description)
+            self.description.setText(description)
             try:
                 pixmap = QPixmap(provider['logo']).scaled(64, 64)
-                self.grid_description.image_box.setPixmap(pixmap)
-                self.grid_description.image_box.show()
+                self.image.setPixmap(pixmap)
+                self.image.show()
             except KeyError:
-                self.grid_description.image_box.hide()
-            self.new_grid_form.hide()
+                self.image.hide()
+            self.grid_form.hide()
             self.grid_description.show()
-    
+
     def on_save(self):
-        introducer_furl = self.new_grid_form.introducer_text_edit.toPlainText()
-        #name = self.new_grid_form.name_line_edit.text()
-        #description = self.new_grid_form.description_text_edit.toPlainText()
+        introducer_furl = self.grid_form.introducer_text_edit.toPlainText()
+        name = self.grid_form.name_line_edit.text()
         if not introducer_furl:
-            # TODO: QMessageBox
-            print('no introducer')
-        try:
-            _, connection_hints = decode_introducer_furl(introducer_furl)
-        except AttributeError:
-            # TODO: QMessageBox
-            print('invalid furl')
-        #print(introducer_furl)
-        #print(name)
-        #print(description)
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Warning)
+            msg.setWindowTitle("Gridsync")
+            msg.setText("Please enter an Introducer fURL.")
+            msg.setStandardButtons(QMessageBox.Ok)
+            msg.exec_()
+        else:
+            try:
+                _, connection_hints = decode_introducer_furl(introducer_furl)
+            except AttributeError:
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Warning)
+                msg.setWindowTitle("Gridsync")
+                msg.setText("Please enter a valid Introducer fURL.")
+                msg.setStandardButtons(QMessageBox.Ok)
+                msg.exec_()
+            add_storage_provider(introducer_furl, name)
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    win = StorageProviderSelector()
-    win.show()
-    sys.exit(app.exec_())
+class FolderSelector(QWidget):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.folder = None
+
+        self.line_edit = QLineEdit()
+
+        self.button = QPushButton()
+        self.button.setText("Browse...")
+        self.button.clicked.connect(self.on_clicked)
+
+        gbox = QGroupBox(self)
+        gbox.setTitle("Select a folder to sync:")
+        gbox_layout = QHBoxLayout(gbox)
+        gbox_layout.addItem(QSpacerItem(100, 0, QSizePolicy.Preferred, 0))
+        gbox_layout.addWidget(self.line_edit)
+        gbox_layout.addWidget(self.button)
+        gbox_layout.addItem(QSpacerItem(100, 0, QSizePolicy.Preferred, 0))
+
+        hbox = QHBoxLayout(self)
+        hbox.addWidget(gbox)
+        #hbox.addItem(QSpacerItem(100, 0, QSizePolicy.Preferred, 0))
+        #hbox.addWidget(self.line_edit)
+        #hbox.addWidget(self.button)
+        #hbox.addItem(QSpacerItem(100, 0, QSizePolicy.Preferred, 0))
+
+    def on_clicked(self):
+        selected_folder = QFileDialog.getExistingDirectory(
+                self, "Select a folder to sync")
+        if selected_folder:
+            self.line_edit.setText(selected_folder)
+            self.folder = selected_folder
