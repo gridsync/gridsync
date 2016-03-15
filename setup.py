e360f62aeac0a8c38049242b834226190f60c56a
Chris Wood <crwood@gmail.com>
2016-03-14 21:39:14 -0400

    Remove pytest TestCommand; use tox

--- setup.py
+++ setup.py
@@ -8,24 +8,6 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
-
 class Tox(TestCommand):
     user_options = [('tox-args=', 'a', "Arguments to pass to tox")]
 
@@ -111,8 +93,6 @@ setup(
     },
     install_requires=requirements,
     test_suite="tests",
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
-    #tests_require=['tox'],
-    #cmdclass={'test': Tox},
+    tests_require=['tox'],
+    cmdclass={'test': Tox},
 )
