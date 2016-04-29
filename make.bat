374abfac8cb83ffad870a304d847755baabc3aa3
Chris Wood <crwood@gmail.com>
2016-04-29 16:37:03 -0400

    Update Windows package to python 3.5 and PyQt 5.6

--- make.bat
+++ make.bat
@@ -4,11 +4,7 @@
 ::
 ::      Python 2.7: https://www.python.org/
 ::      Microsoft Visual C++ Compiler for Python 2.7: https://www.microsoft.com/en-us/download/details.aspx?id=44266
-::      Git: https://git-scm.com/download/win
-::          (select "Use Git from the Windows Command Prompt" and "Checkout
-::          as-is" in setup wizard)
-::      PyQt5 (currently 5.5.1): https://www.riverbankcomputing.com/software/pyqt/download5
-::      Python 3.4 (PyQt5-5.5.1 requires Python 3.4, not 3.5): https://www.python.org/
+::      Python 3.5: https://www.python.org/
 ::
 
 @echo off
@@ -56,12 +52,13 @@ if exist .\dist\Tahoe-LAFS.zip (
     call :frozen-tahoe
 )
 call .\dist\Tahoe-LAFS\tahoe.exe --version-and-path
-call C:\Python34\python.exe -m pip install --upgrade --editable .
-call C:\Python34\python.exe -m pip install --upgrade pyinstaller
+call py -3.5 -m pip install --upgrade pip virtualenv
+call py -3.5 -m virtualenv --clear .\build\venv-gridsync
+call .\build\venv-gridsync\Scripts\activate
+call pip install pyqt5 . pyinstaller
 call set PYTHONHASHSEED=1
-call C:\Python34\Scripts\pyinstaller.exe --windowed --icon=images\gridsync.ico --name=Gridsync gridsync\cli.py
-call set PYTHONHASHSEED=
+call pyinstaller --windowed --icon=images\gridsync.ico --name=Gridsync gridsync\cli.py
 call move dist\Tahoe-LAFS dist\Gridsync\Tahoe-LAFS
-call C:\Python27\python.exe -m zipfile -c dist\Gridsync.zip dist\Gridsync
+call python -m zipfile -c dist\Gridsync.zip dist\Gridsync
+call deactivate
 goto :eof
-
