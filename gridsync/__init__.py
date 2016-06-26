59c9b932a014c3d1279ad3c0f85f6352776806ae
Chris Wood <crwood@gmail.com>
2016-06-25 23:56:30 -0400

    Add 'pkgdir' var, tests

--- gridsync/__init__.py
+++ gridsync/__init__.py
@@ -1,5 +1,6 @@
 """Synchronize local directories with Tahoe-LAFS storage grids."""
 
+import os
 import sys
 
 from gridsync import _version
@@ -12,7 +13,10 @@ __version__ = _version.__version__
 
 
 if getattr(sys, 'frozen', False):
+    pkgdir = os.path.dirname(os.path.realpath(sys.executable))
     try:
         del sys.modules['twisted.internet.reactor']  # PyInstaller workaround
     except KeyError:
         pass
+else:
+    pkgdir = os.path.dirname(os.path.realpath(__file__))
--- tests/test___init__.py
+++ tests/test___init__.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import os
 import sys
 if sys.version_info >= (3, 4):
     from importlib import reload
@@ -9,14 +12,26 @@ def test_the_approval_of_RMS():  # :)
     assert gridsync.__license__.startswith('GPL')
 
 
-def test_frozen_init_del_reactor(monkeypatch):
+def test_pkgdir(monkeypatch):
+    monkeypatch.setattr("sys.frozen", False, raising=False)
+    assert gridsync.pkgdir == os.path.dirname(
+        os.path.realpath(gridsync.__file__))
+
+
+def test_frozen_pkgdir(monkeypatch):
+    monkeypatch.setattr("sys.frozen", True, raising=False)
+    reload(gridsync)
+    assert gridsync.pkgdir == os.path.dirname(os.path.realpath(sys.executable))
+
+
+def test_frozen_del_reactor(monkeypatch):
     monkeypatch.setattr("sys.frozen", True, raising=False)
     sys.modules['twisted.internet.reactor'] = 'test'
     reload(gridsync)
     assert 'twisted.internet.reactor' not in sys.modules
 
 
-def test_frozen_init_del_reactor_pass_without_twisted(monkeypatch):
+def test_frozen_del_reactor_pass_without_twisted(monkeypatch):
     monkeypatch.setattr("sys.frozen", True, raising=False)
     reload(gridsync)
     assert 'twisted.internet.reactor' not in sys.modules
