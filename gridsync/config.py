4dca8bdec1aa78e9ac4d8d21784afe7834b32235
Chris Wood <chris@gridsync.io>
2017-01-09 20:41:28 -0500

    Add Config() helper class for parsing INI files

--- gridsync/config.py
+++ gridsync/config.py
@@ -1,8 +1,49 @@
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
+try:
+    from configparser import RawConfigParser
+except ImportError:
+    from ConfigParser import RawConfigParser  # pylint: disable=import-error
 import logging
 import os
 import yaml
+
+
+class Config(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.config = RawConfigParser(allow_no_value=True)
+
+    def set(self, section, option, value):
+        self.config.read(self.filename)
+        if not self.config.has_section(section):
+            self.config.add_section(section)
+        self.config.set(section, option, value)
+        with open(self.filename, 'w') as f:
+            self.config.write(f)
+
+    def get(self, section, option):
+        self.config.read(self.filename)
+        return self.config.get(section, option)
+
+    def save(self, settings_dict):
+        self.config.read(self.filename)
+        for section, d in settings_dict.items():
+            if not self.config.has_section(section):
+                self.config.add_section(section)
+            for option, value in d.items():
+                self.config.set(section, option, value)
+        with open(self.filename, 'w') as f:
+            self.config.write(f)
+
+    def load(self):
+        self.config.read(self.filename)
+        settings_dict = defaultdict(dict)
+        for section in self.config.sections():
+            for option, value in self.config.items(section):
+                settings_dict[section][option] = value
+        return settings_dict
 
 
 class YamlConfig(object):
--- tests/test_config.py
+++ tests/test_config.py
@@ -2,23 +2,46 @@
 
 import os
 
-from gridsync.config import YamlConfig
+from gridsync.config import Config, YamlConfig
 
 
-def test_yaml_config_file():
-    yaml_config = YamlConfig('test')
-    assert yaml_config.filename == 'test'
+def test_config_set(tmpdir):
+    config = Config(os.path.join(str(tmpdir), 'test_set.ini'))
+    config.set('test_section', 'test_option', 'test_value')
+    with open(config.filename) as f:
+        assert f.read() == '[test_section]\ntest_option = test_value\n\n'
 
 
-def test_save(tmpdir):
-    yaml_config = YamlConfig(os.path.join(str(tmpdir), 'test.yml'))
+def test_config_get(tmpdir):
+    config = Config(os.path.join(str(tmpdir), 'test_get.ini'))
+    with open(config.filename, 'w') as f:
+        f.write('[test_section]\ntest_option = test_value\n\n')
+    assert config.get('test_section', 'test_option') == 'test_value'
+
+
+def test_config_save(tmpdir):
+    config = Config(os.path.join(str(tmpdir), 'test_save.ini'))
+    config.save({'test_section': {'test_option': 'test_value'}})
+    with open(config.filename) as f:
+        assert f.read() == '[test_section]\ntest_option = test_value\n\n'
+
+
+def test_config_load(tmpdir):
+    config = Config(os.path.join(str(tmpdir), 'test_load.ini'))
+    with open(config.filename, 'w') as f:
+        f.write('[test_section]\ntest_option = test_value\n\n')
+    config.load() == {'test_section': {'test_option': 'test_value'}}
+
+
+def test_yaml_config_save(tmpdir):
+    yaml_config = YamlConfig(os.path.join(str(tmpdir), 'test_save.yml'))
     yaml_config.save({'test': 'test'})
     with open(yaml_config.filename) as f:
         assert f.read() == 'test: test\n'
 
 
-def test_load(tmpdir):
-    yaml_config = YamlConfig(os.path.join(str(tmpdir), 'test.yml'))
+def test_yaml_config_load(tmpdir):
+    yaml_config = YamlConfig(os.path.join(str(tmpdir), 'test_load.yml'))
     with open(yaml_config.filename, 'w') as f:
         f.write('test: test\n')
     assert yaml_config.load() == {'test': 'test'}
