b84a86cd4d7044747d9d9ccccaedeb3b710a5ca3
Chris Wood <chris@gridsync.io>
2017-01-27 16:34:33 -0500

    Add tests for config parser exceptions

--- tests/test_config.py
+++ tests/test_config.py
@@ -19,6 +19,20 @@ def test_config_get(tmpdir):
     assert config.get('test_section', 'test_option') == 'test_value'
 
 
+def test_config_get_no_section_error(tmpdir):
+    config = Config(os.path.join(str(tmpdir), 'test_get_no_section_error.ini'))
+    with open(config.filename, 'w') as f:
+        f.write('[test_section]\ntest_option = test_value\n\n')
+    assert config.get('missing_section', 'test_option') is None
+
+
+def test_config_get_no_option_error(tmpdir):
+    config = Config(os.path.join(str(tmpdir), 'test_get_no_option_error.ini'))
+    with open(config.filename, 'w') as f:
+        f.write('[test_section]\ntest_option = test_value\n\n')
+    assert config.get('test_section', 'missing_option') is None
+
+
 def test_config_save(tmpdir):
     config = Config(os.path.join(str(tmpdir), 'test_save.ini'))
     config.save({'test_section': {'test_option': 'test_value'}})
