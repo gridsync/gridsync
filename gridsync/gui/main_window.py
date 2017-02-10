08bb37bb13a2bd95697c6c6bd396454a136eab72
Chris Wood <chris@gridsync.io>
2017-02-10 13:40:29 -0500

    Set combo box size policy to adjust to contents

--- gridsync/gui/main_window.py
+++ gridsync/gui/main_window.py
@@ -19,6 +19,7 @@ from gridsync.tahoe import get_nodedirs, Tahoe
 class ComboBox(QComboBox):
     def __init__(self):
         super(self.__class__, self).__init__()
+        self.setSizeAdjustPolicy(QComboBox.AdjustToContents)
 
     def populate(self, gateways):
         self.clear()
