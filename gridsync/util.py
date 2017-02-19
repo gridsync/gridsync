1b79db23834a92e7be5a86aedde0976a692ec695
Chris Wood <chris@gridsync.io>
2017-02-18 21:27:15 -0500

    Add humanized_list() function, tests

--- gridsync/util.py
+++ gridsync/util.py
@@ -17,3 +17,15 @@ def b2h(b):
         rounded = round(b / (1024 ** i), 2)
         if rounded < 1024:
             return "{} {}".format(rounded, unit)
+
+
+def humanized_list(list_, kind='files'):
+    if len(list_) == 1:
+        return list_[0]
+    elif len(list_) == 2:
+        return " and ".join(list_)
+    elif len(list_) == 3:
+        return "{}, {}, and {}".format(*list_)
+    else:
+        return "{}, {}, and {} other {}".format(list_[0], list_[1],
+                                                len(list_) - 2, kind)
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from gridsync.util import h2b, b2h
+from gridsync.util import h2b, b2h, humanized_list
 
 
 def test_1B_2b():
@@ -25,3 +25,23 @@ def test_1024_2h():
 
 def test_1_2h():
     assert b2h(1) == '1.0 B'
+
+
+def test_humanized_list_Alice():
+    result = humanized_list(['Alice'])
+    assert result == 'Alice'
+
+
+def test_humanized_list_Alice_Bob():
+    result = humanized_list(['Alice', 'Bob'])
+    assert result == 'Alice and Bob'
+
+
+def test_humanized_list_Alice_Bob_Eve():
+    result = humanized_list(['Alice', 'Bob', 'Eve'])
+    assert result == 'Alice, Bob, and Eve'
+
+
+def test_humanized_list_Alice_Bob_Eve_Mallory():
+    result = humanized_list(['Alice', 'Bob', 'Eve', 'Mallory'], 'characters')
+    assert result == 'Alice, Bob, and 2 other characters'
