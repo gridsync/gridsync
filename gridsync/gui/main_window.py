98628b050549fdef9a14a4d09b1adf9462562d6d
Chris Wood <chris@gridsync.io>
2017-02-20 13:17:27 -0500

    Notify user on file-updates (work-in-progress)

--- gridsync/gui/main_window.py
+++ gridsync/gui/main_window.py
@@ -2,6 +2,7 @@
 
 import logging
 import os
+from collections import defaultdict
 
 from PyQt5.QtWidgets import (
     QAbstractItemView, QAction, QComboBox, QFileIconProvider, QGridLayout,
@@ -11,12 +12,13 @@ from PyQt5.QtGui import (
     QFont, QIcon, QKeySequence, QStandardItem, QStandardItemModel)
 from PyQt5.QtCore import QFileInfo, QSize, Qt, QVariant
 from twisted.internet import reactor
-from twisted.internet.defer import inlineCallbacks, returnValue
+from twisted.internet.defer import inlineCallbacks
 from twisted.internet.task import LoopingCall
 
 from gridsync import resource, settings
 from gridsync.desktop import open_folder
 from gridsync.tahoe import get_nodedirs
+from gridsync.util import b2h, humanized_list
 
 
 class ComboBox(QComboBox):
@@ -42,38 +44,78 @@ class Monitor(object):
         self.model = model
         self.gateway = gateway
         self.gui = self.model.gui
-        self.status = ''
-        self.timer = LoopingCall(self.update_status)
-        self.magic_folders_status = {}
+        self.grid_status = ''
+        self.status = defaultdict(dict)
+        self.timer = LoopingCall(self.check_status)
 
-    @inlineCallbacks
-    def get_magic_folder_state(self, magic_folder):
-        status = yield magic_folder.get_magic_folder_status()
+    def add_updated_file(self, magic_folder, path):
+        if 'updated_files' not in self.status[magic_folder]:
+            self.status[magic_folder]['updated_files'] = []
+        if path in self.status[magic_folder]['updated_files']:
+            return
+        elif path.endswith('/') or path.endswith('~') or path.isdigit():
+            return
+        else:
+            self.status[magic_folder]['updated_files'].append(path)
+            logging.debug("Added %s to updated_files list", path)
+
+    def notify_updated_files(self, magic_folder):
+        if 'updated_files' in self.status[magic_folder]:
+            title = magic_folder.name + " folder updated"
+            message = "Updated " + humanized_list(
+                self.status[magic_folder]['updated_files'])
+            self.gui.show_message(title, message)
+            self.status[magic_folder]['updated_files'] = []
+            logging.debug("Cleared updated_files list")
+
+    def get_state_from_status(self, status):
         state = 0
         if status:
             for task in status:
                 if task['status'] == 'queued' or task['status'] == 'started':
-                    state = 1
-        returnValue(state)
+                    if not task['path'].endswith('/'):
+                        state = 1  # "Syncing"
+            if not state:
+                state = 2  # "Up to date"
+        return state
 
     @inlineCallbacks
-    def update_status(self):
+    def check_magic_folder_status(self, magic_folder):
+        status = yield magic_folder.get_magic_folder_status()
+        state = self.get_state_from_status(status)
+        prev = self.status[magic_folder]
+        if status and prev:
+            if prev['status'] and status != prev['status']:
+                for item in status:
+                    if item not in prev['status']:
+                        self.add_updated_file(magic_folder, item['path'])
+                size = yield magic_folder.get_magic_folder_size()
+                self.model.set_size(magic_folder.name, size)
+            if state == 2 and prev['state'] != 2:
+                self.notify_updated_files(magic_folder)
+                size = yield magic_folder.get_magic_folder_size()
+                self.model.set_size(magic_folder.name, size)
+        self.status[magic_folder]['status'] = status
+        self.status[magic_folder]['state'] = state
+        self.model.set_status(magic_folder.name, state)
+
+    @inlineCallbacks
+    def check_grid_status(self):
         num_connected = yield self.gateway.get_connected_servers()
         if not num_connected:
-            status = "Connecting..."
+            grid_status = "Connecting..."
         elif num_connected == 1:
-            status = "Connected to {} storage node".format(num_connected)
+            grid_status = "Connected to {} storage node".format(num_connected)
         else:
-            status = "Connected to {} storage nodes".format(num_connected)
-        if num_connected and status != self.status:
-            self.gui.show_message(self.gateway.name, status)
-        self.status = status
+            grid_status = "Connected to {} storage nodes".format(num_connected)
+        if num_connected and grid_status != self.grid_status:
+            self.gui.show_message(self.gateway.name, grid_status)
+        self.grid_status = grid_status
 
-        if self.gateway.magic_folders:
-            for magic_folder in self.gateway.magic_folders:
-                state = yield self.get_magic_folder_state(magic_folder)
-                self.magic_folders_status[magic_folder.name] = state
-            self.model.update(self.magic_folders_status)
+    def check_status(self):
+        self.check_grid_status()
+        for magic_folder in self.gateway.magic_folders:
+            self.check_magic_folder_status(magic_folder)
 
     def start(self, interval=2):
         self.timer.start(interval, now=True)
@@ -115,17 +157,26 @@ class Model(QStandardItemModel):
             self.add_folder(magic_folder)
         self.monitor.start()
 
-    def update(self, data):
-        for folder, state in data.items():
-            for row in range(self.rowCount()):
-                if folder == self.item(row, 0).text():
-                    if state == 0:
-                        icon = self.icon_up_to_date
-                        text = "Up to date"
-                    else:
-                        icon = self.icon_syncing
-                        text = "Syncing"
-                    self.setItem(row, 1, QStandardItem(icon, text))
+    def get_row_from_name(self, name):
+        for row in range(self.rowCount()):
+            if name == self.item(row).text():
+                return row
+
+    def set_status(self, name, status):
+        if not status:
+            icon = QIcon()
+            text = None
+        elif status == 1:
+            icon = self.icon_syncing
+            text = "Syncing"
+        else:
+            icon = self.icon_up_to_date
+            text = "Up to date"
+        self.setItem(
+            self.get_row_from_name(name), 1, QStandardItem(icon, text))
+
+    def set_size(self, name, size):
+        self.setItem(self.get_row_from_name(name), 2, QStandardItem(b2h(size)))
 
 
 class View(QTreeView):
@@ -267,7 +318,7 @@ class MainWindow(QMainWindow):
         return self.central_widget.currentWidget().layout().itemAt(0).widget()
 
     def get_current_grid_status(self):
-        return self.current_widget().model().monitor.status
+        return self.current_widget().model().monitor.grid_status
 
     def set_current_grid_status(self):
         self.status_bar_label.setText(self.get_current_grid_status())
