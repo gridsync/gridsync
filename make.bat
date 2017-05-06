a8ac7b8dc1bb9e5ff2c22cf58391d391205cd494
Chris Wood <chris@gridsync.io>
2017-05-06 17:40:42 -0400

    Update Windows build script

--- make.bat
+++ make.bat
@@ -1,19 +1,38 @@
 ::
-:: This script assumes that the following dependencies have already been 
-:: downloaded and properly installed:
+:: This script assumes that the following dependencies have been installed:
 ::
-::      Python 2.7: https://www.python.org/
-::      Microsoft Visual C++ Compiler for Python 2.7: https://www.microsoft.com/en-us/download/details.aspx?id=44266
-::      Python 3.5: https://www.python.org/
+::  1) Git <https://git-scm.com/download/win>
+::
+::  2) Python 2.7 <https://www.python.org>
+::
+::  3) Python 3.5 (not 3.6; see PyInstaller#2331) <https://www.python.org>
+::
+::  4) Microsoft Visual C++ Compiler for Python 2.7 <https://aka.ms/vcpython27>
+::
+::  5) Microsoft .NET Framework <https://www.microsoft.com/net/download/framework>
+::
+::  6) Microsoft Visual C++ Build Tools 2015 <http://landinghub.visualstudio.com/visual-cpp-build-tools>
+::      -Select "Windows 8.1 SDK" and "Windows 10 SDK" options during install
 ::
 
 @echo off
 
+if "%1"=="clean" call :clean
 if "%1"=="test" call :test
 if "%1"=="pytest" call :pytest
 if "%1"=="frozen-tahoe" call :frozen-tahoe
 if "%1"=="all" call :all
 if "%1"=="" call :all
+goto :eof
+
+:clean
+call rmdir /s /q .\build
+call rmdir /s /q .\dist
+call rmdir /s /q .\.eggs
+call rmdir /s /q .\.cache
+call rmdir /s /q .\.tox
+call rmdir /s /q .\htmlcov
+call del .\.coverage
 goto :eof
 
 :test
@@ -25,18 +44,18 @@ call python -m pytest || exit /b 1
 goto :eof
 
 :frozen-tahoe
-call mkdir .\build
+call py -2.7 -m pip install --upgrade setuptools pip virtualenv
+call py -2.7 -m virtualenv --clear .\build\venv-tahoe
+call .\build\venv-tahoe\Scripts\activate
 ::call powershell -Command "(New-Object Net.WebClient).DownloadFile('https://tahoe-lafs.org/downloads/tahoe-lafs-1.11.0.zip', '.\build\tahoe-lafs.zip')"
 ::call C:\Python27\python.exe -m zipfile -e .\build\tahoe-lafs.zip .\build
 ::call move .\build\tahoe-lafs-1.11.0 .\build\tahoe-lafs
+call pip install --upgrade setuptools pip
 call git clone https://github.com/tahoe-lafs/tahoe-lafs.git .\build\tahoe-lafs
-call C:\Python27\python.exe -m pip install --upgrade virtualenv
-call C:\Python27\python.exe -m virtualenv --clear .\build\venv-tahoe
-call .\build\venv-tahoe\Scripts\activate
 call copy .\misc\tahoe.spec .\build\tahoe-lafs
 call pushd .\build\tahoe-lafs
-call C:\Python27\python.exe setup.py update_version
-call pip install --find-links=https://tahoe-lafs.org/deps/ .
+call python setup.py update_version
+call pip install .
 call pip install pyinstaller==3.2.1
 call pip list
 call set PYTHONHASHSEED=1
@@ -45,33 +64,26 @@ call python -m zipfile -c dist\Tahoe-LAFS.zip dist\Tahoe-LAFS
 call set PYTHONHASHSEED=
 call move dist ..\..
 call popd
-call .\build\venv-tahoe\Scripts\deactivate
+call deactivate
 goto :eof
 
 :all
 if exist .\dist\Tahoe-LAFS.zip (
-    call C:\Python27\python.exe -m zipfile -e .\dist\Tahoe-LAFS.zip dist
+    call py -2.7 -m zipfile -e .\dist\Tahoe-LAFS.zip dist
+    call .\dist\Tahoe-LAFS\tahoe.exe --version-and-path
 ) else (
     call :frozen-tahoe
 )
-call .\dist\Tahoe-LAFS\tahoe.exe --version-and-path
-call py -3.5 -m pip install --upgrade pip virtualenv
-call py -3.5 -m virtualenv --clear .\build\venv-gridsync
+call py -3.5 -m venv --clear .\build\venv-gridsync
 call .\build\venv-gridsync\Scripts\activate
-call pip install --upgrade pip
-::call pip install sip==4.18.1
-::call pip install pyqt5==5.6
+call pip install --upgrade setuptools pip
 call pip install -r .\requirements\requirements-hashes.txt
-::call pip install --upgrade sip
-::call pip install --upgrade pyqt5
 call pip install --upgrade git+https://github.com/gridsync/qt5reactor.git
 call pip install . 
-::call pip install pyinstaller==3.2.1
-call pip install pyinstaller==3.1.1
+call pip install pyinstaller==3.2.1
 call pip list
 call set PYTHONHASHSEED=1
 call pyinstaller -y --clean --log-level=DEBUG misc\gridsync.spec
-::call move dist\Tahoe-LAFS dist\Gridsync\Tahoe-LAFS
-::call python -m zipfile -c dist\Gridsync.zip dist\Gridsync
+call set PYTHONHASHSEED=
 call deactivate
 goto :eof
