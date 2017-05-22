7e1cc1540ea5c83efa40b1aa1626ba77097c35e8
Chris Wood <chris@gridsync.io>
2017-05-22 15:47:09 -0400

    Remove h2b(), b2h() functions from util.py, tests
    The "humanize" library already provides the needed functionality

--- gridsync/util.py
+++ gridsync/util.py
@@ -1,23 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import division
-
-
-def h2b(s):
-    """Convert 'human-readable' byte-representation to an integer of bytes"""
-    for i, unit in enumerate(['KB', 'MB', 'GB', 'TB', 'PB', 'EB'], start=1):
-        if s.endswith(unit):
-            return int(float(s[:-2].strip()) * 1024 ** i)
-    return int(s[:-1])
-
-
-def b2h(b):
-    """Convert integer of bytes into 'human-readable' byte-representation"""
-    for i, unit in enumerate(['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB']):
-        rounded = round(b / (1024 ** i), 2)
-        if rounded < 1024:
-            return "{} {}".format(rounded, unit)
-
 
 def humanized_list(list_, kind='files'):
     if len(list_) == 1:
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,30 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from gridsync.util import h2b, b2h, humanized_list
-
-
-def test_1B_2b():
-    assert h2b('1B') == 1
-
-
-def test_1024B_2b():
-    assert h2b('1KB') == 1024
-
-
-def test_256GB_2b():
-    assert h2b('256GB') == 274877906944
-
-
-def test_274877906944_2h():
-    assert b2h(274877906944) == '256.0 GB'
-
-
-def test_1024_2h():
-    assert b2h(1024) == '1.0 KB'
-
-
-def test_1_2h():
-    assert b2h(1) == '1.0 B'
+from gridsync.util import humanized_list
 
 
 def test_humanized_list_Alice():
