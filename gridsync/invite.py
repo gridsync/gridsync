52a8d2df991c50985dc0b8feb0cefc3a2dd0a8fd
Chris Wood <chris@gridsync.io>
2017-07-04 20:28:08 -0400

    Use "tahoe invite"-compatible wormhole protocol (WIP)

--- gridsync/invite.py
+++ gridsync/invite.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import json
+import logging
+
 from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks, returnValue
+from wormhole import wormhole
 try:
     from wormhole.wordlist import raw_words
 except ImportError:  # TODO: Switch to new magic-wormhole completion API
     from wormhole._wordlist import raw_words
-from wormhole.xfer_util import receive
 
 from gridsync import settings
 
+APPID = settings['wormhole']['appid']
+RELAY = settings['wormhole']['relay']
+#APPID = "tahoe-lafs.org/lafs"  # XXX
+# https://github.com/tahoe-lafs/tahoe-lafs/pull/418#pullrequestreview-47916533
 
 wordlist = []
 for word in raw_words.items():
@@ -32,6 +39,29 @@ def is_valid(code):
 
 @inlineCallbacks
 def wormhole_receive(code):
-    msg = yield receive(reactor, settings['wormhole']['appid'],
-                        settings['wormhole']['relay'], code)
+    logging.debug("Connecting to %s...", RELAY)
+    wh = wormhole.create(APPID, RELAY, reactor)
+    welcome = yield wh.get_welcome()
+    logging.debug("Connected to wormhole server; got welcome: %s", welcome)
+    wh.set_code(code)
+    logging.debug("Using code: %s", code)
+
+    client_intro = {"abilities": {"client-v1": {}}}
+    wh.send_message(json.dumps(client_intro).encode("utf-8"))
+
+    server_intro = yield wh.get_message()
+    server_intro = json.loads(server_intro.decode("utf-8"))
+    logging.debug("Received server introduction: %s", server_intro)
+
+    # XXX: raise UpgradeRequiredError? Handle "old" xfer_util-based protocol?
+    if 'abilities' not in server_intro:
+        raise Exception("No 'abilities' in server introduction")
+    if 'server-v1' not in server_intro['abilities']:
+        raise Exception("No 'server-v1' in server abilities")
+
+    msg = yield wh.get_message()
+    msg = json.loads(msg.decode("utf-8"))
+    logging.debug("Received message: %s", msg)
+
+    yield wh.close()
     returnValue(msg)
