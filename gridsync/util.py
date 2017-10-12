97e6a0e81184284d1893759afbd0c46101817b69
Chris Wood <chris@gridsync.io>
2017-10-12 01:29:30 -0400

    Add 'dehumanized_size' function, tests

--- gridsync/util.py
+++ gridsync/util.py
@@ -10,3 +10,17 @@ def humanized_list(list_, kind='files'):
         return "{}, {}, and {}".format(*list_)
     return "{}, {}, and {} other {}".format(list_[0], list_[1],
                                             len(list_) - 2, kind)
+
+def dehumanized_size(s):
+    if not s:
+        return 0
+    elif not s[0].isdigit():
+        raise ValueError("Prefix must be a digit (received '{}')".format(s))
+    string = s.upper()
+    if string.endswith('BYTES'):
+        return int(s[:-5])
+    suffixes = ('KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB')
+    for i, suffix in enumerate(suffixes, start=1):
+        if string.endswith(suffix):
+            return int(float(s[:-2].strip()) * 1024 ** i)
+    raise ValueError("Unknown suffix for '{}'".format(s))
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from gridsync.util import humanized_list
+import pytest
+
+from gridsync.util import humanized_list, dehumanized_size
 
 
 def test_humanized_list_Alice():
@@ -21,3 +23,29 @@ def test_humanized_list_Alice_Bob_Eve():
 def test_humanized_list_Alice_Bob_Eve_Mallory():
     result = humanized_list(['Alice', 'Bob', 'Eve', 'Mallory'], 'characters')
     assert result == 'Alice, Bob, and 2 other characters'
+
+
+def test_dehumanize_none_is_0():
+    assert dehumanized_size(None) == 0
+
+
+def test_dehumanize_32B():
+    assert dehumanized_size('32 Bytes') == 32
+
+
+def test_dehumanize_1KB():
+    assert dehumanized_size('1KB') == 1024
+
+
+def test_dehumanize_256GB():
+    assert dehumanized_size('256GB') == 274877906944
+
+
+def test_dehumanize_value_error_prefix_not_digit():
+    with pytest.raises(ValueError):
+        assert dehumanized_size('two bytes') == 2
+
+
+def test_dehumanize_value_error_unknown_suffix():
+    with pytest.raises(ValueError):
+        assert dehumanized_size('3 ninjas') == 3
