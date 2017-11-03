2d31dd9b4c287e1b0a2f3920d20da9a22ad35029
Chris Wood <chris@gridsync.io>
2017-11-03 12:16:16 -0400

    Try adding waitActive() to PyQt regression test

--- tests/test_pyqt.py
+++ tests/test_pyqt.py
@@ -8,4 +8,5 @@ def test_pyqt59_shortcut_key_press_regression(qtbot):
     msg = QMessageBox()
     with qtbot.wait_signal(msg.finished):
         msg.show()
+        qtbot.waitActive(msg)
         qtbot.keyPress(msg, 'O')  # Shortcut to activate default "OK" button 
