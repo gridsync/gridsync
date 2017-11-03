32842e765916f8bc51ac13b9ce5d259c8b653133
Chris Wood <chris@gridsync.io>
2017-11-03 12:34:16 -0400

    Try qtbot.add_widget()

--- tests/test_pyqt.py
+++ tests/test_pyqt.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import QMessageBox
 
 def test_pyqt59_shortcut_key_press_regression(qtbot):
     msg = QMessageBox()
+    qtbot.add_widget(msg)
     with qtbot.wait_signal(msg.finished):
         msg.show()
         qtbot.waitActive(msg)
