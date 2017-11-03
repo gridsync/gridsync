e4a391fdb610b6b003d57c19ab0009c440db63b2
Chris Wood <chris@gridsync.io>
2017-11-03 09:16:44 -0400

    Add regression test for PyQt shortcut/keyPress bug

---
+++ tests/test_pyqt.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from PyQt5.QtWidgets import QMessageBox
+
+
+def test_pyqt59_shortcut_key_press_regression(qtbot):
+    msg = QMessageBox()
+    with qtbot.wait_signal(msg.finished):
+        msg.show()
+        qtbot.keyPress(msg, 'O')  # Shortcut to activate default "OK" button 
