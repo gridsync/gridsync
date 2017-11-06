7dacb66ca9b7bc0d86bc9e1df5fa03f1cfd5f26f
Chris Wood <chris@gridsync.io>
2017-11-06 17:29:24 -0500

    Mark PyQt 5.9 regression test as expected to fail

--- tests/test_pyqt.py
+++ tests/test_pyqt.py
@@ -3,11 +3,16 @@
 import os
 
 import pytest
+from PyQt5.QtCore import PYQT_VERSION_STR
 from PyQt5.QtWidgets import QMessageBox
 
 
 @pytest.mark.skipif('CI' in os.environ, reason="Fails on Travis-CI/AppVeyor")
+@pytest.mark.xfail(PYQT_VERSION_STR == '5.9', reason="Fails on PyQt 5.9")
 def test_pyqt59_shortcut_key_press_regression(qtbot):
+    print(PYQT_VERSION_STR)
+    print(PYQT_VERSION_STR)
+    print(PYQT_VERSION_STR)
     msg = QMessageBox()
     qtbot.add_widget(msg)
     with qtbot.wait_signal(msg.finished):
