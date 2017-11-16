991f31d065d8650b0271e77b405dbfc37bbe31f1
Chris Wood <chris@gridsync.io>
2017-11-16 16:16:53 -0500

    Drop scrypt support; always use Argon2id KDF

--- gridsync/crypto.py
+++ gridsync/crypto.py
@@ -2,48 +2,25 @@
 
 from PyQt5.QtCore import pyqtSignal, QObject
 from nacl.exceptions import CryptoError
-from nacl.pwhash import (
-    kdf_scryptsalsa208sha256, SCRYPT_SALTBYTES, SCRYPT_OPSLIMIT_SENSITIVE,
-    SCRYPT_MEMLIMIT_SENSITIVE)
+from nacl.pwhash import argon2id
 from nacl.secret import SecretBox
 from nacl.utils import random
-
-try:
-    from nacl.pwhash import argon2id
-    ARGON2_AVAILABLE = True
-except ImportError:
-    ARGON2_AVAILABLE = False
 
 
 class VersionError(CryptoError):
     pass
 
 
-class Argon2NotAvailableError(VersionError):
-    pass
-
-
-def encrypt(message, password, use_scrypt=False):
-    if ARGON2_AVAILABLE and not use_scrypt:
-        version = b'2'
-        salt = random(argon2id.SALTBYTES)  # 16
-        key = argon2id.kdf(
-            SecretBox.KEY_SIZE,  # 32
-            password,
-            salt,
-            opslimit=argon2id.OPSLIMIT_SENSITIVE,  # 4
-            memlimit=argon2id.MEMLIMIT_SENSITIVE   # 1073741824
-        )
-    else:
-        version = b'1'
-        salt = random(SCRYPT_SALTBYTES)  # 32
-        key = kdf_scryptsalsa208sha256(
-            SecretBox.KEY_SIZE,  # 32
-            password,
-            salt,
-            opslimit=SCRYPT_OPSLIMIT_SENSITIVE,  # 33554432
-            memlimit=SCRYPT_MEMLIMIT_SENSITIVE   # 1073741824
-        )
+def encrypt(message, password):
+    version = b'1'
+    salt = random(argon2id.SALTBYTES)  # 16
+    key = argon2id.kdf(
+        SecretBox.KEY_SIZE,  # 32
+        password,
+        salt,
+        opslimit=argon2id.OPSLIMIT_SENSITIVE,  # 4
+        memlimit=argon2id.MEMLIMIT_SENSITIVE   # 1073741824
+    )
     box = SecretBox(key)
     encrypted = box.encrypt(message)
     return version + salt + encrypted
@@ -52,10 +29,7 @@ def encrypt(message, password, use_scrypt=False):
 def decrypt(ciphertext, password):
     version = ciphertext[:1]
     ciphertext = ciphertext[1:]
-    if version == b'2':
-        if not ARGON2_AVAILABLE:
-            raise Argon2NotAvailableError(
-                "Argon2 is not available; PyNaCl may be out-of-date")
+    if version == b'1':
         salt = ciphertext[:argon2id.SALTBYTES]  # 16
         encrypted = ciphertext[argon2id.SALTBYTES:]
         key = argon2id.kdf(
@@ -64,16 +38,6 @@ def decrypt(ciphertext, password):
             salt,
             opslimit=argon2id.OPSLIMIT_SENSITIVE,  # 4
             memlimit=argon2id.MEMLIMIT_SENSITIVE   # 1073741824
-        )
-    elif version == b'1':
-        salt = ciphertext[:SCRYPT_SALTBYTES]  # 32
-        encrypted = ciphertext[SCRYPT_SALTBYTES:]
-        key = kdf_scryptsalsa208sha256(
-            SecretBox.KEY_SIZE,  # 32
-            password,
-            salt,
-            opslimit=SCRYPT_OPSLIMIT_SENSITIVE,  # 33554432
-            memlimit=SCRYPT_MEMLIMIT_SENSITIVE   # 1073741824
         )
     else:
         raise VersionError("Invalid version byte; received {}".format(version))
