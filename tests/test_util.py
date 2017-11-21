68369afea10f5da34efd7977ba108ebf48901a45
Chris Wood <chris@gridsync.io>
2017-11-21 10:45:23 -0500

    Parametrize more tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -40,47 +40,31 @@ def test_b58decode_value_error():
         b58decode('abcl23')
 
 
-def test_humanized_list_Alice():
-    result = humanized_list(['Alice'])
-    assert result == 'Alice'
+@pytest.mark.parametrize("items,kind,humanized", [
+    [['Alice'], None, 'Alice'],
+    [['Alice', 'Bob'], None, 'Alice and Bob'],
+    [['Alice', 'Bob', 'Eve'], None, 'Alice, Bob, and Eve'],
+    [['Alice', 'Bob', 'Eve', 'Mallory'], 'characters', 'Alice, Bob, and 2 other characters']
+])
+def test_humanized_list(items, kind, humanized):
+    assert humanized_list(items, kind) == humanized
 
 
-def test_humanized_list_Alice_Bob():
-    result = humanized_list(['Alice', 'Bob'])
-    assert result == 'Alice and Bob'
+@pytest.mark.parametrize("s,dehumanized", [
+    [None, 0],
+    ['32 Bytes', 32],
+    ['1KB', 1024],
+    ['256GB', 274877906944],
+])
+def test_dehumanized_size(s, dehumanized):
+    assert dehumanized_size(s) == dehumanized
 
 
-def test_humanized_list_Alice_Bob_Eve():
-    result = humanized_list(['Alice', 'Bob', 'Eve'])
-    assert result == 'Alice, Bob, and Eve'
-
-
-def test_humanized_list_Alice_Bob_Eve_Mallory():
-    result = humanized_list(['Alice', 'Bob', 'Eve', 'Mallory'], 'characters')
-    assert result == 'Alice, Bob, and 2 other characters'
-
-
-def test_dehumanize_none_is_0():
-    assert dehumanized_size(None) == 0
-
-
-def test_dehumanize_32B():
-    assert dehumanized_size('32 Bytes') == 32
-
-
-def test_dehumanize_1KB():
-    assert dehumanized_size('1KB') == 1024
-
-
-def test_dehumanize_256GB():
-    assert dehumanized_size('256GB') == 274877906944
-
-
-def test_dehumanize_value_error_prefix_not_digit():
+def test_dehumanized_size_value_error_prefix_not_digit():
     with pytest.raises(ValueError):
         assert dehumanized_size('two bytes') == 2
 
 
-def test_dehumanize_value_error_unknown_suffix():
+def test_dehumanized_size_value_error_unknown_suffix():
     with pytest.raises(ValueError):
         assert dehumanized_size('3 ninjas') == 3
