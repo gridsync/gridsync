789011a806adbcd387c36d100a01e818402d7bf2
Chris Wood <chris@gridsync.io>
2017-11-23 13:59:18 -0500

    Skip clipboard text test on headless environments

--- tests/test_desktop.py
+++ tests/test_desktop.py
@@ -8,6 +8,8 @@ def test_get_clipboard_modes():
     assert len(get_clipboard_modes()) >= 1
 
 
+@pytest.mark.skipif(
+    'CI' in os.environ, reason="Fails on some headless environments")
 def test_clipboard_text():
     set_clipboard_text('test')
     assert get_clipboard_text() == 'test'
