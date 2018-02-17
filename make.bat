7480134f473c5aeaeb8b0dae63a525273b6095d0
Chris Wood <chris@gridsync.io>
2018-02-17 10:19:18 -0500

    Try PyInstaller with Python 3.6..

--- make.bat
+++ make.bat
@@ -5,8 +5,8 @@
 ::
 ::  2) Python 2.7 <https://www.python.org>
 ::
-::  3) Python 3.5 (not 3.6; see PyInstaller#2331) <https://www.python.org>
-::      -Select "Add Python 3.5 to PATH" option during install
+::  3) Python 3.6 <https://www.python.org>
+::      -Select "Add Python 3.6 to PATH" option during install
 ::
 ::  4) Microsoft Visual C++ Compiler for Python 2.7 <https://aka.ms/vcpython27>
 ::
@@ -27,7 +27,7 @@ if defined APPVEYOR (
     set PYTHON3=%PYTHON%\python.exe
 ) else (
     set PYTHON2=py -2.7
-    set PYTHON3=py -3.5
+    set PYTHON3=py -3.6
 )
 
 if "%1"=="clean" call :clean
