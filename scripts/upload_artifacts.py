99bf6e81d8bfc34a332c95209492a9c4a53200e3
Chris Wood <chris@gridsync.io>
2018-02-24 12:33:06 -0500

    Add 'upload_artifacts' script

---
+++ scripts/upload_artifacts.py
@@ -0,0 +1,44 @@
+from __future__ import print_function
+
+import os
+import shutil
+import subprocess
+import sys
+import time
+
+
+print("Uploading artifacts to storage grid...")
+
+try:
+    introducer = os.environ['TAHOE_INTRODUCER_FURL']
+except KeyError:
+    sys.exit("TAHOE_INTRODUCER_FURL not set; exiting")
+
+files = sys.argv[1:]
+if not files:
+    sys.exit("No files to upload; exiting")
+
+subprocess.check_output([
+    'dist/Tahoe-LAFS/tahoe',
+    'create-client',
+    '-i', introducer,
+    '--shares-happy=1',
+    '--shares-needed=1',
+    '--shares-total=1',
+    '--webport=tcp:0',
+    '.tahoe'
+])
+subprocess.check_output(['tahoe', '-d', '.tahoe', 'start'])
+time.sleep(3)
+
+for path in files:
+    if os.path.isfile(path):
+        output = subprocess.check_output(
+            ['tahoe', '-d', '.tahoe', 'put', path], stderr=subprocess.STDOUT)
+        resp, cap = output.decode().strip().split('\n')
+        if resp == '200 OK':
+            print(cap, path)
+
+subprocess.check_output(['tahoe', '-d', '.tahoe', 'stop'])
+shutil.rmtree('.tahoe')
+print("Done!")
