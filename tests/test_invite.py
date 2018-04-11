b965a371f76525316c3e2498533fb67d575a2108
Chris Wood <chris@gridsync.io>
2018-04-10 20:33:25 -0400

    Add 'get_settings_from_cheatcode' tests

--- tests/test_invite.py
+++ tests/test_invite.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from gridsync.invite import is_valid
+import os
+
+from gridsync.invite import get_settings_from_cheatcode, is_valid
 
 
 def test_invalid_code_not_three_words():
@@ -21,3 +23,20 @@ def test_invalid_code_third_word_not_in_wordlist():
 
 def test_valid_code_is_valid():
     assert is_valid('1-cranky-tapeworm')
+
+
+def test_get_settings_from_cheatcode(tmpdir_factory, monkeypatch):
+    pkgdir = os.path.join(str(tmpdir_factory.getbasetemp()), 'pkgdir')
+    providers_path = os.path.join(pkgdir, 'resources', 'providers')
+    os.makedirs(providers_path)
+    with open(os.path.join(providers_path, 'test-test.json'), 'w') as f:
+        f.write('{"introducer": "pb://"}')
+    monkeypatch.setattr('gridsync.invite.pkgdir', pkgdir)
+    settings = get_settings_from_cheatcode('test-test')
+    assert settings['introducer'] == 'pb://'
+
+
+def test_get_settings_from_cheatcode_none(tmpdir_factory, monkeypatch):
+    pkgdir = os.path.join(str(tmpdir_factory.getbasetemp()), 'pkgdir-empty')
+    monkeypatch.setattr('gridsync.invite.pkgdir', pkgdir)
+    assert get_settings_from_cheatcode('test-test') is None
