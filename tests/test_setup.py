05a86e6de78214a33d715ebee0b704ad5d467b76
Chris Wood <chris@gridsync.io>
2018-04-13 15:36:43 -0400

    Add validate_grid tests

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from gridsync.setup import prompt_for_grid_name, prompt_for_folder_name
+import os
+
+import yaml
+
+from gridsync.setup import (
+    prompt_for_grid_name, validate_grid, prompt_for_folder_name)
 
 
 def test_prompt_for_grid_name(monkeypatch):
@@ -8,7 +13,69 @@ def test_prompt_for_grid_name(monkeypatch):
         'gridsync.setup.QInputDialog.getText',
         lambda a, b, c, d, e: ('NewGridName', 1)
     )
-    assert prompt_for_grid_name('GridName', 1) == ('NewGridName', 1)
+    assert prompt_for_grid_name('GridName') == ('NewGridName', 1)
+
+
+def test_validate_grid_no_nickname(monkeypatch, tmpdir_factory):
+    monkeypatch.setattr(
+        'gridsync.setup.config_dir', str(tmpdir_factory.mktemp('config_dir')))
+    monkeypatch.setattr(
+        'gridsync.setup.prompt_for_grid_name', lambda x, y: ('NewGridName', 1))
+    assert validate_grid({'nickname': None}) == {'nickname': 'NewGridName'}
+
+
+def test_validate_grid_conflicting_introducer(monkeypatch, tmpdir_factory):
+    config_dir = str(tmpdir_factory.mktemp('config_dir'))
+    monkeypatch.setattr('gridsync.setup.config_dir', config_dir)
+    nodedir = os.path.join(config_dir, 'ExistingGrid')
+    os.makedirs(nodedir)
+    with open(os.path.join(nodedir, 'tahoe.cfg'), 'w') as f:
+        f.write('[client]\nintroducer.furl = pb://11111\n')
+    monkeypatch.setattr(
+        'gridsync.setup.prompt_for_grid_name', lambda x, y: ('NewGridName', 1))
+    settings = {
+        'nickname': 'ExistingGrid',
+        'introducer': 'pb://22222'
+    }
+    assert validate_grid(settings) == {
+        'nickname': 'NewGridName',
+        'introducer': 'pb://22222'
+    }
+
+
+def test_validate_grid_conflicting_servers(monkeypatch, tmpdir_factory):
+    config_dir = str(tmpdir_factory.mktemp('config_dir'))
+    monkeypatch.setattr('gridsync.setup.config_dir', config_dir)
+    private_dir = os.path.join(config_dir, 'ExistingGrid', 'private')
+    os.makedirs(private_dir)
+    servers = {
+        'storage': {
+            'v0-aaaaaaaa': {
+                'ann': {
+                    'anonymous-storage-FURL': 'pb://11111111',
+                    'nickname': 'node-1'
+                }
+            }
+        }
+    }
+    with open(os.path.join(private_dir, 'servers.yaml'), 'w') as f:
+        f.write(yaml.safe_dump(servers, default_flow_style=False))
+    monkeypatch.setattr(
+        'gridsync.setup.prompt_for_grid_name', lambda x, y: ('NewGridName', 1))
+    settings = {
+        'nickname': 'ExistingGrid',
+        'storage': {
+            'anonymous-storage-FURL': 'pb://11111111',
+            'nickname': 'node-1'
+        }
+    }
+    assert validate_grid(settings) == {
+        'nickname': 'NewGridName',
+        'storage': {
+            'anonymous-storage-FURL': 'pb://11111111',
+            'nickname': 'node-1'
+        }
+    }
 
 
 def test_prompt_for_folder_name(monkeypatch):
@@ -16,4 +83,4 @@ def test_prompt_for_folder_name(monkeypatch):
         'gridsync.setup.QInputDialog.getText',
         lambda a, b, c, d, e: ('NewFolderName', 1)
     )
-    assert prompt_for_folder_name('FolderName', "", 1) == ('NewFolderName', 1)
+    assert prompt_for_folder_name('FolderName', 'Grid') == ('NewFolderName', 1)
