259d953bfac0cd097ee01b5a24116a0a8f86347b
Chris Wood <chris@gridsync.io>
2018-04-23 14:36:15 -0400

    Add comment re. PyNaCl incompatibility on old Macs

--- scripts/maybe_rebuild_libsodium.py
+++ scripts/maybe_rebuild_libsodium.py
@@ -11,6 +11,9 @@ if not version:
 major = int(version.split('.')[0])
 minor = int(version.split('.')[1])
 
+# The precompiled libsodium library included with PyNaCl wheels crashes with
+# "EXC_BAD_INSTRUCTION (SIGILL)" on some older (2007 era? OS X 10.9?) Macs.
+# Recompling libsodium/PyNaCl from source on such older systems avoids this.
 if (major, minor) < (10, 10):
     print("Older Mac detected ({}); "
           "Rebuilding libsodium/PyNaCl...".format(version))
