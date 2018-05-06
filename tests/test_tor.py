16ce80abbe3a86dd3dde0bce7b5dd6eaf7dd4911
Chris Wood <chris@gridsync.io>
2018-05-06 12:13:58 -0400

    Add 'tor' module tests

---
+++ tests/test_tor.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from unittest.mock import MagicMock
+
+import pytest
+
+from gridsync.tor import get_tor, TorError
+
+
+@pytest.inlineCallbacks
+def test_get_tor(monkeypatch):
+    fake_tor = MagicMock()
+    monkeypatch.setattr('txtorcon.connect', lambda _: fake_tor)
+    tor = yield get_tor(None)
+    assert tor == fake_tor
+
+
+@pytest.inlineCallbacks
+def test_get_tor_return_none(monkeypatch):
+    fake_connect = MagicMock(side_effect=RuntimeError())
+    monkeypatch.setattr('txtorcon.connect', fake_connect)
+    tor = yield get_tor(None)
+    assert tor == None
