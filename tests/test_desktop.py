90dd703be240a29e25169b810ccc0122cb1b5d4a
Chris Wood <chris@gridsync.io>
2018-06-15 12:17:02 -0400

    Add _dbus_notify() tests

--- tests/test_desktop.py
+++ tests/test_desktop.py
@@ -6,8 +6,36 @@ from unittest.mock import Mock
 import pytest
 
 from gridsync.desktop import (
-    get_clipboard_modes, get_clipboard_text, set_clipboard_text,
+    _dbus_notify, get_clipboard_modes, get_clipboard_text, set_clipboard_text,
     autostart_enable, autostart_is_enabled, autostart_disable)
+
+
+def test__dbus_notify_bus_not_connected(monkeypatch):
+    monkeypatch.setattr(
+        'PyQt5.QtDBus.QDBusConnection.isConnected', lambda _: False)
+    with pytest.raises(OSError):
+        _dbus_notify('', '')
+
+
+def test__dbus_notify_interface_not_valid(monkeypatch):
+    monkeypatch.setattr(
+        'PyQt5.QtDBus.QDBusConnection.isConnected', lambda _: True)
+    monkeypatch.setattr('PyQt5.QtDBus.QDBusInterface.isValid', lambda _: False)
+    with pytest.raises(ValueError):
+        _dbus_notify('', '')
+
+
+def test__dbus_notify_interface_called(monkeypatch):
+    was_called = [False]
+
+    def fake_call(*_):
+        was_called[0] = True
+    monkeypatch.setattr(
+        'PyQt5.QtDBus.QDBusConnection.isConnected', lambda _: True)
+    monkeypatch.setattr('PyQt5.QtDBus.QDBusInterface.isValid', lambda _: True)
+    monkeypatch.setattr('PyQt5.QtDBus.QDBusInterface.call', fake_call)
+    _dbus_notify('', '')
+    assert was_called[0] is True
 
 
 @pytest.fixture()
