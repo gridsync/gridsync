b6ea546b2cf4bbefb61cd2d9eb789de8578dbcd3
Chris Wood <chris@gridsync.io>
2018-07-17 12:09:48 -0400

    Skip regression test on PyQt5 5.7.1

--- tests/test_pyqt.py
+++ tests/test_pyqt.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 
 import pytest
 from PyQt5.QtCore import PYQT_VERSION_STR
 from PyQt5.QtWidgets import QMessageBox
 
 
+@pytest.mark.skipif(PYQT_VERSION_STR == '5.7.1' and sys.platform == 'linux',
+                    reason="Segmentation fault observed on Debian 9")
 @pytest.mark.skipif('CI' in os.environ, reason="Fails on Travis-CI/AppVeyor")
 @pytest.mark.xfail(PYQT_VERSION_STR == '5.9', reason="Fails on PyQt 5.9")
 # https://bugreports.qt.io/browse/QTBUG-61197
