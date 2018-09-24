2144674c72c9950bab9ebcfab0d55d3284fa8be6
Chris Wood <chris@gridsync.io>
2018-09-24 13:03:34 -0400

    Update attribute names

--- gridsync/gui/status.py
+++ gridsync/gui/status.py
@@ -17,17 +17,17 @@ class StatusPanel(QWidget):
         self.num_known = 0
         self.available_space = 0
 
-        self.icon_checkmark = QLabel()
-        self.icon_checkmark.setPixmap(
+        self.checkmark_icon = QLabel()
+        self.checkmark_icon.setPixmap(
             QPixmap(resource('checkmark.png')).scaled(20, 20)
         )
 
-        self.icon_syncing = QLabel()
+        self.syncing_icon = QLabel()
 
         self.sync_movie = QMovie(resource('sync.gif'))
         self.sync_movie.setCacheMode(True)
         self.sync_movie.updated.connect(
-            lambda: self.icon_syncing.setPixmap(
+            lambda: self.syncing_icon.setPixmap(
                 self.sync_movie.currentPixmap().scaled(20, 20)
             )
         )
@@ -37,12 +37,12 @@ class StatusPanel(QWidget):
 
         self.on_sync_state_updated(0)
 
-        self.icon_onion = QLabel()
-        self.icon_onion.setPixmap(
+        self.tor_icon = QLabel()
+        self.tor_icon.setPixmap(
             QPixmap(resource('tor-onion.png')).scaled(24, 24)
         )
         if not self.gateway.use_tor:
-            self.icon_onion.hide()
+            self.tor_icon.hide()
 
         self.globe_icon = QLabel()
         self.globe_icon.setPixmap(
@@ -52,11 +52,11 @@ class StatusPanel(QWidget):
         layout = QGridLayout(self)
         left, _, right, bottom = layout.getContentsMargins()
         layout.setContentsMargins(left, 0, right, bottom - 2)
-        layout.addWidget(self.icon_checkmark, 1, 1)
-        layout.addWidget(self.icon_syncing, 1, 1)
+        layout.addWidget(self.checkmark_icon, 1, 1)
+        layout.addWidget(self.syncing_icon, 1, 1)
         layout.addWidget(self.status_label, 1, 2)
         layout.addItem(QSpacerItem(0, 0, QSizePolicy.Expanding, 0), 1, 3)
-        layout.addWidget(self.icon_onion, 1, 4)
+        layout.addWidget(self.tor_icon, 1, 4)
         layout.addWidget(self.globe_icon, 1, 5)
 
         self.gateway.monitor.total_sync_state_updated.connect(
@@ -69,18 +69,18 @@ class StatusPanel(QWidget):
         if state == 0:
             self.status_label.setText("Connecting...")
             self.sync_movie.setPaused(True)
-            self.icon_syncing.hide()
-            self.icon_checkmark.hide()
+            self.syncing_icon.hide()
+            self.checkmark_icon.hide()
         elif state == 1:
             self.status_label.setText("Syncing")
-            self.icon_checkmark.hide()
-            self.icon_syncing.show()
+            self.checkmark_icon.hide()
+            self.syncing_icon.show()
             self.sync_movie.setPaused(False)
         elif state == 2:
             self.status_label.setText("Up to date")
             self.sync_movie.setPaused(True)
-            self.icon_syncing.hide()
-            self.icon_checkmark.show()
+            self.syncing_icon.hide()
+            self.checkmark_icon.show()
 
     def _update_grid_info_tooltip(self):
         if self.available_space:
