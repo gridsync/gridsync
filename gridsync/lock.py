9a9de1b149719265ee9eaa81fe5b0c22e48eb704
Chris Wood <chris@gridsync.io>
2019-01-18 16:57:59 -0500

    Move platform-specific code into public methods

--- gridsync/lock.py
+++ gridsync/lock.py
@@ -15,43 +15,30 @@ class Lock():
         self.lockfile = lockfile
         self.fd = None
 
-    def _acquire_win32(self):
-        try:
-            os.remove(self.lockfile)
-        except OSError:
-            pass
-        fd = os.open(self.lockfile, os.O_CREAT | os.O_EXCL | os.O_RDWR)
-        self.fd = fd
-
-    def _acquire(self):
-        fd = open(self.lockfile, 'w')
-        fd.flush()
-        fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
-        self.fd = fd
-
     def acquire(self):
         logging.debug("Acquiring lock: %s ...", self.lockfile)
         if sys.platform == 'win32':
-            self._acquire_win32()
+            try:
+                os.remove(self.lockfile)
+            except OSError:
+                pass
+            fd = os.open(self.lockfile, os.O_CREAT | os.O_EXCL | os.O_RDWR)
         else:
-            self._acquire()
+            fd = open(self.lockfile, 'w')
+            fd.flush()
+            fcntl.flock(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        self.fd = fd
         logging.debug("Acquired lock: %s", self.fd)
-
-    def _release_win32(self):
-        if self.fd:
-            os.close(self.fd)
-
-    def _release(self):
-        if self.fd:
-            fcntl.flock(self.fd, fcntl.LOCK_UN)
 
     def release(self):
         logging.debug("Releasing lock: %s ...", self.lockfile)
-        if sys.platform == 'win32':
-            self._release_win32()
+        if not self.fd:
+            logging.warning("No file descriptor found")
+        elif sys.platform == 'win32':
+            os.close(self.fd)
         else:
-            self._release()
-        logging.debug("Lock released.")
+            fcntl.flock(self.fd, fcntl.LOCK_UN)
+        logging.debug("Lock released: %s", self.lockfile)
         try:
             os.remove(self.lockfile)
         except OSError:
