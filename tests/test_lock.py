a75baab70a8539e911a4f087d6e0fb161dd68f4f
Chris Wood <chris@gridsync.io>
2019-01-18 14:29:35 -0500

    Add another acquire() test

--- tests/test_lock.py
+++ tests/test_lock.py
@@ -19,11 +19,19 @@ def test_lock_acquire_lockfile_created(tmpdir):
     assert os.path.isfile(lock.lockfile)
 
 
-def test_lock_acquire_raise_oserror(tmpdir):
+def test_lock_acquire_raise_oserror_on_second_call(tmpdir):
     lock = Lock(os.path.join(str(tmpdir), 'test.lock'))
     lock.acquire()
     with pytest.raises(OSError):
         lock.acquire()
+
+
+def test_lock_acquire_raise_oserror_from_second_instance(tmpdir):
+    lock_1 = Lock(os.path.join(str(tmpdir), 'test.lock'))
+    lock_1.acquire()
+    lock_2 = Lock(os.path.join(str(tmpdir), 'test.lock'))
+    with pytest.raises(OSError):
+        lock_2.acquire()
 
 
 def test_lock_release(tmpdir):
