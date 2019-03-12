173005d5d4298cfaf2f4da4caac2eb4664900049
Chris Wood <chris@gridsync.io>
2019-03-11 20:15:31 -0400

    Scope RawConfigParser to function
    Fix a weird issue whereby Config.get() was somehow caching the value of
    the magic_folder "enabled" option of tahoe.cfg -- even after the `tahoe
    magic-folder leave` command removed the section/option from the file..

--- gridsync/config.py
+++ gridsync/config.py
@@ -7,37 +7,40 @@ from configparser import RawConfigParser, NoOptionError, NoSectionError
 class Config():
     def __init__(self, filename):
         self.filename = filename
-        self.config = RawConfigParser(allow_no_value=True)
 
     def set(self, section, option, value):
-        self.config.read(self.filename)
-        if not self.config.has_section(section):
-            self.config.add_section(section)
-        self.config.set(section, option, value)
+        config = RawConfigParser(allow_no_value=True)
+        config.read(self.filename)
+        if not config.has_section(section):
+            config.add_section(section)
+        config.set(section, option, value)
         with open(self.filename, 'w') as f:
-            self.config.write(f)
+            config.write(f)
 
     def get(self, section, option):
-        self.config.read(self.filename)
+        config = RawConfigParser(allow_no_value=True)
+        config.read(self.filename)
         try:
-            return self.config.get(section, option)
+            return config.get(section, option)
         except (NoOptionError, NoSectionError):
             return None
 
     def save(self, settings_dict):
-        self.config.read(self.filename)
+        config = RawConfigParser(allow_no_value=True)
+        config.read(self.filename)
         for section, d in settings_dict.items():
-            if not self.config.has_section(section):
-                self.config.add_section(section)
+            if not config.has_section(section):
+                config.add_section(section)
             for option, value in d.items():
-                self.config.set(section, option, value)
+                config.set(section, option, value)
         with open(self.filename, 'w') as f:
-            self.config.write(f)
+            config.write(f)
 
     def load(self):
-        self.config.read(self.filename)
+        config = RawConfigParser(allow_no_value=True)
+        config.read(self.filename)
         settings_dict = defaultdict(dict)
-        for section in self.config.sections():
-            for option, value in self.config.items(section):
+        for section in config.sections():
+            for option, value in config.items(section):
                 settings_dict[section][option] = value
         return dict(settings_dict)
