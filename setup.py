0a032c9f130e89b48abefb809e817e0a47c7b0eb
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-03-27 08:30:57 -0400

    declare the direct hyperlink dependency
    already a transitive dependency via twisted

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ if (sys.version_info.major, sys.version_info.minor) < (3, 5):
 requirements = [
     'autobahn',
     'humanize',
+    'hyperlink',
     'magic-wormhole',
     'PyNaCl >= 1.2.0',  # 1.2.0 adds Argon2id KDF
     'PyQt5',
