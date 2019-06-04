12cf6af26e1ef5d0e4263e03fd8f57e08945d4f9
Chris Wood <chris@gridsync.io>
2019-06-04 16:54:55 -0400

    Add Python 3.7 to trove classifiers

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Communications :: File Sharing",
         "Topic :: Desktop Environment",
