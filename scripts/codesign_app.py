3b9361aafe357f9dd989e174641f26210ef4ba5f
Chris Wood <chris@gridsync.io>
2020-01-30 13:32:38 -0500

    Generalize codesign script; accept "app" and "dmg"

--- scripts/codesign_app.py
+++ scripts/codesign_app.py
@@ -3,42 +3,88 @@
 from configparser import RawConfigParser
 from pathlib import Path
 from subprocess import run
+import sys
 
 
-config = RawConfigParser(allow_no_value=True)
-config.read(Path("gridsync", "resources", "config.txt"))
-settings = {}
-for section in config.sections():
-    if section not in settings:
-        settings[section] = {}
-    for option, value in config.items(section):
-        settings[section][option] = value
+def codesign_app(developer_id: str, path: str) -> None:
+    run(
+        [
+            "codesign",
+            "--force",
+            "--deep",
+            f"--sign=Developer ID Application: {developer_id}",
+            "--options=runtime",
+            "--entitlements=misc/entitlements.plist",
+            path,
+        ]
+    )
 
-application_name = settings["application"]["name"]
-mac_developer_id = settings["sign"]["mac_developer_id"]
+def codesign_dmg(developer_id: str, path: str) -> None:
+    run(
+        [
+            "codesign",
+            "--force",
+            "--deep",
+            f"--sign=Developer ID Application: {developer_id}",
+            path,
+        ]
+    )
 
-run(
-    [
-        "codesign",
-        "--force",
-        "--deep",
-        f"--sign=Developer ID Application: {mac_developer_id}",
-        "--options=runtime",
-        "--entitlements=misc/entitlements.plist",
-        f"dist/{application_name}.app",
-    ]
-)
+def codesign_verify(path: str):
+    run(["codesign", "--verify", "--verbose=1", path])
 
-run(["codesign", "--verify", "--verbose=1", f"dist/{application_name}.app"])
+def codesign_display(path: str):
+    run(["codesign", "--display", "--verbose=4", path])
 
-run(["codesign", "--display", "--verbose=4", f"dist/{application_name}.app"])
+def spctl_assess_app(path: str):
+    run(
+        [
+            "spctl",
+            "-vv",
+            "--assess",
+            "--type=exec",
+            path,
+        ]
+    )
 
-run(
-    [
-        "spctl",
-        "--assess",
-        "--type=exec",
-        "--verbose=2",
-        f"dist/{application_name}.app",
-    ]
-)
+def spctl_assess_dmg(path: str):
+    run(
+        [
+            "spctl",
+            "-vv",
+            "--assess",
+            "--type=open",
+            "--context=context:primary-signature",
+            path,
+        ]
+    )
+
+
+if __name__ == "__main__":
+    config = RawConfigParser(allow_no_value=True)
+    config.read(Path("gridsync", "resources", "config.txt"))
+    settings = {}
+    for section in config.sections():
+        if section not in settings:
+            settings[section] = {}
+        for option, value in config.items(section):
+            settings[section][option] = value
+
+    application_name = settings["application"]["name"]
+    mac_developer_id = settings["sign"]["mac_developer_id"]
+
+    option = sys.argv[1]
+    if option == "app":
+        path = "dist/{application_name}.app"
+        codesign_app(mac_developer_id, path)
+        codesign_verify(path)
+        codesign_display(path)
+        spctl_assess_app(path)
+    elif option == "dmg":
+        path = "dist/{application_name}.dmg"
+        codesign_app(mac_developer_id, path)
+        codesign_verify(path)
+        codesign_display(path)
+        spctl_assess_dmg(path)
+    else:
+        sys.exit(f"Unknown option: {option}")
