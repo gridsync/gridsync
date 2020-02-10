8694a35d35c1101566338376af07f30dccd69988
Chris Wood <chris@gridsync.io>
2020-02-10 14:03:54 -0500

    Verify SHA256 hashes during notarization

--- scripts/notarize.py
+++ scripts/notarize.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+import hashlib
 import os
 import sys
 from configparser import RawConfigParser
 from pathlib import Path
 from subprocess import run, CalledProcessError, SubprocessError
 from time import sleep
+from typing import Optional
 
 altool = "/Applications/Xcode.app/Contents/Developer/usr/bin/altool"
 stapler = "/Applications/Xcode.app/Contents/Developer/usr/bin/stapler"
@@ -16,6 +18,14 @@ stapler = "/Applications/Xcode.app/Contents/Developer/usr/bin/stapler"
 
 # security unlock-keychain login.keychain
 # altool --store-password-in-keychain-item gridsync-notarization -u $APPLE_ID -p $APP_SPECIFIC_PASSWORD
+
+
+def sha256sum(filepath):
+    hasher = hashlib.sha256()
+    with open(filepath, "rb") as f:
+        for block in iter(lambda: f.read(4096), b""):
+            hasher.update(block)
+    return hasher.hexdigest()
 
 
 def make_zipfile(src_path: str, dst_path: str) -> None:
@@ -85,24 +95,42 @@ def staple(path: str) -> None:
         raise SubprocessError(f"Error stapling {path}")
 
 
-def notarize(path: str, bundle_id: str, username: str, password: str) -> str:
-    print(f"Uploading {path} for notarization...")
+def notarize(
+    path: str,
+    bundle_id: str,
+    username: str,
+    password: str,
+    sha256_hash: Optional[str] = None,
+) -> str:
+    if sha256_hash:
+        print(f"Uploading {path} ({sha256_hash}) for notarization...")
+    else:
+        print(f"Uploading {path} for notarization...")
     uuid = notarize_app(path, bundle_id, username, password)
     print(f"UUID is {uuid}")
+    hash_verified = False
     notarized = False
+    print("Awaiting response...")
     while not notarized:
-        print("Checking status...")
         results = notarization_info(uuid, username, password)
         print(results)
         status = results["Status"]
+        if sha256_hash and not hash_verified:
+            remote_hash = results.get("Hash")
+            if remote_hash:
+                if remote_hash == sha256_hash:
+                    hash_verified = True
+                    print("Hashes match.")
+                else:
+                    raise Exception(
+                        f"Hash mismatch! The local SHA256 hash ({sha256_hash})"
+                        f" does not match the remote file ({remote_hash})"
+                    )
         if status == "success":
-            print(results["Status Message"])
             notarized = True
         elif status == "invalid":
-            print(status)
             sys.exit(results["Status Message"])
         else:
-            print(status)
             sleep(20)
 
 
@@ -133,8 +161,9 @@ if __name__ == "__main__":
         notarize_path = f"dist/{application_name}.dmg"
         staple_path = f"dist/{application_name}.dmg"
 
+    sha256_hash = sha256sum(notarize_path)
     try:
-        notarize(notarize_path, bundle_id, username, password)
+        notarize(notarize_path, bundle_id, username, password, sha256_hash)
     except SubprocessError as err:
         sys.exit(str(err))
     try:
