e057a528b6c21a68d6ee70781edc5ba861d69d09
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2020-03-16 10:42:16 -0400

    Get the hash right
    Importantly, .git must agree between nix-prefetch-git and fetchgit

--- nix/zkapauthorizer.nix
+++ nix/zkapauthorizer.nix
@@ -3,8 +3,16 @@ let
 in
   pkgs.fetchgit {
     url = "https://github.com/PrivateStorageio/ZKAPAuthorizer";
+    # To update, see nix-prefetch-git.
+    # For example:
+    #
+    # nix-prefetch-git \
+    #    --leave-dotGit \
+    #    --rev ...rev... \
+    #    https://github.com/PrivateStorageio/ZKAPAuthorizer.git
     rev = "45a1ff7908eef5ef5224bd3a7ed12f5cf4fd6a4b";
-    sha256 = "0l7wp0bsg7ha8fkf7z4ilhfbmjnzzjd08znw8mbzja12r4fl7nmg";
+    sha256 = "189hd0g3yyp6vqc4xm2cd3ypnj4nibnl6yvm4vjcifz6waw76a2l";
+
     # ZKAPAuthorizer uses Versioneer which requires a .git at build time.
     leaveDotGit = true;
   }
