91ea8495e5bf36fb26b0953dc29dea7721235689
Chris Wood <chris@gridsync.io>
2020-03-18 16:35:28 -0400

    Remove isVisible() condition

--- gridsync/gui/zkap.py
+++ gridsync/gui/zkap.py
@@ -207,12 +207,11 @@ class ZKAPInfoPane(QWidget):
 
     @Slot(list)
     def on_unpaid_vouchers_updated(self, vouchers):
-        if self.isVisible():  # XXX
-            if vouchers:
-                self.button.hide()
-                self.voucher_link.hide()
-                self.pending_label.show()
-            else:
-                self.pending_label.hide()
-                self.button.show()
-                self.voucher_link.show()
+        if vouchers:
+            self.button.hide()
+            self.voucher_link.hide()
+            self.pending_label.show()
+        else:
+            self.pending_label.hide()
+            self.button.show()
+            self.voucher_link.show()
