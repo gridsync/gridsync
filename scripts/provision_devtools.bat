741e5d06cf5b392c439dfd4f5e27561aed5739e4
Chris Wood <chris@gridsync.io>
2020-07-14 10:20:12 -0400

    Update python interpreters in provisioning scripts

--- scripts/provision_devtools.bat
+++ scripts/provision_devtools.bat
@@ -1,8 +1,8 @@
 choco install -y --no-progress --require-checksums git
 choco install -y --no-progress --require-checksums python2
 choco install -y --no-progress --require-checksums vcpython27
-choco install -y --no-progress --require-checksums -m python3 --version 3.8.2
-choco install -y --no-progress --require-checksums -m python3 --version 3.7.7
+choco install -y --no-progress --require-checksums -m python3 --version 3.8.4
+choco install -y --no-progress --require-checksums -m python3 --version 3.7.8
 choco install -y --no-progress --require-checksums -m python3 --version 3.6.8
 choco install -y --no-progress --require-checksums visualcpp-build-tools
 choco install -y --no-progress --require-checksums innosetup
