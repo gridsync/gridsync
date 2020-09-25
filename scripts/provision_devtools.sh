1ec8d29b8b23e576e7c6b20b0b76a1c7ff8f9769
Chris Wood <chris@gridsync.io>
2020-09-25 10:54:59 -0400

    Use provision_devtools.sh for all GHA dependencies

--- scripts/provision_devtools.sh
+++ scripts/provision_devtools.sh
@@ -12,7 +12,7 @@ if [ "$(uname)" = "Darwin" ]; then
 else
     if [ -f "/usr/bin/apt-get" ]; then
         sudo apt-get -y update
-        sudo apt-get -y install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev git xvfb libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0
+        sudo apt-get -y install --no-install-recommends make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev git xvfb libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libgl1 libgl1-mesa-dev x11-utils libdbus-1-3 libxcb-xfixes0
         SHELLRC=~/.bash_profile
     elif [ -f "/usr/bin/yum" ]; then
         sudo yum -y install gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz git xorg-x11-server-Xvfb
