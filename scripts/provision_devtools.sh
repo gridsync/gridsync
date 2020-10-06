a8db66ca63684d4acc7df35424db725fe5b38f1f
Chris Wood <chris@gridsync.io>
2020-10-06 18:15:57 -0400

    Cleanup

--- scripts/provision_devtools.sh
+++ scripts/provision_devtools.sh
@@ -41,8 +41,6 @@ echo "$ECHO_FLAGS" 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv 
 
 . "$SHELLRC"
 
-pyenv rehash
-pyenv install --list
 pyenv install --skip-existing 2.7.18
 pyenv install --skip-existing 3.9.0
 pyenv install --skip-existing 3.8.6
