3f64c86dea2babccd93e33a9b91a547667a28294
Chris Wood <chris@gridsync.io>
2020-11-16 14:30:30 -0500

    Cleanup

--- gridsync/gui/qrcode.py
+++ gridsync/gui/qrcode.py
@@ -11,18 +11,3 @@ class QRCode(QImage):
         segno.make_qr(data).save(buffer, kind="png")
         buffer.seek(0)
         self.loadFromData(buffer.read())
-
-
-# XXX
-if __name__ == "__main__":
-    import sys
-    from PyQt5.QtWidgets import QApplication, QGridLayout, QLabel, QWidget
-    from PyQt5.QtGui import QPixmap
-    app = QApplication(sys.argv)
-    w = QWidget()
-    layout = QGridLayout(w)
-    label = QLabel()
-    label.setPixmap(QPixmap(QRCode("TEST").scaled(256, 256)))
-    layout.addWidget(label)
-    w.show()
-    sys.exit(app.exec_())
