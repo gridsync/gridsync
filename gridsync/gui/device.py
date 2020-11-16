4e91cf75558a4fa1a12537befb8a9ef7557573df
Chris Wood <chris@gridsync.io>
2020-11-16 15:52:28 -0500

    Remove unused import

--- gridsync/gui/device.py
+++ gridsync/gui/device.py
@@ -1,7 +1,6 @@
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QDialog, QGridLayout, QLabel, QPushButton
 
-from gridsync.bridge import get_local_network_ip
 from gridsync.gui.qrcode import QRCode
 
 
