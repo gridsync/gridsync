80edf51e05a524d3513a4ced1d7ff0179c866084
Chris Wood <chris@gridsync.io>
2021-01-22 13:17:08 -0500

    Add checksum, validity check to generate_vouchers

--- scripts/generate_vouchers.py
+++ scripts/generate_vouchers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import base64
+import binascii
 import hashlib
 import os
 
@@ -15,15 +16,41 @@ def generate_voucher(data: Optional[bytes] = b"") -> str:
     return base64.urlsafe_b64encode(digest).decode("utf-8")
 
 
-def dashify(s):
+def dashify(s: str) -> str:
     return "-".join([s[i : i + 4] for i in range(0, len(s), 4)])
+
+
+def undashify(s: str) -> str:
+    return s.replace("-", "")
+
+
+def get_checksum(b: bytes, length: int = 2) -> bytes:
+    return hashlib.blake2b(b, digest_size=length).digest()
+
+
+def generate_code() -> bytes:
+    b = os.urandom(8)
+    checksum = get_checksum(b)
+    return base64.b32encode(b + checksum)
+
+
+def is_valid(code: str, checksum_length: int = 2) -> bool:
+    code = undashify(code)
+    try:
+        decoded = base64.b32decode(code)
+    except binascii.Error:
+        return False
+    b = decoded[:-checksum_length]
+    checksum = decoded[-checksum_length:]
+    if checksum == get_checksum(b):
+        return True
+    return False
 
 
 if __name__ == "__main__":
     for _ in range(100):
-        b = base64.b32encode(os.urandom(10))
-        print("{} -> {}".format(dashify(b.decode()), generate_voucher(b)))
+        code = generate_code()
+        print("{} -> {}".format(dashify(code.decode()), generate_voucher(code)))
 
 # TODO:
-# Append checksum chars to human/user-facing code?
 # Use different alphabet with fewer ambiguous chars?
