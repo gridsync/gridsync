46c1e691e5d0ef944ff95c3704d2cc73224fa17c
Chris Wood <chris@gridsync.io>
2021-03-01 14:02:22 -0500

    Unset BUILDBOT_* env vars before launching
    To prevent these from being leaked, e.g., in the buildbot web UI

--- scripts/provision_buildbot-worker.sh
+++ scripts/provision_buildbot-worker.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 python2 -m pip install buildbot-worker
 buildbot-worker create-worker ~/buildbot "$BUILDBOT_HOST" "$BUILDBOT_NAME" "$BUILDBOT_PASS"
+unset BUILDBOT_HOST
+unset BUILDBOT_NAME
+unset BUILDBOT_PASS
 buildbot-worker restart ~/buildbot
