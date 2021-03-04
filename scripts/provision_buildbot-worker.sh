cccae9e3513387c80409f0c1a8667cf61d8321bc
Chris Wood <chris@gridsync.io>
2021-03-03 21:13:44 -0500

    Use python3 when provisioning buildbot-worker

--- scripts/provision_buildbot-worker.sh
+++ scripts/provision_buildbot-worker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-python2 -m pip install buildbot-worker
+python3 -m pip install buildbot-worker
 buildbot-worker create-worker ~/buildbot "$BUILDBOT_HOST" "$BUILDBOT_NAME" "$BUILDBOT_PASS"
 unset BUILDBOT_HOST
 unset BUILDBOT_NAME
