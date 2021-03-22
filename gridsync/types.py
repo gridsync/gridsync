ccf270b06609899f864c8089d152c57fc8db039e
Chris Wood <chris@gridsync.io>
2021-03-22 15:37:22 -0400

    Add comment re. "TwistedDeferred" type

--- gridsync/types.py
+++ gridsync/types.py
@@ -6,4 +6,7 @@ A = TypeVar("A")
 
 TreqResponse = _Response
 
+# This can probably be removed with the next Twisted release (after
+# 21.2.0), since type hints have been aded for twisted.internet.defer:
+# https://github.com/twisted/twisted/pull/1448
 TwistedDeferred = Generator[int, None, A]
