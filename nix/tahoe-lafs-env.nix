905666e9cdd58e30a8364bb513029503a089beb0
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-05-12 14:38:57 -0400

    Switch to in-nixpkgs ZKAPAuthorizer

--- nix/tahoe-lafs-env.nix
+++ nix/tahoe-lafs-env.nix
@@ -1,22 +1,15 @@
 { pkgs }:
 let
-  # Notice that ZKAPAuthorizer and the Tahoe-LAFS environment are built with
-  # Python 2 because Tahoe-LAFS has not yet been ported to Python 3.  We have
-  # to be careful not to mix these into the GridSync Python environment
-  # because Python 2 and Python 3 stuff conflicts.
-  zkapauthorizer = pkgs.python2Packages.callPackage (import ./zkapauthorizer.nix) {
-    # Avoid passing tahoe-lafs from pkgs.python2Packages so that
-    # ZKAPAuthorizer feels free to do its own thing on that front.
-    tahoe-lafs = null;
-  };
+  # For now GridSync requires this older tahoe-lafs because it still includes
+  # magic-folder.
+  tahoe-lafs = pkgs.python2Packages.tahoe-lafs-1_14;
   tahoe-lafs-env = pkgs.python2.buildEnv.override {
     # twisted plugins causes collisions between any packages that supply
     # plugins.
     ignoreCollisions = true;
     extraLibs = [
-      # zkapauthorizer pulls in tahoe-lafs.  It might be better to reference
-      # it explicitly here but I'm not sure how to reach it.
-      zkapauthorizer
+      tahoe-lafs
+      (pkgs.python2Packages.zkapauthorizer.override { inherit tahoe-lafs; })
     ];
   };
 in
