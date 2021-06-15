09b967122c83a6c78f58b1848a518906c6e630f5
Chris Wood <chris@gridsync.io>
2021-06-14 23:51:10 -0400

    Add some magic-folder integration tests (WIP)

---
+++ tests/integration/test_magic_folder_integration.py
@@ -0,0 +1,60 @@
+import secrets
+import string
+
+from pytest_twisted import async_yield_fixture, inlineCallbacks
+
+
+def randstr(length: int = 32, alphabet: str = "") -> str:
+    if not alphabet:
+        alphabet = string.ascii_letters + string.digits
+    return "".join(secrets.choice(alphabet) for i in range(length))
+
+
+@async_yield_fixture(scope="module")
+async def magic_folder(tahoe_client):
+    mf = tahoe_client.magic_folder
+    await mf.start()
+    yield mf
+    mf.stop()
+
+
+@inlineCallbacks
+def test_version(magic_folder):
+    output = yield magic_folder.version()
+    assert output.startswith("Magic")
+
+
+@inlineCallbacks
+def test_add_folder_and_get_folder(magic_folder, tmp_path):
+    folder_name = randstr()
+    path = tmp_path / folder_name
+    author = randstr()
+    yield magic_folder.add_folder(path, author)
+
+    folders = yield magic_folder.get_folders()
+    assert folder_name in folders
+
+
+@inlineCallbacks
+def test_get_participants(magic_folder, tmp_path):
+    folder_name = randstr()
+    path = tmp_path / folder_name
+    author = randstr()
+    yield magic_folder.add_folder(path, author)
+
+    participants = yield magic_folder.get_participants(folder_name)
+    assert author in participants
+
+
+@inlineCallbacks
+def test_add_participant(magic_folder, tmp_path):
+    folder_name = randstr()
+    path = tmp_path / folder_name
+    author = randstr()
+    yield magic_folder.add_folder(path, author)
+
+    author_name = randstr()
+    personal_dmd = yield magic_folder.gateway.mkdir()
+    yield magic_folder.add_participant(folder_name, author_name, personal_dmd)
+    participants = yield magic_folder.get_participants(folder_name)
+    assert author_name in participants
