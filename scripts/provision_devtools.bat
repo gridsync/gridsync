42079f4fca77229121c2643b601e51291e38a9f2
Chris Wood <chris@gridsync.io>
2021-06-15 15:03:59 -0400

    Call `refreshenv` after installing choco packages

--- scripts/provision_devtools.bat
+++ scripts/provision_devtools.bat
@@ -5,6 +5,7 @@ choco install -y --no-progress --require-checksums -m python3 --version 3.8.10
 choco install -y --no-progress --require-checksums -m python3 --version 3.7.9
 choco install -y --no-progress --require-checksums visualcpp-build-tools
 choco install -y --no-progress --require-checksums innosetup
+refreshenv
 py -2 -m pip install --upgrade setuptools pip
 py -3 -m pip install --upgrade setuptools pip tox diffoscope
 git config --global core.autocrlf false
