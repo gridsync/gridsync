47205f2a531b70769fd46e2bd0eb33af746cf35d
Chris Wood <chris@gridsync.io>
2021-06-17 14:31:25 -0400

    Add some basic tahoe integration tests

---
+++ tests/integration/test_tahoe_integration.py
@@ -0,0 +1,22 @@
+import os
+
+from pytest_twisted import inlineCallbacks
+
+os.environ["PATH"] = (
+    os.path.join(os.getcwd(), "dist", "Tahoe-LAFS")
+    + os.pathsep
+    + os.environ["PATH"]
+)
+
+
+@inlineCallbacks
+def test_tahoe_client_connected_servers(tahoe_client):
+    yield tahoe_client.await_ready()
+    connected_servers = yield tahoe_client.get_connected_servers()
+    assert connected_servers == 1
+
+
+@inlineCallbacks
+def test_tahoe_client_mkdir(tahoe_client):
+    cap = yield tahoe_client.mkdir()
+    assert cap.startswith("URI:DIR2:")
