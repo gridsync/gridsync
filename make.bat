3a1b9e4e1e1f6130b72e091eca192fced4cc50a8
Chris Wood <chris@gridsync.io>
2021-06-17 17:34:37 -0400

    Add "test-integration" make target

--- make.bat
+++ make.bat
@@ -35,6 +35,7 @@ if defined APPVEYOR (
 
 if "%1"=="clean" call :clean
 if "%1"=="test" call :test
+if "%1"=="test-integration" call :test-integration
 if "%1"=="frozen-tahoe" call :frozen-tahoe
 if "%1"=="pyinstaller" call :pyinstaller
 if "%1"=="zip" call :zip
@@ -61,6 +62,10 @@ goto :eof
 
 :test
 %PYTHON3% -m tox || goto :error
+goto :eof
+
+:test-integration
+%PYTHON3% -m tox -e integration || goto :error
 goto :eof
 
 :frozen-tahoe
