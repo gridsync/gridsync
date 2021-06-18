30284095175dc44a1695b82d74c33d2525be1813
Chris Wood <chris@gridsync.io>
2021-06-17 20:19:47 -0400

    Remove superfluous "diminish" method

--- gridsync/tahoe.py
+++ gridsync/tahoe.py
@@ -1117,11 +1117,6 @@ class Tahoe:
             return json.loads(content.decode("utf-8"))
         return None
 
-    @inlineCallbacks
-    def diminish(self, cap: str) -> TwistedDeferred[str]:
-        output = yield self.get_json(cap)
-        return output[1]["ro_uri"]
-
     def get_rootcap(self):
         if not self.rootcap:
             self.rootcap = self.read_cap_from_file(self.rootcap_path)
