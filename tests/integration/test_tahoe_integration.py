a7bd51f6bf94932a411f425045e1681b4b2f2648
Chris Wood <chris@gridsync.io>
2021-06-29 18:27:05 -0400

    Add integration test to test convergent encryption
    To verify that the filecap is derived deterministically from the file
    contents and the convergence secret

--- tests/integration/test_tahoe_integration.py
+++ tests/integration/test_tahoe_integration.py
@@ -27,3 +27,16 @@ def test_diminish(tahoe_client):
     dircap = yield tahoe_client.mkdir()
     diminished = yield tahoe_client.diminish(dircap)
     assert diminished.startswith("URI:DIR2-RO:")
+
+
+@inlineCallbacks
+def test_upload_convergence_secret_determines_cap(tahoe_client, tmp_path):
+    convergence_secret = tahoe_client.settings.get("convergence")
+    p = tmp_path / "TestFile.txt"
+    p.write_bytes(b"0" * 64)
+    cap = yield tahoe_client.upload(p.resolve())
+    assert (convergence_secret, cap) == (
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+        "URI:CHK:rowlspe46wotpra7jruhtad3xy:"
+        "lnviruztzbcugtpkrxnnodehpstlcoo6pswfgqjhv3teyn656fja:1:1:64",
+    )
