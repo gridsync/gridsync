60e4b0367a18af72f76bc28cbadafffaae799278
Chris Wood <chris@gridsync.io>
2021-07-05 19:07:07 -0400

    Add test to catch, show recovery key OSErrors

--- tests/test_recovery.py
+++ tests/test_recovery.py
@@ -22,3 +22,12 @@ def test_show_error_msg_if_recovery_file_is_a_directory(
     filepath = tmp_path_factory.mktemp("Test.app")
     RecoveryKeyImporter().do_import(str(filepath))
     assert fake_error.call_args[0][1] == "Error loading Recovery Key"
+
+
+def test_show_error_msg_if_os_error_raises(monkeypatch, tmp_path):
+    fake_error = Mock()
+    monkeypatch.setattr("gridsync.recovery.error", fake_error)
+    monkeypatch.setattr("builtins.open", Mock(side_effect=OSError("!Error!")))
+    filepath = tmp_path / "test"
+    RecoveryKeyImporter().do_import(str(filepath))
+    assert fake_error.call_args[0][2] == "!Error!"
