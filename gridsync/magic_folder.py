9fe50ce3d446fc4d6e3f3f81a21a6d95b35a4cb0
Chris Wood <chris@gridsync.io>
2021-07-08 09:38:25 -0400

    Try increasing wait time before win32 cleanup

--- gridsync/magic_folder.py
+++ gridsync/magic_folder.py
@@ -308,7 +308,7 @@ class MagicFolder:
         logging.debug("Restarting magic-folder...")
         self.stop()
         if sys.platform == "win32":
-            yield deferLater(reactor, 2, lambda: None)
+            yield deferLater(reactor, 20, lambda: None)
             self._win32_remove_stale_dirs()
         yield self.start()
         logging.debug("Magic-folder restarted successfully")
