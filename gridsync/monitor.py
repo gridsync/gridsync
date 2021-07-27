024b74c83b12e92f28ad53c7397db7ded4cce6ba
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2021-07-27 09:19:53 -0400

    maybe you'll show me the output if something blows up

--- gridsync/monitor.py
+++ gridsync/monitor.py
@@ -445,6 +445,7 @@ class ZKAPChecker(QObject):
         if remaining and not total:
             print("Remaining: {!r} total: {!r}".format(remaining, total))
             total = self._maybe_load_last_total()
+            1 / 0
         if not total or remaining > total:
             # When redeeming tokens in batches, ZKAPs become available
             # during the "redeeming" state but the *total* number is
