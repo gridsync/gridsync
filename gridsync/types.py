1462491b23bb4b852614cdbf51b0d3dfaab76c25
Chris Wood <chris@gridsync.io>
2021-07-30 11:50:42 -0400

    Update Deferred type wrapper for Twisted 21.7.0
    Twisted 21.7.0 adds type hinting for twisted.internet.defer, changing
    the behavior of this supertype. Unfortunately, it appears that mypy
    still expects Generators for any functions decorated by inlineCallbacks
    (so a bare Deferreds can't be used as a return type annotations in such
    cases -- and thus this wrapper/workaround is still needed).

--- gridsync/types.py
+++ gridsync/types.py
@@ -2,12 +2,12 @@
 from typing import Any, Generator, TypeVar
 
 from treq.response import _Response
+from twisted.internet.defer import Deferred
 
 A = TypeVar("A")
 
 TreqResponse = _Response
 
-# This can probably be removed with the next Twisted release (after
-# 21.2.0), since type hints have been aded for twisted.internet.defer:
-# https://github.com/twisted/twisted/pull/1448
-TwistedDeferred = Generator[int, Any, A]
+# For use with Twisted's inlineCallbacks decorator; where mypy expects
+# a Generator
+TwistedDeferred = Generator[Deferred[Any], Any, A]
