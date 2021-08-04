1dcb3163e80522988948d5f837f2c704cf880630
Chris Wood <chris@gridsync.io>
2021-08-03 21:30:05 -0400

    Remove usused import

--- gridsync/backup.py
+++ gridsync/backup.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 from pathlib import Path
-from typing import TYPE_CHECKING, Dict
+from typing import TYPE_CHECKING
 
 from atomicwrites import atomic_write
 from twisted.internet.defer import inlineCallbacks
