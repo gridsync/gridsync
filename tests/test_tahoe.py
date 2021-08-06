9c8c34f889cc66f3dd9e69efe00000cab840fd4c
Chris Wood <chris@gridsync.io>
2021-08-05 20:09:18 -0400

    Fix broken attribute in lock test

--- tests/test_tahoe.py
+++ tests/test_tahoe.py
@@ -422,7 +422,7 @@ def test_tahoe_stop_locked(locked, call_count, tahoe, monkeypatch):
     lock.locked = locked
     lock.acquire = MagicMock()
     lock.release = MagicMock()
-    tahoe.lock = lock
+    tahoe.backup_manager.lock = lock
     monkeypatch.setattr("os.path.isfile", lambda x: True)
     monkeypatch.setattr("sys.platform", "linux")
     monkeypatch.setattr("gridsync.tahoe.Tahoe.command", MagicMock())
