4098b2d6219f9448b37ca4f5e1fe701e0a8f06f4
Chris Wood <chris@gridsync.io>
2021-08-07 15:41:26 -0400

    Clean up

--- tests/integration/test_magic_folder_integration.py
+++ tests/integration/test_magic_folder_integration.py
@@ -260,24 +260,6 @@ def test_scan(magic_folder, tmp_path):
     assert output == {}
 
 
-# @inlineCallbacks
-# def test_create_backup_cap(magic_folder):
-#    cap = yield magic_folder.create_backup_cap()
-#    assert cap.startswith("URI:DIR2")
-
-
-# @inlineCallbacks
-# def test_get_backup_cap(magic_folder):
-#    cap = yield magic_folder.get_backup_cap()
-#    assert cap.startswith("URI:DIR2")
-
-
-# @inlineCallbacks
-# def test_store_backup_cap_as_attribute(magic_folder):
-#    cap = yield magic_folder.get_backup_cap()
-#    assert magic_folder.backup_cap == cap
-
-
 @inlineCallbacks
 def test_create_folder_backup(magic_folder):
     folders = yield magic_folder.get_folders()
