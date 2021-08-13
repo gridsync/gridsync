65e469f9d2c83ddaacec10282817a958dd466ca5
Tom Prince <tom.prince@private.storage>
2021-08-12 19:33:55 -0600

    Update pyqtchart nix expression.

--- nix/pyqtchart.nix
+++ nix/pyqtchart.nix
@@ -8,12 +8,12 @@
 , lib
 }:
 let
-  inherit (python3Packages) python pyqt5 buildPythonPackage;
+  inherit (python3Packages) python pyqt5 buildPythonPackage pyqt-builder;
   inherit (pyqt5) sip;
 in buildPythonPackage rec {
   pname = "PyQtChart";
   version = "5.15.4";
-  format = "other";
+  format = "pyproject";
 
   src = fetchurl {
     url = "https://files.pythonhosted.org/packages/e6/af/dd493297922be2935ae2de34daea818940c4f747a98d09acaaa5e84cd1dd/${pname}-${version}.tar.gz";
@@ -26,6 +26,7 @@ in buildPythonPackage rec {
     sip
     qtbase
     qtcharts
+    pyqt-builder
   ];
   buildInputs = [
     sip
@@ -36,40 +37,13 @@ in buildPythonPackage rec {
     pyqt5
   ];
 
-  configurePhase = ''
-    runHook preConfigure
 
-    mkdir -p "$out/share/sip/PyQt5"
+  # Avoid running qmake, which is in nativeBuildInputs
+  dontConfigure = true;
 
-    # --no-dist-info disables mk_distinfo which wants to write dist-info to
-    # the wrong place.  Fortunately the minimal dist-info required is simple so
-    # we'll generate it ourselves below.
-    ${python.executable} configure.py -w \
-      --no-dist-info \
-      --destdir="$out/${python.sitePackages}/PyQt5" \
-      --apidir="$out/api/${python.libPrefix}" \
-      --qtchart-sipdir="$out/share/sip/PyQt5" \
-      --pyqt-sipdir="${pyqt5}/share/sip/PyQt5" \
-      --stubsdir="$out/${python.sitePackages}/PyQt5"
-
-    runHook postConfigure
-  '';
-
-  postInstall = ''
-    # Let's make it a namespace package
-    cat << EOF > $out/${python.sitePackages}/PyQt5/__init__.py
-    from pkgutil import extend_path
-    __path__ = extend_path(__path__, __name__)
-    EOF
-
-    # Generate the minimal dist-info required to make the package
-    # discoverable.
-    mkdir -p $out/${python.sitePackages}/${pname}-${version}.dist-info
-    cat << EOF > $out/${python.sitePackages}/${pname}-${version}.dist-info/METADATA
-    Metadata-Version: 1.2
-    Name: ${pname}
-    Version: ${version}
-    EOF
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace "[tool.sip.project]" "[tool.sip.project]''\nsip-include-dirs = [\"${pyqt5}/${python.sitePackages}/PyQt5/bindings\"]"
   '';
 
   installCheckPhase = let
