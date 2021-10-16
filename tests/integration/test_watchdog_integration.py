8c6386fc95a3fdc5ccb98f5765188552d8332425
Chris Wood <chris@gridsync.io>
2021-10-16 14:26:32 -0400

    Add a skip for GitHub Actions

--- tests/integration/test_watchdog_integration.py
+++ tests/integration/test_watchdog_integration.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from gridsync.watchdog import Watchdog
@@ -11,6 +13,9 @@ def watchdog():
     wd.stop()
 
 
+@pytest.mark.skipif(
+    "GITHUB_ACTIONS" in os.environ, reason="Flakey on public infrastructure"
+)
 def test_watchdog_emits_path_modified_signal(watchdog, tmp_path, qtbot):
     watchdog.add_watch(str(tmp_path))
     with qtbot.wait_signal(watchdog.path_modified) as blocker:
