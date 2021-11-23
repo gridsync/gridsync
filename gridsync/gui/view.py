c18036aa4e708814a09b0cf8a05fdf06b42fa3fe
Chris Wood <chris@gridsync.io>
2021-11-23 16:49:27 -0500

    Update View._get_magic_folder_directory method

--- gridsync/gui/view.py
+++ gridsync/gui/view.py
@@ -351,9 +351,8 @@ class View(QTreeView):
             d.addCallback(self.maybe_rescan_rootcap)
 
     def _get_magic_folder_directory(self, folder_name: str) -> str:
-        legacy_data = self.gateway.magic_folders.get(folder_name, {})
         data = self.gateway.magic_folder.magic_folders.get(folder_name, {})
-        return str(data.get("magic_path", legacy_data.get("directory", "")))
+        return str(data.get("magic_path", ""))
 
     def open_folders(self, folders):
         for folder in folders:
