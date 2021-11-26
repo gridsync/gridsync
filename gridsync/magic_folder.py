8f1b0bcef83c78c289e35ee2452c8dd17f657b61
Chris Wood <chris@gridsync.io>
2021-11-25 21:18:04 -0500

    Use `tahoe-objects` endpoint to get object sizes
    Now that https://github.com/LeastAuthority/magic-folder/issues/570 has
    been fixed -- thanks, @meejah! :)

--- gridsync/magic_folder.py
+++ gridsync/magic_folder.py
@@ -680,14 +680,9 @@ class MagicFolder:
 
     @inlineCallbacks
     def get_object_sizes(self, folder_name: str) -> TwistedDeferred[List[int]]:
-        # XXX A placeholder for now...
-        # See https://github.com/LeastAuthority/magic-folder/pull/528
-        # and https://github.com/LeastAuthority/magic-folder/issues/570
-        sizes = []
-        file_status = yield self.get_file_status(folder_name)
-        for item in file_status:
-            # Include size of content, snapshot cap, and metadata cap
-            sizes.extend([item.get("size", 0), 420, 184])
+        sizes = yield self._request(
+            "GET", f"/magic-folder/{folder_name}/tahoe-objects"
+        )
         return sizes
 
     @inlineCallbacks
