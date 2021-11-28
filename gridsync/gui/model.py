2588a186e655ad762aceb78764a69318f0bb5cdb
Chris Wood <chris@gridsync.io>
2021-11-27 22:49:41 -0500

    Clean up

--- gridsync/gui/model.py
+++ gridsync/gui/model.py
@@ -54,13 +54,7 @@ class Model(QStandardItemModel):
         self.monitor.disconnected.connect(self.on_disconnected)
         self.monitor.nodes_updated.connect(self.on_nodes_updated)
         self.monitor.space_updated.connect(self.on_space_updated)
-        # self.monitor.status_updated.connect(self.set_status)
-        # self.monitor.members_updated.connect(self.on_members_updated)
         self.monitor.check_finished.connect(self.update_natural_times)
-        # self.monitor.remote_folder_added.connect(self.add_remote_folder)
-        # self.monitor.transfer_progress_updated.connect(
-        #    self.set_transfer_progress
-        # )
 
         self.mf_monitor = self.gateway.magic_folder.monitor
         self.mf_monitor.folder_added.connect(
