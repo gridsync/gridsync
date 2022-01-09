19dc8a30c996886b2b571e7bcd6ce5ef0cb6f4d9
Chris Wood <chris@gridsync.io>
2022-01-09 17:00:08 -0500

    Add some ZKAPAuthorizer plugin/integration tests
    Ensure the ZKAPAuthorizer plugin loads correctly

---
+++ tests/integration/test_zkapauthorizer_plugin.py
@@ -0,0 +1,53 @@
+from pytest_twisted import async_yield_fixture, inlineCallbacks
+
+from gridsync.tahoe import Tahoe
+
+
+@async_yield_fixture(scope="module")
+async def zkapauthorizer(tmp_path_factory, tahoe_server):
+    client = Tahoe(tmp_path_factory.mktemp("tahoe_client") / "nodedir")
+    print("ZKAPAuthorizer-enabled client nodedir:", client.nodedir)
+    settings = {
+        "nickname": "ZKAPAuthorizer-enabled Test Grid",
+        "shares-needed": "1",
+        "shares-happy": "1",
+        "shares-total": "1",
+        "storage": {
+            "test-grid-storage-server-1": {
+                "anonymous-storage-FURL": "pb://@tcp:/",
+                "nickname": "test-grid-storage-server-1",
+                "storage-options": [
+                    {
+                        "name": "privatestorageio-zkapauthz-v1",
+                        "ristretto-issuer-root-url": "https://example.org/",
+                        "storage-server-FURL": tahoe_server.storage_furl,
+                        "allowed-public-keys": "AAAAAAAAAAAAAAAA",
+                    }
+                ],
+            }
+        },
+    }
+    await client.create_client(**settings)
+    client.save_settings(settings)
+    await client.start()
+    yield client.zkapauthorizer
+    await client.stop()
+
+
+@inlineCallbacks
+def test_zkapauthorizer_version(zkapauthorizer):
+    version = yield zkapauthorizer.get_version()
+    assert version != ""
+
+
+@inlineCallbacks
+def test_zkapauthorizer_add_and_get_voucher(zkapauthorizer):
+    voucher = yield zkapauthorizer.add_voucher()
+    output = yield zkapauthorizer.get_voucher(voucher)
+    assert output["number"] == voucher
+
+
+@inlineCallbacks
+def test_zkapauthorizer_calculate_price(zkapauthorizer):
+    output = yield zkapauthorizer.calculate_price([1024, 2048, 3072, 4096])
+    assert output["price"] == 4
