1c9cbda4c068ee5a9078652fc91e45365d0a1666
Chris Wood <chris@gridsync.io>
2022-01-10 23:46:26 -0500

    Remove python2 while we're at it

--- scripts/provision_devtools.bat
+++ scripts/provision_devtools.bat
@@ -1,12 +1,10 @@
 choco install -y --no-progress --require-checksums git
-choco install -y --no-progress --require-checksums python2
 choco install -y --no-progress --require-checksums -m python3 --version 3.9.7
 choco install -y --no-progress --require-checksums -m python3 --version 3.8.10
 choco install -y --no-progress --require-checksums -m python3 --version 3.7.9
 choco install -y --no-progress --require-checksums visualcpp-build-tools
 choco install -y --no-progress --require-checksums innosetup
 refreshenv
-py -2 -m pip install --upgrade setuptools pip
 py -3 -m pip install --upgrade setuptools pip tox diffoscope
 git config --global core.autocrlf false
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\FileSystem" /v LongPathsEnabled /t REG_DWORD /d 1 /f
