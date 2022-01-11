0d626659b4fb3c58208a698bc3026959014ab275
Chris Wood <chris@gridsync.io>
2022-01-10 23:40:25 -0500

    Set LongPathsEnabled via provision script

--- scripts/provision_devtools.bat
+++ scripts/provision_devtools.bat
@@ -9,3 +9,4 @@ refreshenv
 py -2 -m pip install --upgrade setuptools pip
 py -3 -m pip install --upgrade setuptools pip tox diffoscope
 git config --global core.autocrlf false
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\FileSystem" /v LongPathsEnabled /t REG_DWORD /d 1 /f
