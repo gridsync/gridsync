fd4d4f827872ea71a7988bc181c0152c3415685d
Chris Wood <chris@gridsync.io>
2022-01-16 13:41:38 -0500

    Include commit message from 07a00a2 as a comment

--- scripts/reproducible-pip.py
+++ scripts/reproducible-pip.py
@@ -18,5 +18,13 @@ else:
     # Disable debug information.
     os.environ["CFLAGS"] = "-g0"
 
+
+# When `--require-hashes` is passed to pip, it will not use locally built
+# wheels in its cache, for packages that are only available as sdists. This
+# causes it to always rebuild those packages from source. Since extension
+# modules (like zfec and netifaces) embeded the build directory, which is
+# randomly generated, while the set of *packages* is reproducible, the
+# installed modules aren't. To work around this, we add a wrapper around pip,
+# that forces it to use reproducible temporary directory names.
 tempfile._get_candidate_names().rng.seed(0)
 pip.main()
