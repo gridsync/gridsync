94e0948f3889be21298496615abd7ff645d0680e
Chris Wood <chris@gridsync.io>
2022-01-24 09:55:12 -0500

    Try reproducing a certificate verification failure

--- tahoe.py
+++ tahoe.py
@@ -11,6 +11,7 @@ from future.utils import PY2
 if PY2:
     from future.builtins import filter, map, zip, ascii, chr, hex, input, next, oct, open, pow, round, super, bytes, dict, list, object, range, str, max, min  # noqa: F401
 
+
 import sys
 
 print("#####################################################################")
@@ -19,7 +20,19 @@ print("certifi.where() = ", certifi.where())
 pprint.pprint(dict(os.environ))
 print("#####################################################################")
 
+
+from twisted.internet.task import react
+from twisted.internet.defer import inlineCallbacks
+import treq
+
+@inlineCallbacks
+def main(reactor):
+    resp = yield treq.get("https://github.com")
+    print(resp.code)
+
+
 from allmydata.scripts.runner import run
 
 if __name__ == "__main__":
-    sys.exit(run())
+    #sys.exit(run())
+    react(main)
