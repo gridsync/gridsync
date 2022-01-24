143791b4033911bfaf90853e474621b443999469
Chris Wood <chris@gridsync.io>
2022-01-24 17:35:16 -0500

    Try the fix

--- tests/integration/test_tls.py
+++ tests/integration/test_tls.py
@@ -1,8 +1,14 @@
+import os
+import sys
+
+import cerctifi
 import treq
 from pytest_twisted import inlineCallbacks
 
 
 @inlineCallbacks
 def test_tls(reactor):
+    if sys.platform == "win32":
+        os.environ["SSL_CERT_FILE"] = certifi.where()
     resp = yield treq.get("https://github.com")
     assert resp.code == 200
