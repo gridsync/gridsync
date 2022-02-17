73250aefd2f25cb47d670e364d4b6543c6187b07
Chris Wood <chris@gridsync.io>
2022-02-17 16:24:35 -0500

    Use a more reliable "program" in supervisor tests

--- tests/test_supervisor.py
+++ tests/test_supervisor.py
@@ -1,3 +1,5 @@
+import sys
+
 from pytest_twisted import inlineCallbacks
 from twisted.internet import reactor
 from twisted.internet.task import deferLater
@@ -5,12 +7,14 @@ from twisted.internet.task import deferLater
 from gridsync.supervisor import Supervisor
 from gridsync.system import kill
 
+PROCESS_ARGS = [sys.executable, "-c", "while True: print('OK')"]
+
 
 @inlineCallbacks
 def test_supervisor_writes_pid_to_pidfile(tmp_path):
     supervisor = Supervisor()
     pidfile = tmp_path / "python.pid"
-    pid = yield supervisor.start(["python"], pidfile)
+    pid = yield supervisor.start(PROCESS_ARGS, pidfile)
     assert int(pidfile.read_text()) == pid
 
 
@@ -18,7 +22,7 @@ def test_supervisor_writes_pid_to_pidfile(tmp_path):
 def test_supervisor_restarts_process_when_killed(tmp_path):
     supervisor = Supervisor(restart_delay=0)
     pidfile = tmp_path / "python.pid"
-    pid_1 = yield supervisor.start(["python"], pidfile)
+    pid_1 = yield supervisor.start(PROCESS_ARGS, pidfile, started_trigger="OK")
     kill(pidfile=pidfile)
     yield deferLater(reactor, 0.5, lambda: None)
     pid_2 = int(pidfile.read_text())
@@ -29,7 +33,7 @@ def test_supervisor_restarts_process_when_killed(tmp_path):
 def test_supervisor_does_not_restart_process_when_stopped(tmp_path):
     supervisor = Supervisor(restart_delay=0)
     pidfile = tmp_path / "python.pid"
-    yield supervisor.start(["python"], pidfile)
+    yield supervisor.start(PROCESS_ARGS, pidfile, started_trigger="OK")
     supervisor.stop()
     yield deferLater(reactor, 0.5, lambda: None)
     assert pidfile.exists() is False
