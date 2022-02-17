69d74918060e4190763f7a59f3869ce492176a32
Chris Wood <chris@gridsync.io>
2022-02-17 11:08:57 -0500

    Add test for Supervisor.stop functionality

--- tests/test_supervisor.py
+++ tests/test_supervisor.py
@@ -23,3 +23,13 @@ def test_supervisor_restarts_process_when_killed(tmp_path):
     yield deferLater(reactor, 0.1, lambda: None)
     pid_2 = int(pidfile.read_text())
     assert pid_1 != pid_2
+
+
+@inlineCallbacks
+def test_supervisor_does_not_restart_process_when_stopped(tmp_path):
+    supervisor = Supervisor(restart_delay=0)
+    pidfile = tmp_path / "python.pid"
+    yield supervisor.start(["python"], pidfile)
+    supervisor.stop()
+    yield deferLater(reactor, 0.1, lambda: None)
+    assert pidfile.exists() == False
