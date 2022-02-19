0e515c9cb13b18dd47e6b04c2e5ab72ecbd48ac4
Chris Wood <chris@gridsync.io>
2022-02-19 13:37:43 -0500

    Update tests for previous commit

--- tests/test_supervisor.py
+++ tests/test_supervisor.py
@@ -12,17 +12,17 @@ PROCESS_ARGS = [sys.executable, "-c", "while True: print('OK')"]
 
 @inlineCallbacks
 def test_supervisor_writes_pid_to_pidfile(tmp_path):
-    supervisor = Supervisor()
     pidfile = tmp_path / "python.pid"
-    pid = yield supervisor.start(PROCESS_ARGS, pidfile)
+    supervisor = Supervisor(pidfile=pidfile)
+    pid = yield supervisor.start(PROCESS_ARGS)
     assert int(pidfile.read_text()) == pid
 
 
 @inlineCallbacks
 def test_supervisor_restarts_process_when_killed(tmp_path):
-    supervisor = Supervisor(restart_delay=0)
     pidfile = tmp_path / "python.pid"
-    pid_1 = yield supervisor.start(PROCESS_ARGS, pidfile, started_trigger="OK")
+    supervisor = Supervisor(pidfile=pidfile, restart_delay=0)
+    pid_1 = yield supervisor.start(PROCESS_ARGS, started_trigger="OK")
     kill(pidfile=pidfile)
     yield deferLater(reactor, 2, lambda: None)
     pid_2 = int(pidfile.read_text())
@@ -31,9 +31,9 @@ def test_supervisor_restarts_process_when_killed(tmp_path):
 
 @inlineCallbacks
 def test_supervisor_does_not_restart_process_when_stopped(tmp_path):
-    supervisor = Supervisor(restart_delay=0)
     pidfile = tmp_path / "python.pid"
-    yield supervisor.start(PROCESS_ARGS, pidfile, started_trigger="OK")
+    supervisor = Supervisor(pidfile=pidfile, restart_delay=0)
+    yield supervisor.start(PROCESS_ARGS, started_trigger="OK")
     supervisor.stop()
     yield deferLater(reactor, 0.5, lambda: None)
     assert pidfile.exists() is False
