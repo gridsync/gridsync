590b469fe5eab49fcaa8755592ef6f07472577be
Chris Wood <chris@gridsync.io>
2022-02-22 09:32:59 -0500

    Use `certifi`'s CA bundle on macOS
    Fixes #459

--- gridsync/__init__.py
+++ gridsync/__init__.py
@@ -4,8 +4,6 @@ import os
 import sys
 from collections import namedtuple
 
-import certifi
-
 from gridsync._version import get_versions  # type: ignore
 from gridsync.config import Config
 from gridsync.util import to_bool
@@ -15,9 +13,15 @@ __url__ = "https://github.com/gridsync/gridsync"
 __license__ = "GPLv3"
 
 
-# Workaround for broken-by-default certificate verification on Windows.
-# See https://github.com/twisted/treq/issues/94#issuecomment-116226820
-os.environ["SSL_CERT_FILE"] = certifi.where()
+if sys.platform in ("win32", "darwin"):
+    import certifi
+
+    # Workaround for broken-by-default certificate verification on Windows.
+    # See https://github.com/twisted/treq/issues/94#issuecomment-116226820
+    # TLS certificate verification was also observed to be broken on macOS
+    # 10.16 <https://github.com/gridsync/gridsync/issues/459> -- and using
+    # `certifi`'s CA bundle reportedly fixed it.
+    os.environ["SSL_CERT_FILE"] = certifi.where()
 
 
 if getattr(sys, "frozen", False):
