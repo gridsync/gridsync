3c95dae5a2700c4d413318f4c84911e02cc3b36c
Chris Wood <chris@gridsync.io>
2022-02-28 12:44:37 -0500

    Remove unused import

--- tests/test_magic_folder.py
+++ tests/test_magic_folder.py
@@ -7,7 +7,6 @@ from gridsync.magic_folder import (
     MagicFolder,
     MagicFolderConfigError,
     MagicFolderError,
-    MagicFolderMonitor,
     MagicFolderStatus,
 )
 from gridsync.tahoe import Tahoe
