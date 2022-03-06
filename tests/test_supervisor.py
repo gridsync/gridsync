f786fa0d5561185d97a9c3daabfc1002fb4bd714
Chris Wood <chris@gridsync.io>
2022-03-06 13:44:48 -0500

    Update tests

--- tests/test_supervisor.py
+++ tests/test_supervisor.py
@@ -1,3 +1,5 @@
+import os
+import signal
 import sys
 
 from pytest_twisted import inlineCallbacks
@@ -22,7 +24,7 @@ def test_supervisor_unsets_pid_attribute_on_stop(tmp_path):
     supervisor = Supervisor()
     pid = yield supervisor.start(PROCESS_ARGS)
     pid_was_set = supervisor.pid == pid
-    supervisor.stop()
+    yield supervisor.stop()
     pid_was_unset = supervisor.pid is None
     assert pid_was_set and pid_was_unset
 
@@ -40,7 +42,7 @@ def test_supervisor_restarts_process_when_killed(tmp_path):
     pidfile = tmp_path / "python.pid"
     supervisor = Supervisor(pidfile=pidfile, restart_delay=0)
     pid_1 = yield supervisor.start(PROCESS_ARGS, started_trigger="OK")
-    kill(pidfile=pidfile)
+    os.kill(pid_1, signal.SIGKILL)
     yield deferLater(reactor, 2, lambda: None)
     pid_2 = int(pidfile.read_text())
     assert pid_1 != pid_2
@@ -51,6 +53,6 @@ def test_supervisor_does_not_restart_process_when_stopped(tmp_path):
     pidfile = tmp_path / "python.pid"
     supervisor = Supervisor(pidfile=pidfile, restart_delay=0)
     yield supervisor.start(PROCESS_ARGS, started_trigger="OK")
-    supervisor.stop()
+    yield supervisor.stop()
     yield deferLater(reactor, 0.5, lambda: None)
-    assert pidfile.exists() is False
+    assert supervisor.pid is None
