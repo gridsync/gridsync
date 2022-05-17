681d57e272d6d50819ea6820c8811a971de24a07
Chris Wood <chris@gridsync.io>
2022-05-17 14:36:03 -0400

    Reorder methods

--- gridsync/zkapauthorizer.py
+++ gridsync/zkapauthorizer.py
@@ -54,32 +54,68 @@ class ZKAPAuthorizer:
         return resp
 
     @inlineCallbacks
-    def replicate(self) -> TwistedDeferred[str]:
-        resp = yield self._request("POST", "/replicate")
-        if resp.code == 201:
-            content = yield treq.json_content(resp)
-            return content.get("recovery-capability")
-        content = yield treq.content(resp)
-        print(content)
-        raise TahoeWebError(f"Error configuring replication: {resp.code}")
-
-    @inlineCallbacks
-    def recover(self, dircap: str) -> TwistedDeferred[None]:
-        resp = yield self._request(
-            "POST",
-            "/recover",
-            json.dumps({"recovery-capability": dircap}).encode(),
-        )
-        if resp.code != 202:
-            raise TahoeWebError(f"Error starting recovery: {resp.code}")
-
-    @inlineCallbacks
-    def get_recovery_status(self) -> TwistedDeferred[str]:
-        resp = yield self._request("GET", "/recover")
+    def get_version(self) -> TwistedDeferred[str]:
+        version = ""
+        resp = yield self._request("GET", "/version")
         if resp.code == 200:
             content = yield treq.json_content(resp)
-            return content.get("stage")
-        raise TahoeWebError(f"Error getting recovery status: {resp.code}")
+            version = content.get("version", "")
+        return version
+
+    @inlineCallbacks
+    def _get_content(self, cap: str) -> TwistedDeferred[bytes]:
+        resp = yield treq.get(f"{self.gateway.nodeurl}uri/{cap}")
+        if resp.code == 200:
+            content = yield treq.content(resp)
+            return content
+        raise TahoeWebError(f"Error getting cap content: {resp.code}")
+
+    @inlineCallbacks
+    def get_sizes(self) -> TwistedDeferred[List[Optional[int]]]:
+        sizes: list = []
+        rootcap = self.gateway.get_rootcap()
+        rootcap_bytes = yield self._get_content(f"{rootcap}/?t=json")
+        if not rootcap_bytes:
+            return sizes
+        sizes.append(len(rootcap_bytes))
+        rootcap_data = json.loads(rootcap_bytes.decode("utf-8"))
+        if rootcap_data:
+            dircaps = []
+            for data in rootcap_data[1]["children"].values():
+                rw_uri = data[1].get("rw_uri", "")
+                if rw_uri:  # Only care about dirs the user can write to
+                    dircaps.append(rw_uri)
+            for dircap in dircaps:
+                dircap_bytes = yield self._get_content(f"{dircap}/?t=json")
+                sizes.append(len(dircap_bytes))
+                dircap_data = json.loads(dircap_bytes.decode("utf-8"))
+                for data in dircap_data[1]["children"].values():
+                    size = data[1].get("size", 0)
+                    if size:
+                        sizes.append(size)
+        mf_sizes = yield self.gateway.magic_folder.get_all_object_sizes()
+        sizes.extend(mf_sizes)
+        return sizes
+
+    @inlineCallbacks
+    def calculate_price(self, sizes: List[int]) -> TwistedDeferred[Dict]:
+        if not self.gateway.nodeurl:
+            return {}
+        resp = yield self._request(
+            "POST",
+            "/calculate-price",
+            json.dumps({"version": 1, "sizes": sizes}).encode(),
+        )
+        if resp.code == 200:
+            content = yield treq.json_content(resp)
+            return content
+        raise TahoeWebError(f"Error calculating price: {resp.code}")
+
+    @inlineCallbacks
+    def get_price(self) -> TwistedDeferred[Dict]:
+        sizes = yield self.get_sizes()
+        price = yield self.calculate_price(sizes)
+        return price
 
     @inlineCallbacks
     def add_voucher(
@@ -154,75 +190,39 @@ class ZKAPAuthorizer:
         return self.zkap_dircap
 
     @inlineCallbacks
+    def replicate(self) -> TwistedDeferred[str]:
+        resp = yield self._request("POST", "/replicate")
+        if resp.code == 201:
+            content = yield treq.json_content(resp)
+            return content.get("recovery-capability")
+        content = yield treq.content(resp)
+        print(content)
+        raise TahoeWebError(f"Error configuring replication: {resp.code}")
+
+    @inlineCallbacks
+    def recover(self, dircap: str) -> TwistedDeferred[None]:
+        resp = yield self._request(
+            "POST",
+            "/recover",
+            json.dumps({"recovery-capability": dircap}).encode(),
+        )
+        if resp.code != 202:
+            raise TahoeWebError(f"Error starting recovery: {resp.code}")
+
+    @inlineCallbacks
+    def get_recovery_status(self) -> TwistedDeferred[str]:
+        resp = yield self._request("GET", "/recover")
+        if resp.code == 200:
+            content = yield treq.json_content(resp)
+            return content.get("stage")
+        raise TahoeWebError(f"Error getting recovery status: {resp.code}")
+
+    @inlineCallbacks
     def backup_zkaps(self) -> TwistedDeferred[None]:
         # XXX
         pass
 
     @inlineCallbacks
-    def _get_content(self, cap: str) -> TwistedDeferred[bytes]:
-        resp = yield treq.get(f"{self.gateway.nodeurl}uri/{cap}")
-        if resp.code == 200:
-            content = yield treq.content(resp)
-            return content
-        raise TahoeWebError(f"Error getting cap content: {resp.code}")
-
-    @inlineCallbacks
     def restore_zkaps(self) -> TwistedDeferred[None]:
         # XXX
         pass
-
-    @inlineCallbacks
-    def get_version(self) -> TwistedDeferred[str]:
-        version = ""
-        resp = yield self._request("GET", "/version")
-        if resp.code == 200:
-            content = yield treq.json_content(resp)
-            version = content.get("version", "")
-        return version
-
-    @inlineCallbacks
-    def get_sizes(self) -> TwistedDeferred[List[Optional[int]]]:
-        sizes: list = []
-        rootcap = self.gateway.get_rootcap()
-        rootcap_bytes = yield self._get_content(f"{rootcap}/?t=json")
-        if not rootcap_bytes:
-            return sizes
-        sizes.append(len(rootcap_bytes))
-        rootcap_data = json.loads(rootcap_bytes.decode("utf-8"))
-        if rootcap_data:
-            dircaps = []
-            for data in rootcap_data[1]["children"].values():
-                rw_uri = data[1].get("rw_uri", "")
-                if rw_uri:  # Only care about dirs the user can write to
-                    dircaps.append(rw_uri)
-            for dircap in dircaps:
-                dircap_bytes = yield self._get_content(f"{dircap}/?t=json")
-                sizes.append(len(dircap_bytes))
-                dircap_data = json.loads(dircap_bytes.decode("utf-8"))
-                for data in dircap_data[1]["children"].values():
-                    size = data[1].get("size", 0)
-                    if size:
-                        sizes.append(size)
-        mf_sizes = yield self.gateway.magic_folder.get_all_object_sizes()
-        sizes.extend(mf_sizes)
-        return sizes
-
-    @inlineCallbacks
-    def calculate_price(self, sizes: List[int]) -> TwistedDeferred[Dict]:
-        if not self.gateway.nodeurl:
-            return {}
-        resp = yield self._request(
-            "POST",
-            "/calculate-price",
-            json.dumps({"version": 1, "sizes": sizes}).encode(),
-        )
-        if resp.code == 200:
-            content = yield treq.json_content(resp)
-            return content
-        raise TahoeWebError(f"Error calculating price: {resp.code}")
-
-    @inlineCallbacks
-    def get_price(self) -> TwistedDeferred[Dict]:
-        sizes = yield self.get_sizes()
-        price = yield self.calculate_price(sizes)
-        return price
