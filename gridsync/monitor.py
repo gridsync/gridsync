gridsync/gridsync

commit 290f8263c40b52c8a79ee152e16c87e7f5983a9c

calculate tokens with available endpoints

gridsync/monitor.py
@@ -302,48 +302,27 @@ class ZKAPChecker(QObject):
             vouchers,
             self._time_started,
         )
-        total = parse.total_tokens
+        total_tokens = parse.total_tokens
         self._update_unpaid_vouchers(parse.unpaid_vouchers)
         self._update_redeeming_vouchers(parse.redeeming_vouchers)
         self._update_zkaps_last_redeemed(parse.zkaps_last_redeemed)
 
-        try:
-            zkaps = yield self.gateway.zkapauthorizer.get_zkaps(limit=1)
-        except (ConnectError, TahoeWebError):
-            return  # XXX
-        remaining = zkaps.get("total")
-        if remaining and not total:
-            total = self._maybe_load_last_total()
-        if not total or remaining > total:
-            # When redeeming tokens in batches, ZKAPs become available
-            # during the "redeeming" state but the *total* number is
-            # not shown until the "redeemed" state. To prevent the
-            # appearance of negative ZKAP balances during this time,
-            # temporarily consider the current number of remaining
-            # ZKAPs to be the total. For more context, see:
-            # https://github.com/PrivateStorageio/ZKAPAuthorizer/issues/124
-            total = remaining
-        if remaining != self.zkaps_remaining or total != self.zkaps_total:
-            self.emit_zkaps_updated(remaining, total)
-            if not self.zkaps_remaining and remaining:
-                # Some (non-zero) amount of ZKAPs are now available --
-                # in other words, the grid is now "writeable"
-                self.zkaps_available.emit()
-            self.zkaps_remaining = remaining
-            self.zkaps_total = total
-        elif not remaining or not total:
-            self.emit_zkaps_updated(remaining, total)
-        spending = zkaps.get("lease-maintenance-spending")
-        if spending:
-            count = spending.get("count")
-        else:
-            # If a lease maintenance crawl hasn't yet happened, we can assume
-            # that the cost to renew (in the first crawl) will be equivalent
-            # to the number of ZKAPs already used/consumed/spent.
-            count = self.zkaps_total - self.zkaps_remaining
+        count = yield self.gateway.zkapauthorizer.get_lease_maintenance_spending()
+        unspent_tokens = yield self.gateway.zkapauthorizer.get_total_zkaps()
         if count and count != self.zkaps_renewal_cost:
             self.zkaps_renewal_cost_updated.emit(count)
             self.zkaps_renewal_cost = count
+
+        if unspent_tokens != self.zkaps_remaining or total_tokens != self.zkaps_total:
+            self.emit_zkaps_updated(unspent_tokens, total_tokens)
+            if not self.zkaps_remaining and unspent_tokens:
+                # Some (non-zero) amount of ZKAPs are now available --
+                # in other words, the grid is now "writeable"
+                self.zkaps_available.emit()
+            self.zkaps_remaining = unspent_tokens
+            self.zkaps_total = total_tokens
+        elif not unspent_tokens or not total_tokens:
+            self.emit_zkaps_updated(unspent_tokens, total_tokens)
 
         # XXX/FIXME: This assumes that leases will be renewed every 27 days.
         # daily_cost = self.zkaps_renewal_cost / 27

gridsync/zkapauthorizer.py
@@ -6,7 +6,8 @@ import json
 from typing import TYPE_CHECKING, Dict, List, Optional
 
 import treq
-from twisted.internet.defer import inlineCallbacks
+from atomicwrites import atomic_write
+from twisted.internet.defer import inlineCallbacks, returnValue
 
 from gridsync.errors import TahoeWebError
 from gridsync.types import TreqResponse, TwistedDeferred
@@ -123,8 +124,9 @@ class ZKAPAuthorizer:
             "PUT", "/voucher", json.dumps({"voucher": voucher}).encode()
         )
         if resp.code == 200:
-            return voucher
-        raise TahoeWebError(f"Error adding voucher: {resp.code}")
+            returnValue(voucher)
+        content = yield resp.content()
+        raise TahoeWebError(f"Error adding voucher: {resp.code}: {content}")
 
     @inlineCallbacks
     def get_voucher(self, voucher: str) -> TwistedDeferred[Dict]:
@@ -142,25 +144,6 @@ class ZKAPAuthorizer:
             return content.get("vouchers")
         raise TahoeWebError(f"Error getting vouchers: {resp.code}")
 
-    @inlineCallbacks
-    def get_zkaps(
-        self, limit: Optional[int] = 0, position: Optional[str] = None
-    ) -> TwistedDeferred[bytes]:
-        query_params = []
-        if limit:
-            query_params.append(f"limit={limit}")
-        if position:
-            query_params.append(f"position={position}")
-        if query_params:
-            query_string = "?" + "&".join(query_params)
-        else:
-            query_string = ""
-        resp = yield self._request("GET", f"/unblinded-token{query_string}")
-        if resp.code == 200:
-            content = yield treq.json_content(resp)
-            return content
-        raise TahoeWebError(f"Error getting ZKAPs: {resp.code}")
-
     def zkap_payment_url(self, voucher: str) -> str:
         if not self.zkap_payment_url_root:
             return ""
@@ -169,6 +152,35 @@ class ZKAPAuthorizer:
             voucher,
             hashlib.sha256(voucher.encode()).hexdigest(),
         )
+
+    @inlineCallbacks
+    def get_total_zkaps(self) -> TwistedDeferred[int]:
+        """
+        Uses the /lease-maintenance endpoint to ask ZKAPAuthorizer how
+        many tokens it knows about.
+
+        :returns: the total number of ZKAPs we have (spend and unspent
+            together)
+        """
+        resp = yield self._request("GET", "/lease-maintenance")
+        if resp.code == 200:
+            content = yield treq.json_content(resp)
+            return content.get("total", 0)
+        raise TahoeWebError(f"Error getting ZKAPs: {resp.code}")
+
+    @inlineCallbacks
+    def get_lease_maintenance_spending(self) -> TwistedDeferred[Union[None, int]]:
+        """
+        Uses the /lease-maintenance endpoint to ask ZKAPAuthorizer how
+        much we've spent on lease-maintenance
+
+        :returns: ???
+        """
+        resp = yield self._request("GET", "/lease-maintenance")
+        if resp.code == 200:
+            content = yield treq.json_content(resp)
+            return content.get("spending", None)
+        raise TahoeWebError(f"Error getting ZKAPs: {resp.code}")
 
     @inlineCallbacks
     def get_zkap_dircap(self) -> TwistedDeferred[str]:
@@ -208,6 +220,18 @@ class ZKAPAuthorizer:
     @inlineCallbacks
     def get_recovery_status(self) -> TwistedDeferred[str]:
         resp = yield self._request("GET", "/recover")
+
+    def _get_content(self, cap: str) -> TwistedDeferred[bytes]:
+        resp = yield treq.get(f"{self.gateway.nodeurl}uri/{cap}")
+        if resp.code == 200:
+            content = yield treq.content(resp)
+            return content
+        raise TahoeWebError(f"Error getting cap content: {resp.code}")
+
+    @inlineCallbacks
+    def get_version(self) -> TwistedDeferred[str]:
+        version = ""
+        resp = yield self._request("GET", "/version")
         if resp.code == 200:
             content = yield treq.json_content(resp)
             return content.get("stage")

commit fdccd4a11d47186ae97d3ca980d120d4230ef355

Restore (accidentally?) removed functionality
290f8263c40b52c8a79ee152e16c87e7f5983a9c broke the ZKAP bar chart (which
previously displayed blank/grey segments for ZKAPs that were pending
redemption). This commit restores the old behavior so that still-pending
ZKAPs don't get displayed as having been already spent.

gridsync/monitor.py
@@ -305,31 +305,46 @@ class ZKAPChecker(QObject):
             vouchers,
             self._time_started,
         )
-        total_tokens = parse.total_tokens
+        total = parse.total_tokens
         self._update_unpaid_vouchers(parse.unpaid_vouchers)
         self._update_redeeming_vouchers(parse.redeeming_vouchers)
         self._update_zkaps_last_redeemed(parse.zkaps_last_redeemed)
 
-        lm = yield self.gateway.zkapauthorizer.get_lease_maintenance()
-        count = lm.get("spending", None)
-        unspent_tokens = lm.get("total", 0)
-        if count and count != self.zkaps_renewal_cost:
-            self.zkaps_renewal_cost_updated.emit(count)
-            self.zkaps_renewal_cost = count
-
-        if (
-            unspent_tokens != self.zkaps_remaining
-            or total_tokens != self.zkaps_total
-        ):
-            self.emit_zkaps_updated(unspent_tokens, total_tokens)
-            if not self.zkaps_remaining and unspent_tokens:
+        try:
+            lm = yield self.gateway.zkapauthorizer.get_lease_maintenance()
+        except (ConnectError, TahoeWebError):
+            return  # XXX
+        remaining = lm.get("total", 0)
+        if remaining and not total:
+            total = self._maybe_load_last_total()
+        if not total or remaining > total:
+            # When redeeming tokens in batches, ZKAPs become available
+            # during the "redeeming" state but the *total* number is
+            # not shown until the "redeemed" state. To prevent the
+            # appearance of negative ZKAP balances during this time,
+            # temporarily consider the current number of remaining
+            # ZKAPs to be the total. For more context, see:
+            # https://github.com/PrivateStorageio/ZKAPAuthorizer/issues/124
+            total = remaining
+        if remaining != self.zkaps_remaining or total != self.zkaps_total:
+            self.emit_zkaps_updated(remaining, total)
+            if not self.zkaps_remaining and remaining:
                 # Some (non-zero) amount of ZKAPs are now available --
                 # in other words, the grid is now "writeable"
                 self.zkaps_available.emit()
-            self.zkaps_remaining = unspent_tokens
-            self.zkaps_total = total_tokens
-        elif not unspent_tokens or not total_tokens:
-            self.emit_zkaps_updated(unspent_tokens, total_tokens)
+            self.zkaps_remaining = remaining
+            self.zkaps_total = total
+        elif not remaining or not total:
+            self.emit_zkaps_updated(remaining, total)
+        count = lm.get("spending", None)
+        if not count:
+            # If a lease maintenance crawl hasn't yet happened, we can assume
+            # that the cost to renew (in the first crawl) will be equivalent
+            # to the number of ZKAPs already used/consumed/spent.
+            count = self.zkaps_total - self.zkaps_remaining
+        if count and count != self.zkaps_renewal_cost:
+            self.zkaps_renewal_cost_updated.emit(count)
+            self.zkaps_renewal_cost = count
 
         # XXX/FIXME: This assumes that leases will be renewed every 27 days.
         # daily_cost = self.zkaps_renewal_cost / 27