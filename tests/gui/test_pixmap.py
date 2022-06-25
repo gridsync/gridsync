9f87a4183e8c69fb00a4e5d9a6cb5f3c7d9a84ed
Chris Wood <chris@gridsync.io>
2022-06-25 00:26:35 -0400

    Update, add more pixmap module tests

--- tests/gui/test_pixmap.py
+++ tests/gui/test_pixmap.py
@@ -1,11 +1,25 @@
 """
 Tests for ``gridsync.gui.pixmap``.
 """
+from qtpy.QtGui import QPixmap
 
 from gridsync import resource
-from gridsync.gui.pixmap import BadgedPixmap
+from gridsync.gui.pixmap import BadgedPixmap, CompositePixmap, Pixmap
+
+
+def test_pixmap():
+    p = Pixmap(resource("gridsync.png"), 16)
+    size = p.size()
+    assert (size.width(), size.height()) == (16, 16)
+
+
+def test_composite_pixmap(gui):
+    original = QPixmap(resource("gridsync.png"))
+    composite = CompositePixmap(original, "gridsync.png", grayout=True)
+    assert composite != original
 
 
 def test_badged_pixmap(gui):
-    bp = BadgedPixmap(resource("gridsync.png"), "test")
-    assert bp is not None
+    original = QPixmap(resource("gridsync.png"))
+    badged = BadgedPixmap(original, "test")
+    assert badged != original
