981baf0e7ff5c0e08b95b619186b7132a56b5c4a
Chris Wood <chris@gridsync.io>
2022-07-20 09:20:00 -0400

    Rename Tahoe method: diminish -> get_readonly_cap

--- gridsync/tahoe.py
+++ gridsync/tahoe.py
@@ -603,7 +603,7 @@ class Tahoe:
         )
 
     @inlineCallbacks
-    def diminish(self, cap: str) -> TwistedDeferred[str]:
+    def get_readonly_cap(self, cap: str) -> TwistedDeferred[str]:
         output = yield self.get_json(cap)
         return output[1]["ro_uri"]
 
