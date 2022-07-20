46dc9c33248f4b7ec88ade49aaa87e931ae1b018
Chris Wood <chris@gridsync.io>
2022-07-20 14:52:46 -0400

    Add docstring

--- gridsync/capabilities.py
+++ gridsync/capabilities.py
@@ -3,6 +3,12 @@ from allmydata.uri import from_string as uri_from_string
 
 
 def diminish(cap: str) -> str:
+    """
+    Diminish a readwrite capability string into a read(only) one.
+
+    If the capability is already a read(only) cap, return it unchanged.
+    Raises a ValueError, if the capability type cannot be determined.
+    """
     uri = uri_from_string(cap)
     if isinstance(uri, UnknownURI):
         raise ValueError(f'Unknown URI type: "{cap}"')
