4bf69e63f03bc059d1fbaa75d9bc73dfecef7931
Florian Sesser <florian@leastauthority.com>
2022-08-02 16:56:26 +0200

    cleanup

--- tests/gui/test_charter.py
+++ tests/gui/test_charter.py
@@ -11,28 +11,7 @@ from twisted.python.filepath import FilePath
 from qtpy.QtCore import Qt
 
 from gridsync.gui import Gui
-# from gridsync.gui import MainWindow
 
-# Without this, QWidget() aborts the process with "QWidget: Must construct a
-# QApplication before a QWidget".
-#app = QApplication([])
-
-
-#@pytest.fixture
-#def gui(preferences: Preferences) -> Gui:
-#    """
-#    The value of this fixture is a GUI object which tests can add widgets to
-#    and do other GUI-y things to.
-#    """
-#    return Gui(MagicMock(), preferences=preferences)
-
-#def test_with_qtbot(qtbot):
-#    #with qtbot.wait_signals([proc.started, proc.finished], timeout=10000, order='strict'):
-#    window = Gui(MagicMock())
-#    #qtbot.addWidget(window)
-#    window.show()
-#    qtbot.waitForWindowShown(window)
-#    qtbot.mouseClick(window)
 
 def test_with_common_gui_addressing(qtbot):
     gui = Gui(MagicMock())
@@ -43,21 +22,6 @@ def test_with_common_gui_addressing(qtbot):
     qtbot.mouseClick(window.toolbar.folder_button, Qt.MouseButton.LeftButton)
     window.toolbar.export_action_triggered.emit() # XXX: meh, we want to click instead of emitting signals.
     qtbot.wait(5000)
-
-def test_with_qtbot_mw(qtbot):
-    #with qtbot.wait_signals([proc.started, proc.finished], timeout=10000, order='strict'):
-    window = MainWindow(Gui(MagicMock()))
-    #qtbot.addWidget(window)
-    window.show()
-    qtbot.waitForWindowShown(window)
-
-def test_with_qtbot_mw(qtbot):
-    #with qtbot.wait_signals([proc.started, proc.finished], timeout=10000, order='strict'):
-    window = MainWindow(Gui(MagicMock()))
-    #qtbot.addWidget(window)
-    window.show()
-    qtbot.waitForWindowShown(window)
-    qtbot.mouseClick(window.add_folder_icon)
 
 
 def test_basics(qtbot):
