b21a3128e09c87d5d85731e9cb8256bb8ee836e9
Chris Wood <chris@gridsync.io>
2022-08-05 23:23:22 -0400

    Always upgrade buildbot-worker

--- scripts/provision_buildbot-worker.sh
+++ scripts/provision_buildbot-worker.sh
@@ -3,7 +3,7 @@ set -e
 . ~/.$(basename "$SHELL"rc)
 buildbot-worker stop ~/buildbot || true
 rm -rf ~/buildbot || true
-python3 -m pip install buildbot-worker
+python3 -m pip install --upgrade buildbot-worker
 buildbot-worker create-worker ~/buildbot "$BUILDBOT_HOST" "$BUILDBOT_NAME" "$BUILDBOT_PASS"
 unset BUILDBOT_HOST
 unset BUILDBOT_NAME
