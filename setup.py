f7ca825fd266c2d6492e937af3f924f5d8b05a37
Chris Wood <chris@gridsync.io>
2022-08-25 18:30:48 -0400

    Add "tahoe-capabilities" dependency

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ requirements = [
     "PyNaCl >= 1.2.0",  # 1.2.0 adds Argon2id KDF
     "pyyaml",
     "qtpy",
+    "tahoe-capabilities",
     "tahoe-lafs",
     "treq",
     "twisted[tls] >= 21.7.0",  # 21.7.0 adds Deferred type hinting/annotations
