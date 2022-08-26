e62e090b9240bbcffb7a63513beb91c2a5436522
Chris Wood <chris@gridsync.io>
2022-08-25 21:41:23 -0400

    Remove "tahoe-lafs" dependency

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ requirements = [
     "pyyaml",
     "qtpy",
     "tahoe-capabilities",
-    "tahoe-lafs",
     "treq",
     "twisted[tls] >= 21.7.0",  # 21.7.0 adds Deferred type hinting/annotations
     "txdbus ; sys_platform != 'darwin' and sys_platform != 'win32'",
