c1e48f933074a2582daaa201cb016105f27ceb7f
Chris Wood <chris@gridsync.io>
2022-09-10 20:50:41 -0400

    WIP

--- scripts/notarize.py
+++ scripts/notarize.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import hashlib
+import json
 import os
 import sys
 from configparser import RawConfigParser
 from pathlib import Path
+from secrets import compare_digest
 from subprocess import CalledProcessError, SubprocessError, run
 from time import sleep
 from typing import Optional
@@ -160,21 +162,86 @@ def store_credentials(
     )
 
 
-def submit(filepath: str, keychain_profile: str) -> None:
-    run(
+def notarytool(
+    subcommand: str, args: list[str], keychain_profile: str
+) -> dict[str, str]:
+    proc = run(
         [
             "xcrun",
             "notarytool",
-            "submit",
-            "--keychain-profile",
-            keychain_profile,
-            "--wait",
-            filepath,
+            subcommand,
+            f"--keychain-profile={keychain_profile}",
+            "--output-format=json",
         ]
+        + args,
+        capture_output=True,
+        text=True,
     )
+    if proc.returncode:
+        raise SubprocessError(proc.stderr.strip())
+    if proc.stdout:
+        result = json.loads(proc.stdout.strip())
+    else:
+        return {}
+    message = result.get("message")
+    if message:
+        print("###", message)
+    return result
+
+
+def submit(filepath: str, keychain_profile: str) -> str:  # submission-id
+    result = notarytool("submit", [filepath], keychain_profile)
+    return result["id"]
+
+
+def info(submission_id: str, keychain_profile: str) -> dict[str, str]:
+    result = notarytool("info", [submission_id], keychain_profile)
+    return result
+
+
+def wait(submission_id: str, keychain_profile: str) -> str:
+    result = notarytool("wait", [submission_id], keychain_profile)
+    return result["status"]
+
+
+def log(submission_id: str, keychain_profile: str) -> dict[str, str]:
+    result = notarytool("log", [submission_id], keychain_profile)
+    return result
+
+
+def main(path: str, keychain_profile: str) -> None:
+    if not path.lower().endswith(".dmg") and not path.lower().endswith(".zip"):
+        print("Creating ZIP archive...")
+        submission_path = path + ".zip"
+        make_zipfile(path, submission_path)
+    else:
+        submission_path = path
+    submitted_hash = sha256sum(submission_path)
+    print(f"Uploading {submission_path} ({submitted_hash})...")
+    submission_id = submit(submission_path, keychain_profile)
+    print(info(submission_id, keychain_profile))
+    print("Waiting for result...")
+    status = wait(submission_id, keychain_profile)
+    result = log(submission_id, keychain_profile)
+    print(json.dumps(result, sort_keys=True, indent=2))
+    if status != "Accepted":
+        sys.exit(f'ERROR: Notarization failed; status: "{status}"')
+    notarized_hash = result["sha256"]
+    if not compare_digest(submitted_hash, notarized_hash):
+        sys.exit(
+            "ERROR: SHA-256 hash mismatch\n"
+            f"Submitted: {submitted_hash}\n"
+            f"Notarized: {submitted_hash}"
+        )
+    staple(path)
+    print("Success!")
 
 
 if __name__ == "__main__":
+    path = sys.argv[1]
+    main(path, "gridsync")
+    sys.exit()
+
     config = RawConfigParser(allow_no_value=True)
     config.read(Path("gridsync", "resources", "config.txt"))
     settings = {}
