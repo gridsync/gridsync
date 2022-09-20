f63040d8c087a60b4a653c980b0301f47d2176b8
Chris Wood <chris@gridsync.io>
2022-09-20 12:50:05 -0400

    Re-order things

--- gridsync/log.py
+++ gridsync/log.py
@@ -7,6 +7,13 @@ from typing import Optional
 from twisted.python.log import PythonLoggingObserver, startLogging
 
 
+class LogFormatter(logging.Formatter):
+    def formatTime(
+        self, record: logging.LogRecord, datefmt: Optional[str] = None
+    ) -> str:
+        return datetime.now(timezone.utc).isoformat()
+
+
 class DequeHandler(logging.Handler):
     def __init__(self, deque: collections.deque) -> None:
         super().__init__()
@@ -16,29 +23,25 @@ class DequeHandler(logging.Handler):
         self.deque.append(self.format(record))
 
 
-class LogFormatter(logging.Formatter):
-    def formatTime(
-        self, record: logging.LogRecord, datefmt: Optional[str] = None
-    ) -> str:
-        return datetime.now(timezone.utc).isoformat()
-
-
 def initialize_logger(
     log_deque: collections.deque, to_stdout: bool = False
 ) -> None:
-    deque_handler = DequeHandler(log_deque)
-    observer = PythonLoggingObserver()
-    observer.start()
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
     formatter = LogFormatter(
         fmt="%(asctime)s %(levelname)s %(funcName)s %(message)s"
     )
+
+    deque_handler = DequeHandler(log_deque)
     deque_handler.setFormatter(formatter)
-    logger = logging.getLogger()
     logger.addHandler(deque_handler)
+
     if to_stdout:
         stdout_handler = logging.StreamHandler(stream=sys.stdout)
-        startLogging(sys.stdout)
         stdout_handler.setFormatter(formatter)
         logger.addHandler(stdout_handler)
-    logger.setLevel(logging.DEBUG)
+        startLogging(sys.stdout)
+
+    observer = PythonLoggingObserver()
+    observer.start()
     logging.debug("Hello World!")
