23ac6022b1bc0cd975750d53fb8f30080a2a39ac
Chris Wood <chris@gridsync.io>
2022-09-29 22:00:57 -0400

    Restore DequeHandler; add MemoryLogger

--- gridsync/log.py
+++ gridsync/log.py
@@ -1,3 +1,4 @@
+import collections
 import logging
 import sys
 from datetime import datetime, timezone
@@ -96,3 +97,59 @@ class MultiFileLogger:
         for p in find_log_files(f"{self.basename}.{logger_name}.log*"):
             messages.extend(read_log_messages(p))
         return messages
+
+
+class DequeHandler(logging.Handler):
+    def __init__(self, deque: collections.deque) -> None:
+        super().__init__()
+        self.deque = deque
+
+    def emit(self, record: logging.LogRecord) -> None:
+        self.deque.append(self.format(record))
+
+
+def make_memory_logger(
+    deque: collections.deque,
+    name: Optional[str] = None,
+    fmt: Optional[str] = "%(asctime)s %(levelname)s %(funcName)s %(message)s",
+) -> logging.Logger:
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+
+    if not name:
+        name = APP_NAME
+
+    handler = DequeHandler(deque)
+    if fmt:
+        handler.setFormatter(LogFormatter(fmt=fmt))
+    logger.addHandler(handler)
+    return logger
+
+
+class MemoryLogger:
+    def __init__(self, basename: str, maxlen: int = 10_000) -> None:
+        self.basename = basename
+        self.maxlen = maxlen
+        self._loggers: dict[str, logging.Logger] = {}
+        self._deques: dict[str, collections.deque] = {}
+
+    def log(
+        self, logger_name: str, message: str, omit_fmt: bool = False
+    ) -> None:
+        name = f"{self.basename}.{logger_name}"
+        logger = self._loggers.get(name)
+        if not logger:
+            deque: collections.deque = collections.deque(maxlen=self.maxlen)
+            if omit_fmt:
+                logger = make_memory_logger(deque, name, fmt=None)
+            else:
+                logger = make_memory_logger(deque, name)
+            self._loggers[name] = logger
+            self._deques[name] = deque
+        logger.debug(message)
+
+    def read_messages(self, logger_name: str) -> list[str]:
+        deque = self._deques.get(f"{self.basename}.{logger_name}")
+        if deque:
+            return list(deque)
