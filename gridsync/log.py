61f4319319e63b6bea60c8ca6dccf74a64fe1bb7
Chris Wood <chris@gridsync.io>
2022-09-30 22:00:22 -0400

    Remove DequeHandler, MemoryLogger

--- gridsync/log.py
+++ gridsync/log.py
@@ -1,4 +1,3 @@
-import collections
 import logging
 import sys
 from datetime import datetime, timezone
@@ -15,15 +14,6 @@ if _logging_path:
     LOGS_PATH = Path(_logging_path)
 else:
     LOGS_PATH = Path(config_dir, "logs")
-
-
-class DequeHandler(logging.Handler):
-    def __init__(self, deque: collections.deque) -> None:
-        super().__init__()
-        self.deque = deque
-
-    def emit(self, record: logging.LogRecord) -> None:
-        self.deque.append(self.format(record))
 
 
 class LogFormatter(logging.Formatter):
@@ -59,33 +49,10 @@ def make_file_logger(
     return logger
 
 
-def make_memory_logger(
-    deque: collections.deque,
-    name: Optional[str] = None,
-    fmt: Optional[str] = "%(asctime)s %(levelname)s %(funcName)s %(message)s",
-) -> logging.Logger:
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)
-    logger.propagate = False
-
-    if not name:
-        name = APP_NAME
-
-    handler = DequeHandler(deque)
-    if fmt:
-        handler.setFormatter(LogFormatter(fmt=fmt))
-    logger.addHandler(handler)
-    return logger
-
-
 def initialize_logger(
     to_stdout: bool = False,
-    memory_logger_deque: Optional[collections.deque] = None,
 ) -> None:
-    if memory_logger_deque is not None:
-        logger = make_memory_logger(memory_logger_deque)
-    else:
-        logger = make_file_logger()
+    logger = make_file_logger()
     formatter = LogFormatter(
         fmt="%(asctime)s %(levelname)s %(funcName)s %(message)s"
     )
@@ -135,29 +102,3 @@ class MultiFileLogger:
         for p in find_log_files(f"{self.basename}.{logger_name}.log*"):
             messages.extend(read_log_messages(p))
         return messages
-
-
-class MemoryLogger:
-    def __init__(self, basename: str, maxlen: int = 10_000) -> None:
-        self.basename = basename
-        self.maxlen = maxlen
-        self._loggers: dict[str, logging.Logger] = {}
-        self._deques: dict[str, collections.deque] = {}
-
-    def log(
-        self, logger_name: str, message: str, omit_fmt: bool = False
-    ) -> None:
-        name = f"{self.basename}.{logger_name}"
-        logger = self._loggers.get(name)
-        if not logger:
-            deque: collections.deque = collections.deque(maxlen=self.maxlen)
-            if omit_fmt:
-                logger = make_memory_logger(deque, name, fmt=None)
-            else:
-                logger = make_memory_logger(deque, name)
-            self._loggers[name] = logger
-            self._deques[name] = deque
-        logger.debug(message)
-
-    def read_messages(self, logger_name: str) -> list[str]:
-        return list(self._deques.get(f"{self.basename}.{logger_name}", []))
