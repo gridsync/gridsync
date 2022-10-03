2316a223e650cfc0916039ca0cb34eccfce969c2
meejah <meejah@meejah.ca>
2022-10-03 09:13:35 -0600

    fix tests, remove no-longer-relevant tests

--- tests/test_supervisor.py
+++ tests/test_supervisor.py
@@ -13,31 +13,31 @@ PROCESS_ARGS = [sys.executable, "-c", "while True: print('OK')"]
 
 @inlineCallbacks
 def test_supervisor_sets_pid_attribute_on_start(tmp_path):
-    supervisor = Supervisor()
+    supervisor = Supervisor(tmp_path / "pidfile")
     pid, _ = yield supervisor.start(PROCESS_ARGS)
-    assert supervisor.pid == pid
+    assert supervisor.process.pid == pid
 
 
 @inlineCallbacks
 def test_supervisor_sets_name_attribute_on_start(tmp_path):
-    supervisor = Supervisor()
+    supervisor = Supervisor(tmp_path / "pidfile")
     _, name = yield supervisor.start(PROCESS_ARGS)
     assert supervisor.name == name
 
 
 @inlineCallbacks
 def test_supervisor_unsets_pid_attribute_on_stop(tmp_path):
-    supervisor = Supervisor()
+    supervisor = Supervisor(tmp_path / "pidfile")
     pid, _ = yield supervisor.start(PROCESS_ARGS)
-    pid_was_set = supervisor.pid == pid
+    pid_was_set = supervisor.process.pid == pid
     yield supervisor.stop()
-    pid_was_unset = supervisor.pid is None
+    pid_was_unset = supervisor.process is None
     assert pid_was_set and pid_was_unset
 
 
 @inlineCallbacks
 def test_supervisor_unsets_name_attribute_on_stop(tmp_path):
-    supervisor = Supervisor()
+    supervisor = Supervisor(tmp_path / "pidfile")
     _, name = yield supervisor.start(PROCESS_ARGS)
     name_was_set = supervisor.name == name
     yield supervisor.stop()
@@ -46,56 +46,29 @@ def test_supervisor_unsets_name_attribute_on_stop(tmp_path):
 
 
 @inlineCallbacks
-def test_supervisor_writes_pid_to_pidfile_on_start(tmp_path):
-    pidfile = tmp_path / "python.pid"
-    supervisor = Supervisor(pidfile=pidfile)
-    pid, _ = yield supervisor.start(PROCESS_ARGS)
-    assert int(pidfile.read_text().split()[0]) == pid
-
-
-@inlineCallbacks
-def test_supervisor_writes_name_to_pidfile_on_start(tmp_path):
-    pidfile = tmp_path / "python.pid"
-    supervisor = Supervisor(pidfile=pidfile)
-    _, name = yield supervisor.start(PROCESS_ARGS)
-    assert " ".join(pidfile.read_text().split()[1:]) == name
-
-
-@inlineCallbacks
-def test_supervisor_removes_pidfile_on_stop(tmp_path):
-    pidfile = tmp_path / "python.pid"
-    supervisor = Supervisor(pidfile=pidfile)
-    pid, _ = yield supervisor.start(PROCESS_ARGS)
-    pidfile_was_written = int(pidfile.read_text().split()[0]) == pid
-    yield supervisor.stop()
-    pidfile_was_removed = not pidfile.exists()
-    assert pidfile_was_written and pidfile_was_removed
-
-
-@inlineCallbacks
 def test_supervisor_restarts_process_when_killed(tmp_path):
     pidfile = tmp_path / "python.pid"
-    supervisor = Supervisor(pidfile=pidfile, restart_delay=0)
+    supervisor = Supervisor(pidfile, restart_delay=0)
     pid, _ = yield supervisor.start(PROCESS_ARGS, started_trigger="OK")
     assert supervisor.is_running()
     Process(pid).kill()
-    yield until(lambda: supervisor.pid != pid)
+    yield until(lambda: supervisor.process.pid != pid)
     assert supervisor.is_running()
 
 
 @inlineCallbacks
 def test_supervisor_does_not_restart_process_when_stopped(tmp_path):
     pidfile = tmp_path / "python.pid"
-    supervisor = Supervisor(pidfile=pidfile, restart_delay=0)
+    supervisor = Supervisor(pidfile, restart_delay=0)
     yield supervisor.start(PROCESS_ARGS, started_trigger="OK")
     yield supervisor.stop()
     yield deferLater(reactor, 0.5, lambda: None)
-    assert supervisor.pid is None
+    assert supervisor.process is None
 
 
 @inlineCallbacks
 def test_supervisor_calls_call_before_start(tmp_path):
-    supervisor = Supervisor()
+    supervisor = Supervisor(tmp_path / "pidfile")
     f_was_called = [False]
 
     def f():
@@ -110,7 +83,7 @@ def test_supervisor_calls_call_before_start(tmp_path):
 
 @inlineCallbacks
 def test_supervisor_calls_call_after_start(tmp_path):
-    supervisor = Supervisor()
+    supervisor = Supervisor(tmp_path / "pidfile")
     f_was_called = [False]
 
     def f():
