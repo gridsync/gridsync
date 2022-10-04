465d1c3a362b3d1bde4ddf890185983ea2384985
Chris Wood <chris@gridsync.io>
2022-10-04 13:04:11 -0400

    Use *_HELP_URL constants defined in __init__.py

--- gridsync/gui/menu.py
+++ gridsync/gui/menu.py
@@ -9,7 +9,14 @@ from qtpy.QtCore import Qt
 from qtpy.QtGui import QIcon
 from qtpy.QtWidgets import QAction, QMenu, QMessageBox
 
-from gridsync import APP_NAME, __version__, resource, settings
+from gridsync import (
+    APP_NAME,
+    DOCS_HELP_URL,
+    ISSUES_HELP_URL,
+    __version__,
+    resource,
+    settings,
+)
 
 if TYPE_CHECKING:
     from gridsync.gui import AbstractGui
@@ -39,22 +46,20 @@ class Menu(QMenu):
 
         help_menu = QMenu(self)
         help_menu.setTitle("Help")
-        help_settings = settings.get("help")
-        if help_settings:
-            if help_settings.get("docs_url"):
-                docs_action = QAction(
-                    QIcon(""), "Browse Documentation...", self
-                )
-                docs_action.triggered.connect(
-                    lambda: webbrowser.open(settings["help"]["docs_url"])
-                )
-                help_menu.addAction(docs_action)
-            if help_settings.get("issues_url"):
-                issue_action = QAction(QIcon(""), "Report Issue...", self)
-                issue_action.triggered.connect(
-                    lambda: webbrowser.open(settings["help"]["issues_url"])
-                )
-                help_menu.addAction(issue_action)
+        if DOCS_HELP_URL:
+            docs_action = QAction(
+                QIcon(""), "Browse Documentation...", self
+            )
+            docs_action.triggered.connect(
+                lambda: webbrowser.open(DOCS_HELP_URL)
+            )
+            help_menu.addAction(docs_action)
+        if ISSUES_HELP_URL:
+            issue_action = QAction(QIcon(""), "Report Issue...", self)
+            issue_action.triggered.connect(
+                lambda: webbrowser.open(ISSUES_HELP_URL)
+            )
+            help_menu.addAction(issue_action)
         export_action = QAction(QIcon(""), "View Debug Information...", self)
         export_action.triggered.connect(self.gui.show_debug_exporter)
         help_menu.addAction(export_action)
