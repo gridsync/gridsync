929820947e93b236704b73f264dfefadc7d70227
Chris Wood <chris@gridsync.io>
2023-03-20 23:52:59 -0400

    Reduce QR code border size

--- gridsync/gui/qrcode.py
+++ gridsync/gui/qrcode.py
@@ -8,6 +8,6 @@ class QRCode(QImage):
     def __init__(self, data: str) -> None:
         super().__init__()
         buffer = BytesIO()
-        segno.make_qr(data).save(buffer, kind="png")
+        segno.make_qr(data).save(buffer, kind="png", border=1)
         buffer.seek(0)
         self.loadFromData(buffer.read())
