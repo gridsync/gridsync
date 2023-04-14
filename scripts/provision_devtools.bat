803218981930d70e223c3f8bded20bbe5f658099
Chris Wood <chris@gridsync.io>
2023-04-14 10:01:19 -0400

    Update python interpreter versions on Windows

--- scripts/provision_devtools.bat
+++ scripts/provision_devtools.bat
@@ -1,6 +1,6 @@
 choco install -y --no-progress --require-checksums git
-choco install -y --no-progress --require-checksums -m python3 --version 3.10.8
-choco install -y --no-progress --require-checksums -m python3 --version 3.11.0
+choco install -y --no-progress --require-checksums -m python3 --version 3.10.11
+choco install -y --no-progress --require-checksums -m python3 --version 3.11.3
 choco install -y --no-progress --require-checksums visualcpp-build-tools
 choco install -y --no-progress --require-checksums innosetup
 refreshenv
