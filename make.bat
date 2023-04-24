62f5058e0cc718bd84dc6706e80d2993b95fef49
Chris Wood <chris@gridsync.io>
2023-04-24 14:27:19 -0400

    Try a pyinstaller-merged build on Windows...

--- make.bat
+++ make.bat
@@ -53,10 +53,11 @@ py -m tox -e pyinstaller-windows || goto :error
 goto :eof
 
 :pyinstaller
-:: call :pyinstaller-merged
-py -m tox -e pyinstaller-tahoe-windows || goto :error
-py -m tox -e pyinstaller-magic-folder-windows || goto :error
-py -m tox -e pyinstaller-gridsync-windows || goto :error
+call :pyinstaller-merged
+:: XXX
+:: py -m tox -e pyinstaller-tahoe-windows || goto :error
+:: py -m tox -e pyinstaller-magic-folder-windows || goto :error
+:: py -m tox -e pyinstaller-gridsync-windows || goto :error
 goto :eof
 
 :zip
