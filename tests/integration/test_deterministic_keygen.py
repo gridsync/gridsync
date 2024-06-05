da6b96e49f5f2998a61b82a709b7479c71eb071b
Chris Wood <chris@gridsync.io>
2024-06-05 15:27:18 -0400

    Remove standalone test_deterministic_keygen module
    This functionality is now covered by the tahoe integration tests

--- tests/integration/test_deterministic_keygen.py
+++
@@ -1,7 +0,0 @@
-import deterministic_keygen
-
-
-def test_deterministic_rsa() -> None:
-    phrase = deterministic_keygen.generate_phrase()
-    key = deterministic_keygen.derive_rsa_key_from_phrase(phrase)
-    assert key.startswith("-----BEGIN PRIVATE KEY-----")
