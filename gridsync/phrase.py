7c5daea21b9b5416b7d8119df6a4bb044e88ba79
Chris Wood <chris@gridsync.io>
2024-06-07 10:57:44 -0400

    Make wordlist available via phrase module

--- gridsync/phrase.py
+++ gridsync/phrase.py
@@ -7,3 +7,6 @@ def to_mnemonic(b: bytes) -> list[str]:
 
 def to_entropy(mnemonic: list[str]) -> bytes:
     return bytes(Mnemonic(language="english").to_entropy(mnemonic))
+
+
+wordlist = Mnemonic(language="english").wordlist
