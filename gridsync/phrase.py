14ca8a8172fe64830ed8c65dfe4184ceb5c25cdc
Chris Wood <chris@gridsync.io>
2024-06-21 16:12:24 -0400

    Allow passing language to phrase utility functions

--- gridsync/phrase.py
+++ gridsync/phrase.py
@@ -1,12 +1,12 @@
 from mnemonic import Mnemonic
 
 
-def to_mnemonic(b: bytes) -> list[str]:
-    return Mnemonic(language="english").to_mnemonic(b).split(" ")
+def to_mnemonic(b: bytes, language: str = "english") -> list[str]:
+    return Mnemonic(language=language).to_mnemonic(b).split(" ")
 
 
-def to_entropy(mnemonic: list[str]) -> bytes:
-    return bytes(Mnemonic(language="english").to_entropy(mnemonic))
+def to_entropy(mnemonic: list[str], language: str = "english") -> bytes:
+    return bytes(Mnemonic(language=language).to_entropy(mnemonic))
 
 
 wordlist = Mnemonic(language="english").wordlist
