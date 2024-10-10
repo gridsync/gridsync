5641d38ba9d5af9a55c5467297eb13c44523da28
Chris Wood <chris@gridsync.io>
2024-10-10 14:29:07 -0400

    Fix branch name

--- scripts/provision_devtools.sh
+++ scripts/provision_devtools.sh
@@ -35,7 +35,7 @@ else
     fi
 fi
 
-git clone --branch master https://github.com/pyenv/pyenv.git ~/.pyenv || git --git-dir=$HOME/.pyenv/.git pull --force --ff origin master || git --git-dir=$HOME/.pyenv/.git reset --hard origin/main || git --git-dir=$HOME/.pyenv/.git merge origin/main
+git clone --branch master https://github.com/pyenv/pyenv.git ~/.pyenv || git --git-dir=$HOME/.pyenv/.git pull --force --ff origin master || git --git-dir=$HOME/.pyenv/.git reset --hard origin/master || git --git-dir=$HOME/.pyenv/.git merge origin/master
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> "$SHELLRC"
 echo 'export PATH="$PYENV_ROOT/bin:$HOME/bin:$PATH"' >> "$SHELLRC"
 echo 'eval "$(pyenv init --path)"' >> "$SHELLRC"
