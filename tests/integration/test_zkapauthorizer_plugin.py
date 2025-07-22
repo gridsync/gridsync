9e04e162acf16502a8752209d03dbf1990908d5a
Chris Wood <chris@gridsync.io>
2025-07-22 16:43:20 -0400

    Conditionally skip zkap integration test module

--- tests/integration/test_zkapauthorizer_plugin.py
+++ tests/integration/test_zkapauthorizer_plugin.py
@@ -1,23 +1,22 @@
 import pytest
 from pytest_twisted import inlineCallbacks
 
-from gridsync.tahoe import ZKAPAUTHORIZER_AVAILABLE
+from gridsync import features
 
-
-@pytest.mark.skipif(
-    not ZKAPAUTHORIZER_AVAILABLE,
-    reason="zkapauthorizer plugin not available",
+# "pytestmark" is a special global variable that can be used to apply marks at
+# the module level. See https://docs.pytest.org/en/stable/example/markers.html
+pytestmark = pytest.mark.skipif(
+    not features.zkapauthorizer,
+    reason="zkapauthorizer plugin not enabled",
 )
+
+
 @inlineCallbacks
 def test_zkapauthorizer_version(zkapauthorizer):
     version = yield zkapauthorizer.get_version()
     assert version != ""
 
 
-@pytest.mark.skipif(
-    not ZKAPAUTHORIZER_AVAILABLE,
-    reason="zkapauthorizer plugin not available",
-)
 @inlineCallbacks
 def test_zkapauthorizer_add_and_get_voucher(zkapauthorizer):
     voucher = yield zkapauthorizer.add_voucher()
@@ -25,10 +24,6 @@ def test_zkapauthorizer_add_and_get_voucher(zkapauthorizer):
     assert output["number"] == voucher
 
 
-@pytest.mark.skipif(
-    not ZKAPAUTHORIZER_AVAILABLE,
-    reason="zkapauthorizer plugin not available",
-)
 @inlineCallbacks
 def test_zkapauthorizer_calculate_price(zkapauthorizer):
     output = yield zkapauthorizer.calculate_price([1024, 2048, 3072, 4096])
